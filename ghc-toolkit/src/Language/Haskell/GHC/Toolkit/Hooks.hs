0ea516476a3696baaf5bd3632d315174751c1fd4
Shao Cheng <astrohavoc@gmail.com>
2018-03-16 03:28:17 +0800

    Implement Run for getting (Map Module IR) in a convenient way

--- ghc-toolkit/src/Language/Haskell/GHC/Toolkit/Hooks.hs
+++ ghc-toolkit/src/Language/Haskell/GHC/Toolkit/Hooks.hs
@@ -15,35 +15,37 @@ import Module
 import PipelineMonad
 
 runPhaseWithCompiler ::
-     Compiler
-  -> IO (PhasePlus -> FilePath -> DynFlags -> CompPipeline (PhasePlus, FilePath))
-runPhaseWithCompiler c = do
-  first_run_modules <- newIORef emptyModuleSet
-  pure $ \phase_plus input_fn dflags ->
-    case phase_plus of
-      HscOut src_flavour _ (HscRecomp cgguts mod_summary@ModSummary {..}) -> do
-        first_run <-
-          liftIO $
-          atomicModifyIORef' first_run_modules $ \s ->
-            if elemModuleSet ms_mod s
-              then (delModuleSet s ms_mod, False)
-              else (extendModuleSet s ms_mod, True)
-        if first_run
-          then do
-            let hsc_lang = hscTarget dflags
-                next_phase = hscPostBackendPhase dflags src_flavour hsc_lang
-            output_fn <- phaseOutputFilename next_phase
-            PipeState {hsc_env = hsc_env'} <- getPipeState
-            (outputFilename, mStub, foreign_files, _stg, _cmmRaw) <-
-              liftIO $ hscGenHardCode' hsc_env' cgguts mod_summary output_fn
-            stub_o <- liftIO (mapM (compileStub hsc_env') mStub)
-            foreign_os <-
-              liftIO $ mapM (uncurry (compileForeign hsc_env')) foreign_files
-            setForeignOs (maybe [] return stub_o ++ foreign_os)
-            withIR
-              c
-              mod_summary
-              IR {core = cgguts, stg = _stg, cmmRaw = _cmmRaw}
-            pure (RealPhase next_phase, outputFilename)
-          else runPhase phase_plus input_fn dflags
-      _ -> runPhase phase_plus input_fn dflags
+     MonadIO m
+  => Compiler
+  -> m (PhasePlus -> FilePath -> DynFlags -> CompPipeline (PhasePlus, FilePath))
+runPhaseWithCompiler c =
+  liftIO $ do
+    first_run_modules <- newIORef emptyModuleSet
+    pure $ \phase_plus input_fn dflags ->
+      case phase_plus of
+        HscOut src_flavour _ (HscRecomp cgguts mod_summary@ModSummary {..}) -> do
+          first_run <-
+            liftIO $
+            atomicModifyIORef' first_run_modules $ \s ->
+              if elemModuleSet ms_mod s
+                then (delModuleSet s ms_mod, False)
+                else (extendModuleSet s ms_mod, True)
+          if first_run
+            then do
+              let hsc_lang = hscTarget dflags
+                  next_phase = hscPostBackendPhase dflags src_flavour hsc_lang
+              output_fn <- phaseOutputFilename next_phase
+              PipeState {hsc_env = hsc_env'} <- getPipeState
+              (outputFilename, mStub, foreign_files, _stg, _cmmRaw) <-
+                liftIO $ hscGenHardCode' hsc_env' cgguts mod_summary output_fn
+              stub_o <- liftIO (mapM (compileStub hsc_env') mStub)
+              foreign_os <-
+                liftIO $ mapM (uncurry (compileForeign hsc_env')) foreign_files
+              setForeignOs (maybe [] return stub_o ++ foreign_os)
+              withIR
+                c
+                mod_summary
+                IR {core = cgguts, stg = _stg, cmmRaw = _cmmRaw}
+              pure (RealPhase next_phase, outputFilename)
+            else runPhase phase_plus input_fn dflags
+        _ -> runPhase phase_plus input_fn dflags
---
+++ ghc-toolkit/src/Language/Haskell/GHC/Toolkit/Run.hs
@@ -0,0 +1,70 @@
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE UnboxedTuples #-}
+
+module Language.Haskell.GHC.Toolkit.Run
+  ( Config(..)
+  , defaultConfig
+  , run
+  ) where
+
+import CmdLineParser
+import Control.Monad.IO.Class
+import Data.Functor
+import Data.IORef
+import qualified Data.Map.Strict as M
+import DynFlags
+import GHC
+import Hooks
+import Language.Haskell.GHC.Toolkit.BuildInfo
+import Language.Haskell.GHC.Toolkit.Compiler
+import Language.Haskell.GHC.Toolkit.Hooks
+import Panic
+
+newtype Config = Config
+  { ghcFlags :: [String]
+  }
+
+defaultConfig :: Config
+defaultConfig =
+  Config {ghcFlags = ["-Wall", "-O2", "-no-keep-hi-files", "-no-keep-o-files"]}
+
+run :: MonadIO m => Config -> [String] -> m (M.Map Module IR)
+run Config {..} targets =
+  liftIO $
+  defaultErrorHandler defaultFatalMessager defaultFlushOut $
+  runGhc (Just ghcLibDir) $ do
+    dflags <- getSessionDynFlags
+    (dflags', lefts, warns) <- parseDynamicFlags dflags $ map noLoc ghcFlags
+    case (# lefts, warns #) of
+      (# [], [] #) -> do
+        (rp, read_mod_map) <-
+          liftIO $ do
+            mod_map_ref <- newIORef M.empty
+            rp <-
+              runPhaseWithCompiler $
+              Compiler $ \ModSummary {..} ir ->
+                liftIO $
+                atomicModifyIORef' mod_map_ref $ \mod_map ->
+                  (M.insert ms_mod ir mod_map, ())
+            pure (rp, liftIO $ readIORef mod_map_ref)
+        void $
+          setSessionDynFlags
+            dflags'
+              { ghcMode = CompManager
+              , hooks = emptyHooks {runPhaseHook = Just rp}
+              }
+        traverse (`guessTarget` Nothing) targets >>= setTargets
+        ok_flag <- load LoadAllTargets
+        case ok_flag of
+          Succeeded -> read_mod_map
+          Failed ->
+            liftIO $ throwGhcExceptionIO $ Panic "GHC.load returned Failed."
+      _ ->
+        liftIO $
+        throwGhcExceptionIO $
+        Panic $
+        "When parsing ghc options: " <> show ghcFlags <>
+        ", leftover arguments: " <>
+        show (map unLoc lefts) <>
+        ", warnings: " <>
+        show (map (unLoc . warnMsg) warns)
