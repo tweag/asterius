61328ba97dfed152505015743e633b7ffa3bb7a7
Shao Cheng <astrohavoc@gmail.com>
2018-03-21 22:00:24 +0800

    Implement ObjectStore.

--- asterius/src/Asterius/FrontendPlugin.hs
+++ asterius/src/Asterius/FrontendPlugin.hs
@@ -5,38 +5,35 @@ module Asterius.FrontendPlugin
   ( frontendPlugin
   ) where
 
-import Data.Foldable
-import Data.List.Extra
 import GHC
 import GhcPlugins
 import Language.Haskell.GHC.Toolkit.Compiler
 import Language.Haskell.GHC.Toolkit.FrontendPlugin
+import Language.Haskell.GHC.Toolkit.ObjectStore
 import Language.Haskell.GHC.Toolkit.Orphans.Show ()
-import System.Directory
-import System.FilePath
+import System.Environment
 import Text.Show.Pretty
-import UnliftIO.Environment
 
 frontendPlugin :: FrontendPlugin
 frontendPlugin =
   frontendPluginFromCompiler $ do
-    obj_topdir <- getEnv "ASTERIUS_LIB_DIR"
+    Store {..} <-
+      liftIO $ do
+        obj_topdir <- getEnv "ASTERIUS_LIB_DIR"
+        newStore
+          StoreConfig
+            { storeTopDir = obj_topdir
+            , objectExt = "ddump-cmm-raw-ast"
+            , cacheObject = True
+            , rawRead = readFile
+            , rawWrite = writeFile
+            }
     pure $
       defaultCompiler
         { withIR =
-            \ModSummary {ms_mod = Module {..}} IR {..} ->
+            \ModSummary {..} IR {..} ->
               liftIO $ do
-                let obj_fn ext =
-                      obj_topdir </> unitIdString moduleUnitId </>
-                      foldr1
-                        (</>)
-                        (wordsBy (== '.') (moduleNameString moduleName)) <.>
-                      ext
-                createDirectoryIfMissing True $ takeDirectory $ obj_fn ""
-                for_
-                  [ ("simpl", ppShow core)
-                  , ("stg", ppShow stg)
-                  , ("cmm", ppShow cmm)
-                  , ("cmm-raw", ppShow cmmRaw)
-                  ] $ \(e, s) -> writeFile (obj_fn $ "dump-" ++ e ++ "-ast") s
+                objectWrite ms_mod $ ppShow cmmRaw
+                s <- objectRead ms_mod
+                print (ms_mod, length s)
         }
---
+++ ghc-toolkit/src/Language/Haskell/GHC/Toolkit/ObjectStore.hs
@@ -0,0 +1,66 @@
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE StrictData #-}
+
+module Language.Haskell.GHC.Toolkit.ObjectStore
+  ( StoreConfig(..)
+  , Store(..)
+  , newStore
+  ) where
+
+import Control.Monad.IO.Class
+import Data.IORef
+import qualified Data.Map.Strict as M
+import Module
+import System.Directory
+import System.FilePath
+
+data StoreConfig m a = StoreConfig
+  { storeTopDir, objectExt :: FilePath
+  , cacheObject :: Bool
+  , rawRead :: FilePath -> m a
+  , rawWrite :: FilePath -> a -> m ()
+  }
+
+data Store m a = Store
+  { objectRead :: Module -> m a
+  , objectWrite :: Module -> a -> m ()
+  }
+
+newStore :: MonadIO m => StoreConfig m a -> m (Store m a)
+newStore StoreConfig {..} =
+  if cacheObject
+    then do
+      obj_map <- liftIO $ newIORef M.empty
+      pure
+        Store
+          { objectRead =
+              \m -> do
+                om <- liftIO $ readIORef obj_map
+                case M.lookup m om of
+                  Just r -> pure r
+                  _ -> do
+                    r <- rawRead $ p m
+                    liftIO $
+                      atomicModifyIORef' obj_map $ \om' ->
+                        (M.insert m r om', ())
+                    pure r
+          , objectWrite = w
+          }
+    else pure Store {objectRead = rawRead . p, objectWrite = w}
+  where
+    w m a = do
+      let x = p m
+      liftIO $ createDirectoryIfMissing True $ takeDirectory x
+      rawWrite x a
+    p Module {..} =
+      storeTopDir </> unitIdString moduleUnitId </>
+      foldr1
+        (</>)
+        (foldr
+           (\case
+              '.' -> ([] :)
+              c -> \(x:xs) -> (c : x) : xs)
+           [[]]
+           (moduleNameString moduleName)) <.>
+      objectExt
