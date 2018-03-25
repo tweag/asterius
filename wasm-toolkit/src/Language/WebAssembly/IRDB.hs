b64b7a98748ab6a9b93b115098b71650d1c4a403
Shao Cheng <astrohavoc@gmail.com>
2018-03-25 19:02:31 +0800

    WIP sancheck

--- wasm-toolkit/src/Language/WebAssembly/IRDB.hs
+++ wasm-toolkit/src/Language/WebAssembly/IRDB.hs
@@ -47,6 +47,8 @@ data SanCheckError spec
                                 (StaticSymbol spec)
   | UnresolvedFunctionSymbolError (SanCheckErrorContext spec)
                                   (FunctionSymbol spec)
+  | UnresolvedEntryBlockError (SanCheckErrorContext spec)
+                              (FunctionSymbol spec)
 
 deriving instance
          ConstraintSymbolSpec Show spec => Show (SanCheckError spec)
@@ -57,10 +59,10 @@ addModule ::
   -> ModuleSymbol spec
   -> Module spec
   -> m (ModuleMap spec)
-addModule mm mod_name mod_rec = do
-  sanCheckStatics mm mod_name mod_rec
-  sanCheckFunctions mm mod_name mod_rec
-  pure ModuleMap {unModuleMap = HM.insert mod_name mod_rec $ unModuleMap mm}
+addModule mod_map mod_sym mod_rec = do
+  sanCheckStatics mod_map mod_sym mod_rec
+  sanCheckFunctions mod_map mod_sym mod_rec
+  pure ModuleMap {unModuleMap = HM.insert mod_sym mod_rec $ unModuleMap mod_map}
 
 sanCheckStatics ::
      (MonadError (SanCheckError spec) m, HashSymbolSpec spec)
@@ -68,9 +70,9 @@ sanCheckStatics ::
   -> ModuleSymbol spec
   -> Module spec
   -> m ()
-sanCheckStatics mm mod_name mod_rec =
+sanCheckStatics mod_map mod_sym mod_rec =
   for_ (HM.toList $ statics mod_rec) $
-  uncurry $ sanCheckStatic mm mod_name mod_rec
+  uncurry $ sanCheckStatic mod_map mod_sym mod_rec
 
 sanCheckStatic ::
      (MonadError (SanCheckError spec) m, HashSymbolSpec spec)
@@ -80,12 +82,12 @@ sanCheckStatic ::
   -> StaticSymbol spec
   -> Static spec
   -> m ()
-sanCheckStatic mm mod_name mod_rec static_name static_rec = do
-  let cxt = SanCheckErrorContext mm mod_name mod_rec
+sanCheckStatic mod_map mod_sym mod_rec static_name static_rec = do
+  let cxt = SanCheckErrorContext mod_map mod_sym mod_rec
   unless (align static_rec >= 1) $
     throwError $ StaticAlignmentError cxt static_name
   for_ (elements static_rec) $
-    sanCheckStaticElement mm mod_name mod_rec static_name
+    sanCheckStaticElement mod_map mod_sym mod_rec static_name
 
 sanCheckStaticElement ::
      (MonadError (SanCheckError spec) m, HashSymbolSpec spec)
@@ -95,13 +97,14 @@ sanCheckStaticElement ::
   -> StaticSymbol spec
   -> StaticElement spec
   -> m ()
-sanCheckStaticElement mm mod_name mod_rec static_name static_element = do
-  let cxt = SanCheckErrorContext mm mod_name mod_rec
+sanCheckStaticElement mod_map mod_sym mod_rec static_name static_element = do
+  let cxt = SanCheckErrorContext mod_map mod_sym mod_rec
   case static_element of
     Uninitialized size ->
       unless (size >= 0) $ throwError $ UninitializedStaticError cxt static_name
-    StaticSymbolElement sym -> sanCheckStaticSymbol mm mod_name mod_rec sym
-    FunctionSymbolElement sym -> sanCheckFunctionSymbol mm mod_name mod_rec sym
+    StaticSymbolElement sym -> sanCheckStaticSymbol mod_map mod_sym mod_rec sym
+    FunctionSymbolElement sym ->
+      sanCheckFunctionSymbol mod_map mod_sym mod_rec sym
     _ -> pure ()
 
 sanCheckFunctions ::
@@ -110,7 +113,82 @@ sanCheckFunctions ::
   -> ModuleSymbol spec
   -> Module spec
   -> m ()
-sanCheckFunctions = undefined
+sanCheckFunctions mod_map mod_sym mod_rec =
+  for_ (HM.toList $ functions mod_rec) $
+  uncurry $ sanCheckFunction mod_map mod_sym mod_rec
+
+sanCheckFunction ::
+     (MonadError (SanCheckError spec) m, HashSymbolSpec spec)
+  => ModuleMap spec
+  -> ModuleSymbol spec
+  -> Module spec
+  -> FunctionSymbol spec
+  -> Function spec
+  -> m ()
+sanCheckFunction mod_map mod_sym mod_rec func_sym func_rec = do
+  let cxt = SanCheckErrorContext mod_map mod_sym mod_rec
+  unless (HM.member (entryBlock func_rec) (blocks func_rec)) $
+    throwError $ UnresolvedEntryBlockError cxt func_sym
+  sanCheckBlocks mod_map mod_sym mod_rec func_sym func_rec
+
+sanCheckBlocks ::
+     (MonadError (SanCheckError spec) m, HashSymbolSpec spec)
+  => ModuleMap spec
+  -> ModuleSymbol spec
+  -> Module spec
+  -> FunctionSymbol spec
+  -> Function spec
+  -> m ()
+sanCheckBlocks mod_map mod_sym mod_rec func_sym func_rec =
+  for_ (HM.toList $ blocks func_rec) $
+  uncurry $ sanCheckBlock mod_map mod_sym mod_rec func_sym func_rec
+
+sanCheckBlock ::
+     (MonadError (SanCheckError spec) m, HashSymbolSpec spec)
+  => ModuleMap spec
+  -> ModuleSymbol spec
+  -> Module spec
+  -> FunctionSymbol spec
+  -> Function spec
+  -> BlockSymbol spec
+  -> Block spec
+  -> m ()
+sanCheckBlock mod_map mod_sym mod_rec func_sym func_rec block_sym block_rec = do
+  sanCheckExpression
+    mod_map
+    mod_sym
+    mod_rec
+    func_sym
+    func_rec
+    block_sym
+    block_rec
+    (body block_rec)
+  sanCheckBranch mod_map mod_sym mod_rec func_sym func_rec block_sym block_rec
+
+sanCheckExpression ::
+     (MonadError (SanCheckError spec) m, HashSymbolSpec spec)
+  => ModuleMap spec
+  -> ModuleSymbol spec
+  -> Module spec
+  -> FunctionSymbol spec
+  -> Function spec
+  -> BlockSymbol spec
+  -> Block spec
+  -> Expression spec
+  -> m ()
+sanCheckExpression _ _ _ _ _ _ _ _ = pure ()
+
+sanCheckBranch ::
+     (MonadError (SanCheckError spec) m, HashSymbolSpec spec)
+  => ModuleMap spec
+  -> ModuleSymbol spec
+  -> Module spec
+  -> FunctionSymbol spec
+  -> Function spec
+  -> BlockSymbol spec
+  -> Block spec
+  -> m ()
+sanCheckBranch _ _ _ _ _ _ _ = pure ()
 
 sanCheckStaticSymbol ::
      (MonadError (SanCheckError spec) m, HashSymbolSpec spec)
@@ -119,13 +197,13 @@ sanCheckStaticSymbol ::
   -> Module spec
   -> StaticSymbol spec
   -> m ()
-sanCheckStaticSymbol mm mod_name mod_rec static_sym =
+sanCheckStaticSymbol mod_map mod_sym mod_rec static_sym =
   unless
     (HM.member static_sym (statics mod_rec) ||
-     any (HM.member static_sym . statics) (unModuleMap mm)) $
+     any (HM.member static_sym . statics) (unModuleMap mod_map)) $
   throwError $
   UnresolvedStaticSymbolError
-    (SanCheckErrorContext mm mod_name mod_rec)
+    (SanCheckErrorContext mod_map mod_sym mod_rec)
     static_sym
 
 sanCheckFunctionSymbol ::
@@ -135,11 +213,11 @@ sanCheckFunctionSymbol ::
   -> Module spec
   -> FunctionSymbol spec
   -> m ()
-sanCheckFunctionSymbol mm mod_name mod_rec func_sym =
+sanCheckFunctionSymbol mod_map mod_sym mod_rec func_sym =
   unless
     (HM.member func_sym (functions mod_rec) ||
-     any (HM.member func_sym . functions) (unModuleMap mm)) $
+     any (HM.member func_sym . functions) (unModuleMap mod_map)) $
   throwError $
   UnresolvedFunctionSymbolError
-    (SanCheckErrorContext mm mod_name mod_rec)
+    (SanCheckErrorContext mod_map mod_sym mod_rec)
     func_sym
