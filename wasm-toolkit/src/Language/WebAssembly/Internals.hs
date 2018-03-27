62cfc7f24fe808cfa55d77c5cc862c1e814cc9c8
Shao Cheng <astrohavoc@gmail.com>
2018-03-27 11:14:36 +0800

    WIP, add peekSBS for serializing CmmLit

--- wasm-toolkit/src/Language/WebAssembly/Internals.hs
+++ wasm-toolkit/src/Language/WebAssembly/Internals.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE ConstraintKinds #-}
 {-# LANGUAGE MagicHash #-}
 {-# LANGUAGE UnboxedTuples #-}
@@ -5,34 +6,63 @@
 module Language.WebAssembly.Internals
   ( withSBS
   , withSV
+  , peekSBS
   ) where
 
-import qualified Data.ByteString.Short as SBS
 import qualified Data.ByteString.Short.Internal as SBS
 import qualified Data.Vector.Storable as SV
-import Foreign
-import Foreign.C
 import GHC.Exts
 import GHC.Types
+import UnliftIO
+import UnliftIO.Foreign
 
 {-# INLINEABLE withSBS #-}
-withSBS :: SBS.ShortByteString -> (Ptr CChar -> IO r) -> IO r
+withSBS :: MonadUnliftIO m => SBS.ShortByteString -> (Ptr CChar -> m r) -> m r
 withSBS (SBS.SBS ba) cont =
-  IO
-    (\s0 ->
-       case newPinnedByteArray# (l0 +# 1#) s0 of
-         (# s1, mba #) ->
-           case copyByteArray# ba 0# mba 0# l0 s1 of
-             s2 ->
-               case writeWord8Array# mba l0 0## s2 of
-                 s3 ->
-                   case unsafeFreezeByteArray# mba s3 of
-                     (# s4, ba' #) ->
-                       case cont (Ptr (byteArrayContents# ba')) of
-                         IO cf -> cf s4)
+  withRunInIO
+    (\u ->
+       (IO
+          (\s0 ->
+             case newPinnedByteArray# (l0 +# 1#) s0 of
+               (# s1, mba #) ->
+                 case copyByteArray# ba 0# mba 0# l0 s1 of
+                   s2 ->
+                     case writeWord8Array# mba l0 0## s2 of
+                       s3 ->
+                         case unsafeFreezeByteArray# mba s3 of
+                           (# s4, ba' #) ->
+                             case (u . cont) (Ptr (byteArrayContents# ba')) of
+                               IO cf -> cf s4)))
   where
     l0 = sizeofByteArray# ba
 
 {-# INLINEABLE withSV #-}
-withSV :: (Storable a, Num n) => SV.Vector a -> (Ptr a -> n -> IO r) -> IO r
-withSV v cont = SV.unsafeWith v (\p -> cont p (fromIntegral (SV.length v)))
+withSV ::
+     (MonadUnliftIO m, Storable a, Num n)
+  => SV.Vector a
+  -> (Ptr a -> n -> m r)
+  -> m r
+withSV v cont =
+  withRunInIO
+    (\u ->
+       SV.unsafeWith
+         v
+         (\p -> (\buf len -> u (cont buf len)) p (fromIntegral (SV.length v))))
+
+{-# INLINEABLE peekSBS #-}
+peekSBS :: (MonadIO m, Storable a) => a -> m SBS.ShortByteString
+peekSBS a =
+  liftIO
+    (alloca
+       (\buf@(Ptr addr) -> do
+          poke buf a
+          IO
+            (\s0 ->
+               case newByteArray# l s0 of
+                 (# s1, mba #) ->
+                   case copyAddrToByteArray# addr mba 0# l s1 of
+                     s2 ->
+                       case unsafeFreezeByteArray# mba s2 of
+                         (# s3, ba #) -> (# s3, SBS.SBS ba #))))
+  where
+    !(I# l) = sizeOf a
