90004ba4fe7addf8bef24a9de9ec062271e923c3
Shao Cheng <astrohavoc@gmail.com>
2018-03-31 17:29:17 +0800

    Use ld.gold in ahc-boot

--- asterius/test/ahc-boot.hs
+++ asterius/test/ahc-boot.hs
@@ -10,7 +10,9 @@ main = do
     BootArgs
       { bootDir = bootdir
       , configureOptions =
-          "--verbose --enable-split-sections --ghc-option=-keep-hc-files --ghc-option=-keep-s-files --ghc-option=-ddump-to-file --ghc-option=-ddump-asm --ghc-option=-ddump-cmm-raw --ghc-option=-ddump-cmm --ghc-option=-ddump-stg --ghc-option=-ddump-simpl"
+          "--with-ld=" ++
+          ("ld.gold" <.> exeExtension) ++
+          " --verbose --enable-split-sections --ghc-option=-keep-hc-files --ghc-option=-keep-s-files --ghc-option=-ddump-to-file --ghc-option=-ddump-asm --ghc-option=-ddump-cmm-raw --ghc-option=-ddump-cmm --ghc-option=-ddump-stg --ghc-option=-ddump-simpl"
       , buildOptions = ""
       , installOptions = ""
       }
