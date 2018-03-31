561fef5b4600e47d02d056f856f36230948ca0c3
Shao Cheng <astrohavoc@gmail.com>
2018-03-31 18:14:28 +0800

    Fix ahc-boot

--- asterius/test/ahc-boot.hs
+++ asterius/test/ahc-boot.hs
@@ -1,4 +1,5 @@
 import Asterius.Boot
+import Distribution.System
 import System.Directory
 import System.FilePath
 
@@ -10,9 +11,10 @@ main = do
     BootArgs
       { bootDir = bootdir
       , configureOptions =
-          "--with-ld=" ++
-          ("ld.gold" <.> exeExtension) ++
-          " --verbose --enable-split-sections --ghc-option=-keep-hc-files --ghc-option=-keep-s-files --ghc-option=-ddump-to-file --ghc-option=-ddump-asm --ghc-option=-ddump-cmm-raw --ghc-option=-ddump-cmm --ghc-option=-ddump-stg --ghc-option=-ddump-simpl"
+          (case buildOS of
+             Windows -> "--enable-split-objs"
+             _ -> "--enable-split-sections") ++
+          " --verbose --ghc-option=-keep-hc-files --ghc-option=-keep-s-files --ghc-option=-ddump-to-file --ghc-option=-ddump-asm --ghc-option=-ddump-cmm-raw --ghc-option=-ddump-cmm --ghc-option=-ddump-stg --ghc-option=-ddump-simpl"
       , buildOptions = ""
       , installOptions = ""
       }
