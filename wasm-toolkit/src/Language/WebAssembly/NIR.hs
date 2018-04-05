7d3abcba8fb8a0c9fe38833ab89ba3358da9cccb
Shao Cheng <astrohavoc@gmail.com>
2018-04-05 23:16:15 +0800

    Add most unops/binops to NIR

--- wasm-toolkit/src/Language/WebAssembly/NIR.hs
+++ wasm-toolkit/src/Language/WebAssembly/NIR.hs
@@ -8,6 +8,8 @@
 module Language.WebAssembly.NIR
   ( ValueType(..)
   , FunctionType(..)
+  , UnaryOp(..)
+  , BinaryOp(..)
   , Expression(..)
   , Function(..)
   , marshalFunction
@@ -32,6 +34,8 @@ data ValueType
   | Auto
   deriving (Show, Generic, Data)
 
+instance Serialize ValueType
+
 data FunctionType = FunctionType
   { name :: SBS.ShortByteString
   , returnType :: ValueType
@@ -40,7 +44,138 @@ data FunctionType = FunctionType
 
 instance Serialize FunctionType
 
-instance Serialize ValueType
+data UnaryOp
+  = ClzInt32
+  | CtzInt32
+  | PopcntInt32
+  | NegFloat32
+  | AbsFloat32
+  | CeilFloat32
+  | FloorFloat32
+  | TruncFloat32
+  | NearestFloat32
+  | SqrtFloat32
+  | EqZInt32
+  | ClzInt64
+  | CtzInt64
+  | PopcntInt64
+  | NegFloat64
+  | AbsFloat64
+  | CeilFloat64
+  | FloorFloat64
+  | TruncFloat64
+  | NearestFloat64
+  | SqrtFloat64
+  | EqZInt64
+  | ExtendSInt32
+  | ExtendUInt32
+  | WrapInt64
+  | TruncSFloat32ToInt32
+  | TruncSFloat32ToInt64
+  | TruncUFloat32ToInt32
+  | TruncUFloat32ToInt64
+  | TruncSFloat64ToInt32
+  | TruncSFloat64ToInt64
+  | TruncUFloat64ToInt32
+  | TruncUFloat64ToInt64
+  | ReinterpretFloat32
+  | ReinterpretFloat64
+  | ConvertSInt32ToFloat32
+  | ConvertSInt32ToFloat64
+  | ConvertUInt32ToFloat32
+  | ConvertUInt32ToFloat64
+  | ConvertSInt64ToFloat32
+  | ConvertSInt64ToFloat64
+  | ConvertUInt64ToFloat32
+  | ConvertUInt64ToFloat64
+  | PromoteFloat32
+  | DemoteFloat64
+  | ReinterpretInt32
+  | ReinterpretInt64
+  deriving (Show, Generic, Data)
+
+instance Serialize UnaryOp
+
+data BinaryOp
+  = AddInt32
+  | SubInt32
+  | MulInt32
+  | DivSInt32
+  | DivUInt32
+  | RemSInt32
+  | RemUInt32
+  | AndInt32
+  | OrInt32
+  | XorInt32
+  | ShlInt32
+  | ShrUInt32
+  | ShrSInt32
+  | RotLInt32
+  | RotRInt32
+  | EqInt32
+  | NeInt32
+  | LtSInt32
+  | LtUInt32
+  | LeSInt32
+  | LeUInt32
+  | GtSInt32
+  | GtUInt32
+  | GeSInt32
+  | GeUInt32
+  | AddInt64
+  | SubInt64
+  | MulInt64
+  | DivSInt64
+  | DivUInt64
+  | RemSInt64
+  | RemUInt64
+  | AndInt64
+  | OrInt64
+  | XorInt64
+  | ShlInt64
+  | ShrUInt64
+  | ShrSInt64
+  | RotLInt64
+  | RotRInt64
+  | EqInt64
+  | NeInt64
+  | LtSInt64
+  | LtUInt64
+  | LeSInt64
+  | LeUInt64
+  | GtSInt64
+  | GtUInt64
+  | GeSInt64
+  | GeUInt64
+  | AddFloat32
+  | SubFloat32
+  | MulFloat32
+  | DivFloat32
+  | CopySignFloat32
+  | MinFloat32
+  | MaxFloat32
+  | EqFloat32
+  | NeFloat32
+  | LtFloat32
+  | LeFloat32
+  | GtFloat32
+  | GeFloat32
+  | AddFloat64
+  | SubFloat64
+  | MulFloat64
+  | DivFloat64
+  | CopySignFloat64
+  | MinFloat64
+  | MaxFloat64
+  | EqFloat64
+  | NeFloat64
+  | LtFloat64
+  | LeFloat64
+  | GtFloat64
+  | GeFloat64
+  deriving (Show, Generic, Data)
+
+instance Serialize BinaryOp
 
 data Expression
   = ConstI32 Int32
@@ -62,6 +197,10 @@ data Expression
            , condition, value :: Expression }
   | GetLocal { index :: BinaryenIndex
              , localType :: ValueType }
+  | Unary { unaryOp :: UnaryOp
+          , operand0 :: Expression }
+  | Binary { binaryOp :: BinaryOp
+           , operand0, operand1 :: Expression }
   | Return { value :: Expression }
   | Null
   deriving (Show, Generic, Data)
@@ -86,6 +225,137 @@ marshalValueType t =
     F32 -> c_BinaryenTypeFloat32
     F64 -> c_BinaryenTypeFloat64
     Auto -> c_BinaryenTypeAuto
+
+marshalUnaryOp :: UnaryOp -> BinaryenOp
+marshalUnaryOp op =
+  case op of
+    ClzInt32 -> c_BinaryenClzInt32
+    CtzInt32 -> c_BinaryenCtzInt32
+    PopcntInt32 -> c_BinaryenPopcntInt32
+    NegFloat32 -> c_BinaryenNegFloat32
+    AbsFloat32 -> c_BinaryenAbsFloat32
+    CeilFloat32 -> c_BinaryenCeilFloat32
+    FloorFloat32 -> c_BinaryenFloorFloat32
+    TruncFloat32 -> c_BinaryenTruncFloat32
+    NearestFloat32 -> c_BinaryenNearestFloat32
+    SqrtFloat32 -> c_BinaryenSqrtFloat32
+    EqZInt32 -> c_BinaryenEqZInt32
+    ClzInt64 -> c_BinaryenClzInt64
+    CtzInt64 -> c_BinaryenCtzInt64
+    PopcntInt64 -> c_BinaryenPopcntInt64
+    NegFloat64 -> c_BinaryenNegFloat64
+    AbsFloat64 -> c_BinaryenAbsFloat64
+    CeilFloat64 -> c_BinaryenCeilFloat64
+    FloorFloat64 -> c_BinaryenFloorFloat64
+    TruncFloat64 -> c_BinaryenTruncFloat64
+    NearestFloat64 -> c_BinaryenNearestFloat64
+    SqrtFloat64 -> c_BinaryenSqrtFloat64
+    EqZInt64 -> c_BinaryenEqZInt64
+    ExtendSInt32 -> c_BinaryenExtendSInt32
+    ExtendUInt32 -> c_BinaryenExtendUInt32
+    WrapInt64 -> c_BinaryenWrapInt64
+    TruncSFloat32ToInt32 -> c_BinaryenTruncSFloat32ToInt32
+    TruncSFloat32ToInt64 -> c_BinaryenTruncSFloat32ToInt64
+    TruncUFloat32ToInt32 -> c_BinaryenTruncUFloat32ToInt32
+    TruncUFloat32ToInt64 -> c_BinaryenTruncUFloat32ToInt64
+    TruncSFloat64ToInt32 -> c_BinaryenTruncSFloat64ToInt32
+    TruncSFloat64ToInt64 -> c_BinaryenTruncSFloat64ToInt64
+    TruncUFloat64ToInt32 -> c_BinaryenTruncUFloat64ToInt32
+    TruncUFloat64ToInt64 -> c_BinaryenTruncUFloat64ToInt64
+    ReinterpretFloat32 -> c_BinaryenReinterpretFloat32
+    ReinterpretFloat64 -> c_BinaryenReinterpretFloat64
+    ConvertSInt32ToFloat32 -> c_BinaryenConvertSInt32ToFloat32
+    ConvertSInt32ToFloat64 -> c_BinaryenConvertSInt32ToFloat64
+    ConvertUInt32ToFloat32 -> c_BinaryenConvertUInt32ToFloat32
+    ConvertUInt32ToFloat64 -> c_BinaryenConvertUInt32ToFloat64
+    ConvertSInt64ToFloat32 -> c_BinaryenConvertSInt64ToFloat32
+    ConvertSInt64ToFloat64 -> c_BinaryenConvertSInt64ToFloat64
+    ConvertUInt64ToFloat32 -> c_BinaryenConvertUInt64ToFloat32
+    ConvertUInt64ToFloat64 -> c_BinaryenConvertUInt64ToFloat64
+    PromoteFloat32 -> c_BinaryenPromoteFloat32
+    DemoteFloat64 -> c_BinaryenDemoteFloat64
+    ReinterpretInt32 -> c_BinaryenReinterpretInt32
+    ReinterpretInt64 -> c_BinaryenReinterpretInt64
+
+marshalBinaryOp :: BinaryOp -> BinaryenOp
+marshalBinaryOp op =
+  case op of
+    AddInt32 -> c_BinaryenAddInt32
+    SubInt32 -> c_BinaryenSubInt32
+    MulInt32 -> c_BinaryenMulInt32
+    DivSInt32 -> c_BinaryenDivSInt32
+    DivUInt32 -> c_BinaryenDivUInt32
+    RemSInt32 -> c_BinaryenRemSInt32
+    RemUInt32 -> c_BinaryenRemUInt32
+    AndInt32 -> c_BinaryenAndInt32
+    OrInt32 -> c_BinaryenOrInt32
+    XorInt32 -> c_BinaryenXorInt32
+    ShlInt32 -> c_BinaryenShlInt32
+    ShrUInt32 -> c_BinaryenShrUInt32
+    ShrSInt32 -> c_BinaryenShrSInt32
+    RotLInt32 -> c_BinaryenRotLInt32
+    RotRInt32 -> c_BinaryenRotRInt32
+    EqInt32 -> c_BinaryenEqInt32
+    NeInt32 -> c_BinaryenNeInt32
+    LtSInt32 -> c_BinaryenLtSInt32
+    LtUInt32 -> c_BinaryenLtUInt32
+    LeSInt32 -> c_BinaryenLeSInt32
+    LeUInt32 -> c_BinaryenLeUInt32
+    GtSInt32 -> c_BinaryenGtSInt32
+    GtUInt32 -> c_BinaryenGtUInt32
+    GeSInt32 -> c_BinaryenGeSInt32
+    GeUInt32 -> c_BinaryenGeUInt32
+    AddInt64 -> c_BinaryenAddInt64
+    SubInt64 -> c_BinaryenSubInt64
+    MulInt64 -> c_BinaryenMulInt64
+    DivSInt64 -> c_BinaryenDivSInt64
+    DivUInt64 -> c_BinaryenDivUInt64
+    RemSInt64 -> c_BinaryenRemSInt64
+    RemUInt64 -> c_BinaryenRemUInt64
+    AndInt64 -> c_BinaryenAndInt64
+    OrInt64 -> c_BinaryenOrInt64
+    XorInt64 -> c_BinaryenXorInt64
+    ShlInt64 -> c_BinaryenShlInt64
+    ShrUInt64 -> c_BinaryenShrUInt64
+    ShrSInt64 -> c_BinaryenShrSInt64
+    RotLInt64 -> c_BinaryenRotLInt64
+    RotRInt64 -> c_BinaryenRotRInt64
+    EqInt64 -> c_BinaryenEqInt64
+    NeInt64 -> c_BinaryenNeInt64
+    LtSInt64 -> c_BinaryenLtSInt64
+    LtUInt64 -> c_BinaryenLtUInt64
+    LeSInt64 -> c_BinaryenLeSInt64
+    LeUInt64 -> c_BinaryenLeUInt64
+    GtSInt64 -> c_BinaryenGtSInt64
+    GtUInt64 -> c_BinaryenGtUInt64
+    GeSInt64 -> c_BinaryenGeSInt64
+    GeUInt64 -> c_BinaryenGeUInt64
+    AddFloat32 -> c_BinaryenAddFloat32
+    SubFloat32 -> c_BinaryenSubFloat32
+    MulFloat32 -> c_BinaryenMulFloat32
+    DivFloat32 -> c_BinaryenDivFloat32
+    CopySignFloat32 -> c_BinaryenCopySignFloat32
+    MinFloat32 -> c_BinaryenMinFloat32
+    MaxFloat32 -> c_BinaryenMaxFloat32
+    EqFloat32 -> c_BinaryenEqFloat32
+    NeFloat32 -> c_BinaryenNeFloat32
+    LtFloat32 -> c_BinaryenLtFloat32
+    LeFloat32 -> c_BinaryenLeFloat32
+    GtFloat32 -> c_BinaryenGtFloat32
+    GeFloat32 -> c_BinaryenGeFloat32
+    AddFloat64 -> c_BinaryenAddFloat64
+    SubFloat64 -> c_BinaryenSubFloat64
+    MulFloat64 -> c_BinaryenMulFloat64
+    DivFloat64 -> c_BinaryenDivFloat64
+    CopySignFloat64 -> c_BinaryenCopySignFloat64
+    MinFloat64 -> c_BinaryenMinFloat64
+    MaxFloat64 -> c_BinaryenMaxFloat64
+    EqFloat64 -> c_BinaryenEqFloat64
+    NeFloat64 -> c_BinaryenNeFloat64
+    LtFloat64 -> c_BinaryenLtFloat64
+    LeFloat64 -> c_BinaryenLeFloat64
+    GtFloat64 -> c_BinaryenGtFloat64
+    GeFloat64 -> c_BinaryenGeFloat64
 
 marshalFunctionType ::
      MonadIO m => BinaryenModuleRef -> FunctionType -> m BinaryenFunctionTypeRef
@@ -130,6 +400,13 @@ marshalExpression m e =
       withSV ns $ \nsp nl ->
         withSBS defaultName $ \dn -> c_BinaryenSwitch m nsp nl dn c v
     GetLocal {..} -> c_BinaryenGetLocal m index $ marshalValueType localType
+    Unary {..} -> do
+      x <- marshalExpression m operand0
+      c_BinaryenUnary m (marshalUnaryOp unaryOp) x
+    Binary {..} -> do
+      x <- marshalExpression m operand0
+      y <- marshalExpression m operand1
+      c_BinaryenBinary m (marshalBinaryOp binaryOp) x y
     Return {..} -> do
       v <- marshalExpression m value
       c_BinaryenReturn m v
--- wasm-toolkit/test/nir-test.hs
+++ wasm-toolkit/test/nir-test.hs
@@ -2,31 +2,8 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import Bindings.Binaryen.Raw
-import qualified Data.ByteString.Short as SBS
 import Data.Functor
-import Data.String
-import qualified Data.Vector as V
 import Language.WebAssembly.NIR
-
-maxn :: Int
-maxn = 2
-
-blockTag :: Int -> SBS.ShortByteString
-blockTag = ("block" <>) . fromString . show
-
-f :: Int -> Expression
-f 0 =
-  Block
-    (blockTag 0)
-    [ Switch
-        (V.fromList [blockTag i | i <- [0 .. maxn]])
-        "block_out"
-        (GetLocal 0 I32)
-        (ConstI32 233)
-    ]
-    Auto
-f n =
-  Block (blockTag n) [f $ n - 1, Return $ ConstI32 $ fromIntegral $ n - 1] Auto
 
 main :: IO ()
 main = do
@@ -34,6 +11,7 @@ main = do
   void $
     marshalFunction m $
     Function "func" (FunctionType "func_type" I32 [I32]) [] $
-    Block "block_out" [f maxn, Return $ ConstI32 $ fromIntegral maxn] Auto
+    Binary AddInt32 (GetLocal 0 I32) (GetLocal 0 I32)
+  c_BinaryenModulePrint m
   c_BinaryenModuleValidate m >>= print
   c_BinaryenModuleDispose m
