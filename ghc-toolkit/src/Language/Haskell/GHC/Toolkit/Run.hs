781a3fc611a7c24bf1a917f8a5127d906b99042e
Shao Cheng <astrohavoc@gmail.com>
2018-04-13 05:43:48 +0800

    No need to roundtrip via frontend plugin to retrieve cmm rep

--- ghc-toolkit/src/Language/Haskell/GHC/Toolkit/Run.hs
+++ ghc-toolkit/src/Language/Haskell/GHC/Toolkit/Run.hs
@@ -1,19 +1,22 @@
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE StrictData #-}
-{-# LANGUAGE UnboxedTuples #-}
 
 module Language.Haskell.GHC.Toolkit.Run
   ( Config
   , ghcFlags
+  , ghcLibDir
   , defaultConfig
-  , run
+  , runHaskell
+  , runCmm
   ) where
 
-import CmdLineParser
+import Control.Exception
 import Control.Monad.IO.Class
 import Data.Functor
 import Data.IORef
 import qualified Data.Map.Strict as M
+import DriverPhases
+import DriverPipeline
 import DynFlags
 import GHC
 import qualified Language.Haskell.GHC.Toolkit.BuildInfo as BI
@@ -39,41 +42,51 @@ defaultConfig =
     , ghcLibDir = BI.ghcLibDir
     }
 
-run :: MonadIO m => Config -> [String] -> m (M.Map Module HaskellIR)
-run Config {..} targets =
+runHaskell :: MonadIO m => Config -> [String] -> m (M.Map Module HaskellIR)
+runHaskell Config {..} targets =
   liftIO $
   defaultErrorHandler defaultFatalMessager defaultFlushOut $
   runGhc (Just ghcLibDir) $ do
     dflags <- getSessionDynFlags
-    (dflags', lefts, warns) <- parseDynamicFlags dflags $ map noLoc ghcFlags
-    case (# lefts, warns #) of
-      (# [], [] #) -> do
-        (h, read_mod_map) <-
-          liftIO $ do
-            mod_map_ref <- newIORef M.empty
-            h <-
-              hooksFromCompiler $
-              defaultCompiler
-                { withHaskellIR =
-                    \ModSummary {..} ir ->
-                      liftIO $
-                      atomicModifyIORef' mod_map_ref $ \mod_map ->
-                        (M.insert ms_mod ir mod_map, ())
-                }
-            pure (h, liftIO $ readIORef mod_map_ref)
-        void $ setSessionDynFlags dflags' {ghcMode = CompManager, hooks = h}
-        traverse (`guessTarget` Nothing) targets >>= setTargets
-        ok_flag <- load LoadAllTargets
-        case ok_flag of
-          Succeeded -> read_mod_map
-          Failed ->
-            liftIO $ throwGhcExceptionIO $ Panic "GHC.load returned Failed."
-      _ ->
-        liftIO $
-        throwGhcExceptionIO $
-        Panic $
-        "When parsing ghc options: " <> show ghcFlags <>
-        ", leftover arguments: " <>
-        show (map unLoc lefts) <>
-        ", warnings: " <>
-        show (map (unLoc . warnMsg) warns)
+    (dflags', _, _) <- parseDynamicFlags dflags $ map noLoc ghcFlags
+    (h, read_mod_map) <-
+      liftIO $ do
+        mod_map_ref <- newIORef M.empty
+        h <-
+          hooksFromCompiler $
+          defaultCompiler
+            { withHaskellIR =
+                \ModSummary {..} ir ->
+                  liftIO $
+                  atomicModifyIORef' mod_map_ref $ \mod_map ->
+                    (M.insert ms_mod ir mod_map, ())
+            }
+        pure (h, liftIO $ readIORef mod_map_ref)
+    void $ setSessionDynFlags dflags' {ghcMode = CompManager, hooks = h}
+    traverse (`guessTarget` Nothing) targets >>= setTargets
+    ok_flag <- load LoadAllTargets
+    case ok_flag of
+      Succeeded -> read_mod_map
+      Failed -> liftIO $ throwGhcExceptionIO $ Panic "GHC.load returned Failed."
+
+runCmm :: MonadIO m => Config -> String -> m CmmIR
+runCmm Config {..} cmm_fn =
+  liftIO $
+  defaultErrorHandler defaultFatalMessager defaultFlushOut $
+  runGhc (Just ghcLibDir) $ do
+    dflags <- getSessionDynFlags
+    (dflags', _, _) <- parseDynamicFlags dflags $ map noLoc ghcFlags
+    (h, read_cmm_ir) <-
+      liftIO $ do
+        cmm_ir_ref <- newIORef $ throw $ Panic "Failed to compile Cmm."
+        h <-
+          hooksFromCompiler $
+          defaultCompiler {withCmmIR = liftIO . writeIORef cmm_ir_ref}
+        pure (h, readIORef cmm_ir_ref)
+    void $
+      setSessionDynFlags
+        dflags' {ghcMode = OneShot, ghcLink = NoLink, hooks = h}
+    env <- getSession
+    liftIO $ do
+      oneShot env StopLn [(cmm_fn, Just CmmCpp)]
+      read_cmm_ir
