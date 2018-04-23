3b58175e8b6fd5e208f587b00c9a00354ef1339a
Shao Cheng <astrohavoc@gmail.com>
2018-04-23 20:35:53 +0800

    Include genapply into build script of ghc-toolkit, to properly generate AutoApply.cmm

--- ghc-toolkit/Setup.hs
+++ ghc-toolkit/Setup.hs
@@ -3,13 +3,15 @@
 
 import qualified Data.Map as M
 import Distribution.Simple
-import Distribution.Simple.BuildPaths
+import Distribution.Simple.BuildPaths hiding (exeExtension)
 import Distribution.Simple.LocalBuildInfo
 import Distribution.Types.BuildInfo
 import Distribution.Types.Library
 import Distribution.Types.PackageDescription
+import qualified Paths_genapply
 import System.Directory
 import System.FilePath
+import System.Process
 
 main :: IO ()
 main =
@@ -23,14 +25,20 @@ main =
             let [clbi@LibComponentLocalBuildInfo {componentUnitId = uid}] =
                   componentNameMap lbi M.! CLibName
                 amp = autogenComponentModulesDir lbi clbi
+                self_datadir =
+                  datadir $
+                  absoluteComponentInstallDirs pkg_descr lbi uid NoCopyDest
+                rts_datadir = self_datadir </> "boot-libs" </> "rts"
             createDirectoryIfMissing True amp
             writeFile (amp </> "BuildInfo_ghc_toolkit.hs") $
               "module BuildInfo_ghc_toolkit where\nghcLibDir :: FilePath\nghcLibDir = " ++
               show (m M.! "LibDir") ++
-              "\ndataDir :: FilePath\ndataDir = " ++
-              show
-                (datadir $
-                 absoluteComponentInstallDirs pkg_descr lbi uid NoCopyDest)
+              "\ndataDir :: FilePath\ndataDir = " ++ show self_datadir
+            genapply <-
+              (</> "genapply" <.> exeExtension) <$> Paths_genapply.getBinDir
+            autoapply <- readProcess genapply [] ""
+            createDirectoryIfMissing True rts_datadir
+            writeFile (rts_datadir </> "AutoApply.cmm") autoapply
             pure
               lbi
                 { localPkgDescr =
