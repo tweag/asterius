947f4f50edde88c037a37d8b205e2a9925a0f045
Shao Cheng <astrohavoc@gmail.com>
2018-07-11 16:48:11 +0800

    Fix jsffi test suite broken by previous commit

--- asterius/test/jsffi.hs
+++ asterius/test/jsffi.hs
@@ -2,4 +2,13 @@ import System.Process
 
 main :: IO ()
 main =
-  callProcess "ahc-link" ["--input", "test/jsffi/jsffi.hs", "--debug", "--run"]
+  callProcess
+    "ahc-link"
+    [ "--input"
+    , "test/jsffi/jsffi.hs"
+    , "--output-link-report"
+    , "test/jsffi/jsffi.link.txt"
+    , "--force"
+    , "--debug"
+    , "--run"
+    ]
