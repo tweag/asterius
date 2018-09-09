a2bdf09171fc300edac49abee3d886cd0f86e881
Shao Cheng <cheng.shao@tweag.io>
2018-09-10 06:28:50 +0800

    Implement makeHaskellCallback1; support targeting browsers in ahc-link

--- asterius/test/jsffi/jsffi.hs
+++ asterius/test/jsffi/jsffi.hs
@@ -21,6 +21,18 @@ foreign import javascript "false" js_false :: Bool
 
 foreign import javascript "true" js_true :: Bool
 
+foreign import javascript "__asterius_jsffi.makeHaskellCallback(${1})" js_make_hs_callback
+  :: StablePtr (IO ()) -> JSRef
+
+foreign import javascript "__asterius_jsffi.makeHaskellCallback1(${1})" js_make_hs_callback1
+  :: StablePtr (JSRef -> IO ()) -> JSRef
+
+foreign import javascript "setTimeout(${1},${2})" js_set_timeout
+  :: JSRef -> Int -> IO ()
+
+foreign import javascript "setTimeout(${1},${2},${3})" js_set_timeout1
+  :: JSRef -> Int -> JSRef -> IO ()
+
 foreign export javascript "mult_hs" (*) :: Int -> Int -> Int
 
 main :: IO ()
@@ -39,3 +51,7 @@ main = do
   js_print $ callJSObjectMethod json "parse" [toJSString "{}"]
   print_int $ fromEnum js_false
   print_int $ fromEnum js_true
+  io <- newStablePtr $ print_int 123456
+  js_set_timeout (js_make_hs_callback io) 1000
+  ev_io <- newStablePtr js_print
+  js_set_timeout1 (js_make_hs_callback1 ev_io) 2000 json
