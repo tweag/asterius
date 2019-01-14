0a25370b8abaad4156759e95ddec1b944a892538
Shao Cheng <cheng.shao@tweag.io>
2019-01-14 17:19:18 +0800

    Add reverse SPT in StablePtrManager for use of gc #52

--- asterius/rts/rts.stableptr.mjs
+++ asterius/rts/rts.stableptr.mjs
@@ -1,12 +1,24 @@
 export class StablePtrManager {
   constructor() {
-    this.spt = [];
-    Object.seal(this);
+    this.spt = new Map();
+    this.rev_spt = new Map();
+    Object.freeze(this);
   }
 
-  newStablePtr(addr) { return this.spt.push(addr) - 1; }
+  newStablePtr(addr) {
+    let sp = this.rev_spt.get(addr);
+    if (!sp) {
+      sp = this.spt.size + 1;
+      this.spt.set(sp, addr);
+      this.rev_spt.set(addr, sp);
+    }
+    return sp;
+  }
 
-  deRefStablePtr(sp) { return this.spt[sp]; }
+  deRefStablePtr(sp) { return this.spt.get(sp); }
 
-  freeStablePtr(sp) { delete this.spt[sp]; }
+  freeStablePtr(sp) {
+    this.rev_spt.delete(this.spt.get(sp));
+    this.spt.delete(sp);
+  }
 }
