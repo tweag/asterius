ff74196ec79b1035ab90a9f880e0ec2842786e8a
Shao Cheng <cheng.shao@tweag.io>
2019-02-13 12:18:24 +0800

    Improve arguments processing in ahc

--- ghc-toolkit/src/Language/Haskell/GHC/Toolkit/FakeGHC.hs
+++ ghc-toolkit/src/Language/Haskell/GHC/Toolkit/FakeGHC.hs
@@ -22,28 +22,27 @@ data FakeGHCOptions = FakeGHCOptions
 fakeGHCMain :: FakeGHCOptions -> IO ()
 fakeGHCMain FakeGHCOptions {..} = do
   args0 <- getArgs
-  case partition (== "--make") args0 of
-    ([], _) -> callProcess ghc args0
-    (_, args1) ->
-      let (minusB_args, args2) = partition ("-B" `isPrefixOf`) args1
-          new_ghc_libdir =
-            case minusB_args of
-              [minusB_arg] -> drop 2 minusB_arg
-              _ -> ghcLibDir
-       in GHC.defaultErrorHandler GHC.defaultFatalMessager GHC.defaultFlushOut $
-          GHC.runGhc (Just new_ghc_libdir) $ do
-            dflags0 <- GHC.getSessionDynFlags
-            (dflags1, fileish_args, _) <-
-              GHC.parseDynamicFlags dflags0 (map GHC.noLoc args2)
-            void $
-              GHC.setSessionDynFlags
-                dflags1
-                  { GHC.ghcMode = GHC.CompManager
-                  , GHC.ghcLink = GHC.NoLink
-                  , GHC.hscTarget = GHC.HscAsm
-                  , GHC.verbosity = 1
-                  }
-            GHC.frontend
-              frontendPlugin
-              []
-              [(GHC.unLoc m, Nothing) | m <- fileish_args]
+  let (minusB_args, args1) = partition ("-B" `isPrefixOf`) args0
+      new_ghc_libdir =
+        case minusB_args of
+          [minusB_arg] -> drop 2 minusB_arg
+          _ -> ghcLibDir
+  case partition (== "--make") args1 of
+    ([], _) -> callProcess ghc $ ("-B" <> new_ghc_libdir) : args1
+    (_, args2) ->
+      GHC.defaultErrorHandler GHC.defaultFatalMessager GHC.defaultFlushOut $
+      GHC.runGhc (Just new_ghc_libdir) $ do
+        dflags0 <- GHC.getSessionDynFlags
+        (dflags1, fileish_args, _) <-
+          GHC.parseDynamicFlags dflags0 (map GHC.noLoc args2)
+        void $
+          GHC.setSessionDynFlags
+            dflags1
+              { GHC.ghcMode = GHC.CompManager
+              , GHC.ghcLink = GHC.NoLink
+              , GHC.hscTarget = GHC.HscAsm
+              }
+        GHC.frontend
+          frontendPlugin
+          []
+          [(GHC.unLoc m, Nothing) | m <- fileish_args]
