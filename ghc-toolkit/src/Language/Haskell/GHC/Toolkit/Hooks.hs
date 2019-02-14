5872862e7769528c4e6fad17d6adb26b2e66d7c9
Shao Cheng <cheng.shao@tweag.io>
2019-02-14 10:03:05 +0800

    Assume all Haskell targets contain JSFFI clauses & remove redundant pipeline hacks

--- ghc-toolkit/src/Language/Haskell/GHC/Toolkit/Hooks.hs
+++ ghc-toolkit/src/Language/Haskell/GHC/Toolkit/Hooks.hs
@@ -1,7 +1,6 @@
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE TypeApplications #-}
 
 module Language.Haskell.GHC.Toolkit.Hooks
   ( hooksFromCompiler
@@ -9,15 +8,12 @@ module Language.Haskell.GHC.Toolkit.Hooks
 
 import qualified CmmInfo as GHC
 import Control.Concurrent
-import Control.Monad
 import Control.Monad.IO.Class
-import Data.Data (Data, gmapQl)
 import qualified Data.Map.Strict as Map
 import qualified Data.Set as Set
 import qualified DriverPhases as GHC
 import qualified DriverPipeline as GHC
 import qualified DynFlags as GHC
-import qualified ForeignCall as GHC
 import qualified Hooks as GHC
 import qualified HscMain as GHC
 import qualified HscTypes as GHC
@@ -26,16 +22,6 @@ import qualified Module as GHC
 import qualified PipelineMonad as GHC
 import qualified StgCmm as GHC
 import qualified Stream
-import Type.Reflection
-
-hasJSFFI :: Data a => a -> Bool
-hasJSFFI t =
-  case eqTypeRep (typeOf t) (typeRep @GHC.CCallConv) of
-    Just HRefl ->
-      case t of
-        GHC.JavaScriptCallConv -> True
-        _ -> False
-    _ -> gmapQl (||) False hasJSFFI t
 
 hooksFromCompiler :: Compiler -> IO GHC.Hooks
 hooksFromCompiler Compiler {..} = do
@@ -47,7 +33,6 @@ hooksFromCompiler Compiler {..} = do
   cmm_raw_map_ref <- newMVar Map.empty
   cmm_ref <- newEmptyMVar
   cmm_raw_ref <- newEmptyMVar
-  jsffi_set_ref <- newMVar Set.empty
   pure
     GHC.emptyHooks
       { GHC.tcRnModuleHook =
@@ -61,8 +46,6 @@ hooksFromCompiler Compiler {..} = do
             liftIO $ do
               store parsed_map_ref parsed_module
               store typechecked_map_ref typechecked_module
-              when (hasJSFFI $ GHC.hpm_module parsed_module_orig) $
-                modifyMVar_ jsffi_set_ref $ pure . Set.insert ms_mod
             pure typechecked_module
       , GHC.stgCmmHook =
           Just $ \dflags this_mod data_tycons cost_centre_info stg_binds hpc_info -> do
@@ -93,28 +76,16 @@ hooksFromCompiler Compiler {..} = do
             case phase of
               GHC.HscOut src_flavour _ (GHC.HscRecomp cgguts mod_summary@GHC.ModSummary {..}) -> do
                 r <-
-                  do skip_gcc <-
-                       liftIO $ do
-                         s <- readMVar jsffi_set_ref
-                         pure $ Set.member ms_mod s
-                     if skip_gcc
-                       then do
-                         output_fn <-
-                           GHC.phaseOutputFilename $
-                           GHC.hscPostBackendPhase dflags src_flavour $
-                           GHC.hscTarget dflags
-                         GHC.PipeState {GHC.hsc_env = hsc_env'} <-
-                           GHC.getPipeState
-                         (outputFilename, _, _) <-
-                           liftIO $
-                           GHC.hscGenHardCode
-                             hsc_env'
-                             cgguts
-                             mod_summary
-                             output_fn
-                         GHC.setForeignOs []
-                         pure (GHC.RealPhase GHC.StopLn, outputFilename)
-                       else GHC.runPhase phase input_fn dflags
+                  do output_fn <-
+                       GHC.phaseOutputFilename $
+                       GHC.hscPostBackendPhase dflags src_flavour $
+                       GHC.hscTarget dflags
+                     GHC.PipeState {GHC.hsc_env = hsc_env'} <- GHC.getPipeState
+                     (outputFilename, _, _) <-
+                       liftIO $
+                       GHC.hscGenHardCode hsc_env' cgguts mod_summary output_fn
+                     GHC.setForeignOs []
+                     pure (GHC.RealPhase GHC.StopLn, outputFilename)
                 f <-
                   liftIO $
                   modifyMVar mods_set_ref $ \s ->
