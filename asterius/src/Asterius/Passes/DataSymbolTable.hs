91b035b80a0c011451c4e62436c0a5a0ac09ee0b
Shao Cheng <cheng.shao@tweag.io>
2019-03-01 17:44:07 +0800

    Append adjacent data segments in the memory layout pass

--- asterius/src/Asterius/Passes/DataSymbolTable.hs
+++ asterius/src/Asterius/Passes/DataSymbolTable.hs
@@ -54,29 +54,38 @@ makeMemory AsteriusModule {..} sym_map last_addr =
     , memoryExportName = "memory"
     , dataSegments =
         Map.foldrWithKey'
-          (\statics_sym AsteriusStatics {..} statics_segs ->
+          (\statics_sym ss@AsteriusStatics {..} statics_segs ->
              fst $
-             foldl'
-               (\(static_segs, static_addr) static ->
-                  case static of
-                    SymbolStatic sym o ->
-                      ( DataSegment
-                          { content =
-                              encodeStorable $
-                              case Map.lookup sym sym_map of
-                                Just addr -> addr + fromIntegral o
-                                _ -> invalidSymbol
-                          , offset = static_addr
-                          } :
-                        static_segs
-                      , static_addr + 8)
-                    Uninitialized l ->
-                      (static_segs, static_addr + fromIntegral l)
-                    Serialized buf ->
-                      ( DataSegment {content = buf, offset = static_addr} :
-                        static_segs
-                      , static_addr + fromIntegral (SBS.length buf)))
-               (statics_segs, fromIntegral $ unTag $ sym_map ! statics_sym)
+             foldr'
+               (\static (static_segs, static_tail_addr) ->
+                  let flush_static_segs buf =
+                        ( case static_segs of
+                            DataSegment {..}:static_segs'
+                              | offset == static_tail_addr ->
+                                DataSegment
+                                  { content = buf <> content
+                                  , offset = static_addr
+                                  } :
+                                static_segs'
+                            _ ->
+                              DataSegment {content = buf, offset = static_addr} :
+                              static_segs
+                        , static_addr)
+                        where
+                          static_addr =
+                            static_tail_addr - fromIntegral (SBS.length buf)
+                   in case static of
+                        SymbolStatic sym o ->
+                          flush_static_segs $
+                          encodeStorable $
+                          case Map.lookup sym sym_map of
+                            Just addr -> addr + fromIntegral o
+                            _ -> invalidSymbol
+                        Uninitialized l ->
+                          (static_segs, static_tail_addr - fromIntegral l)
+                        Serialized buf -> flush_static_segs buf)
+               ( statics_segs
+               , fromIntegral $ unTag $ sym_map ! statics_sym + sizeofStatics ss)
                asteriusStatics)
           []
           staticsMap
