b83f325e5a2509c811e5405e77c7081cfb63745b
Shao Cheng <cheng.shao@tweag.io>
2019-03-04 18:16:35 +0800

    Implement a stock ahc-iserv

--- asterius/app/ahc-iserv.hs
+++ asterius/app/ahc-iserv.hs
@@ -1,2 +1,20 @@
+import Control.Exception
+import Data.IORef
+import GHCi.Message
+import GHCi.Signals
+import GHCi.Utils
+import Lib
+import System.Environment.Blank
+
 main :: IO ()
-main = error "Psst! Hey kid, wanna evaluate some splices?"
+main = do
+  arg0:arg1:_ <- getArgs
+  inh <- getGhcHandle $ read arg1
+  outh <- getGhcHandle $ read arg0
+  installSignalHandlers
+  lo_ref <- newIORef Nothing
+  uninterruptibleMask $
+    serv
+      True
+      pure
+      Pipe {pipeRead = inh, pipeWrite = outh, pipeLeftovers = lo_ref}
