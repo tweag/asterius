9883351b1a6fa01c358681e5ad941954049ed2ac
Shao Cheng <cheng.shao@tweag.io>
2019-03-06 23:32:38 +0800

    Compile a splice to raw cmm instead of going to ghci bytecode

--- asterius/src/Asterius/Iserv/CompileCoreExpr.hs
+++ asterius/src/Asterius/Iserv/CompileCoreExpr.hs
@@ -2,21 +2,27 @@ module Asterius.Iserv.CompileCoreExpr
   ( compileCoreExpr
   ) where
 
+import Asterius.Iserv.Trace
+import qualified CmmInfo as GHC
 import qualified CorePrep as GHC
 import qualified CoreSyn as GHC
 import qualified CoreTidy as GHC
 import qualified CoreToStg as GHC
 import qualified CoreUtils as GHC
+import qualified CostCentre as GHC
 import Data.IORef
 import Data.Tuple
 import qualified GHCi.RemoteTypes as GHC
+import qualified HscMain as GHC
 import qualified HscTypes as GHC
 import qualified Id as GHC
+import Language.Haskell.GHC.Toolkit.Orphans.Show ()
 import qualified Module as GHC
 import qualified Name as GHC
 import qualified SimplCore as GHC
 import qualified SimplStg as GHC
 import qualified SrcLoc as GHC
+import qualified Stream
 import qualified UniqSupply as GHC
 import Unsafe.Coerce
 import qualified VarEnv as GHC
@@ -33,11 +39,22 @@ compileCoreExpr us_ref hsc_env src_span ds_expr = do
   let tidy_expr = GHC.tidyExpr GHC.emptyTidyEnv simpl_expr
   prepd_expr <- GHC.corePrepExpr dflags hsc_env tidy_expr
   u <- atomicModifyIORef' us_ref $ swap . GHC.takeUniqFromSupply
-  let m = GHC.mkModule (GHC.stringToUnitId "asdf") (GHC.mkModuleName "ASDF")
+  let this_mod =
+        GHC.mkModule (GHC.stringToUnitId "asdf") (GHC.mkModuleName "ASDF")
       occ_n = GHC.mkVarOcc "asdf"
-      n = GHC.mkExternalName u m occ_n src_span
+      n = GHC.mkExternalName u this_mod occ_n src_span
       b = GHC.mkVanillaGlobal n (GHC.exprType ds_expr)
       prepd_binds = [GHC.NonRec b prepd_expr]
-      (stg_binds', _) = GHC.coreToStg dflags m prepd_binds
-  stg_binds <- GHC.stg2stg dflags m stg_binds'
+      (stg_binds, _) = GHC.coreToStg dflags this_mod prepd_binds
+  stg_binds2 <- GHC.stg2stg dflags this_mod stg_binds
+  cmms <-
+    GHC.doCodeGen
+      hsc_env
+      this_mod
+      []
+      GHC.emptyCollectedCCs
+      stg_binds2
+      (GHC.emptyHpcInfo False)
+  raw_cmms <- GHC.cmmToRawCmm dflags (Just this_mod) cmms >>= Stream.collect
+  trace True $ show raw_cmms
   GHC.mkForeignRef (unsafeCoerce $ GHC.RemotePtr 0) (pure ())
