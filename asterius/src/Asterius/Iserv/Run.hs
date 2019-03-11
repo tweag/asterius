44322a732ea33763fc44484118f6c3e9393e2411
Shao Cheng <cheng.shao@tweag.io>
2019-03-11 08:11:23 +0800

    Remove unnecessary iserv msg handlers since the hscCompileCoreExpr hack

--- asterius/src/Asterius/Iserv/Run.hs
+++ asterius/src/Asterius/Iserv/Run.hs
@@ -4,35 +4,14 @@ module Asterius.Iserv.Run
   ( run
   ) where
 
+import Control.Monad.Fail
 import Data.ByteString (ByteString)
-import qualified Data.ByteString as BS
 import Data.IORef
-import Data.Traversable
 import GHCi.Message
 import GHCi.RemoteTypes
 import qualified Language.Haskell.TH.Syntax as TH
+import Prelude hiding (fail)
 import Unsafe.Coerce
-
-initLinker :: IO ()
-initLinker = pure ()
-
-lookupSymbol :: String -> IO (Maybe (RemotePtr ()))
-lookupSymbol _ = pure $ Just $ RemotePtr 0
-
-loadArchive :: String -> IO ()
-loadArchive _ = pure ()
-
-addLibrarySearchPath :: String -> IO (RemotePtr ())
-addLibrarySearchPath _ = pure $ RemotePtr 0
-
-removeLibrarySearchPath :: RemotePtr () -> IO Bool
-removeLibrarySearchPath _ = pure True
-
-resolveObjs :: IO Bool
-resolveObjs = pure True
-
-mallocData :: ByteString -> IO (RemotePtr ())
-mallocData _ = pure $ RemotePtr 0
 
 startTH :: IO (RemoteRef (IORef QState))
 startTH = pure $ unsafeCoerce $ RemotePtr 0
@@ -51,14 +30,6 @@ runTH pipe _ _ _ _ = do
 run :: Pipe -> Message a -> IO a
 run pipe msg =
   case msg of
-    InitLinker -> initLinker
-    LookupSymbol str -> lookupSymbol str
-    LoadArchive str -> loadArchive str
-    AddLibrarySearchPath str -> addLibrarySearchPath str
-    RemoveLibrarySearchPath ptr -> removeLibrarySearchPath ptr
-    ResolveObjs -> resolveObjs
-    MallocData bs -> mallocData bs
-    MallocStrings bss -> for bss $ mallocData . (`BS.snoc` 0)
     StartTH -> startTH
     RunTH rstate rhv ty mb_loc -> runTH pipe rstate rhv ty mb_loc
-    _ -> undefined
+    _ -> fail $ "Asterius.Iserv.Run.run: unsupported message: " <> show msg
