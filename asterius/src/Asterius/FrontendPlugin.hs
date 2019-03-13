b6d4737b6fcffd9ea7f61d4b6f6a61013fa1d08d
Shao Cheng <cheng.shao@tweag.io>
2019-03-13 08:46:49 +0800

    Enable ghc linting flags when ASTERIUS_DEBUG is on

--- asterius/src/Asterius/FrontendPlugin.hs
+++ asterius/src/Asterius/FrontendPlugin.hs
@@ -24,58 +24,65 @@ import System.FilePath
 
 frontendPlugin :: GHC.FrontendPlugin
 frontendPlugin =
-  makeFrontendPlugin $
-  liftIO $ do
-    is_debug <- isJust <$> getEnv "ASTERIUS_DEBUG"
-    get_ffi_mod_ref <- newIORef $ error "get_ffi_mod_ref not initialized"
-    us_ref <- GHC.mkSplitUniqSupply 'A' >>= newIORef
-    (c, get_ffi_mod) <-
-      addFFIProcessor
-        mempty
-          { compileCoreExpr = Just $ Iserv.compileCoreExpr True us_ref
-          , withHaskellIR =
-              \GHC.ModSummary {..} ir@HaskellIR {..} obj_path -> do
-                dflags <- GHC.getDynFlags
-                setDynFlagsRef dflags
-                let mod_sym = marshalToModuleSymbol ms_mod
-                liftIO $
-                  case runCodeGen (marshalHaskellIR ms_mod ir) dflags ms_mod of
-                    Left err -> throwIO err
-                    Right m' -> do
-                      get_ffi_mod <- readIORef get_ffi_mod_ref
-                      ffi_mod <- get_ffi_mod mod_sym
-                      let m = ffi_mod <> m'
-                      encodeFile obj_path m
-                      when is_debug $ do
-                        let p = (obj_path -<.>)
-                        writeFile (p "dump-wasm-ast") $ show m
-                        writeFile (p "dump-cmm-raw-ast") $ show cmmRaw
-                        asmPrint dflags (p "dump-cmm-raw") cmmRaw
-                        writeFile (p "dump-cmm-ast") $ show cmm
-                        asmPrint dflags (p "dump-cmm") cmm
-                        writeFile (p "dump-stg-ast") $ show stg
-                        asmPrint dflags (p "dump-stg") stg
-                        writeFile (p "dump-core-ast") $ show core
-                        asmPrint dflags (p "dump-core") $ GHC.cg_binds core
-          , withCmmIR =
-              \ir@CmmIR {..} obj_path -> do
-                dflags <- GHC.getDynFlags
-                setDynFlagsRef dflags
-                let ms_mod =
-                      GHC.Module GHC.rtsUnitId $
-                      GHC.mkModuleName $ takeBaseName obj_path
-                liftIO $
-                  case runCodeGen (marshalCmmIR ms_mod ir) dflags ms_mod of
-                    Left err -> throwIO err
-                    Right m -> do
-                      encodeFile obj_path m
-                      when is_debug $ do
-                        let p = (obj_path -<.>)
-                        writeFile (p "dump-wasm-ast") $ show m
-                        writeFile (p "dump-cmm-raw-ast") $ show cmmRaw
-                        asmPrint dflags (p "dump-cmm-raw") cmmRaw
-                        writeFile (p "dump-cmm-ast") $ show cmm
-                        asmPrint dflags (p "dump-cmm") cmm
-          }
-    writeIORef get_ffi_mod_ref get_ffi_mod
-    pure c
+  makeFrontendPlugin $ do
+    is_debug <- liftIO $ isJust <$> getEnv "ASTERIUS_DEBUG"
+    when is_debug $ do
+      dflags <- GHC.getSessionDynFlags
+      void $
+        GHC.setSessionDynFlags $
+        dflags `GHC.gopt_set` GHC.Opt_DoCoreLinting `GHC.gopt_set`
+        GHC.Opt_DoStgLinting `GHC.gopt_set`
+        GHC.Opt_DoCmmLinting
+    liftIO $ do
+      get_ffi_mod_ref <- newIORef $ error "get_ffi_mod_ref not initialized"
+      us_ref <- GHC.mkSplitUniqSupply 'A' >>= newIORef
+      (c, get_ffi_mod) <-
+        addFFIProcessor
+          mempty
+            { compileCoreExpr = Just $ Iserv.compileCoreExpr True us_ref
+            , withHaskellIR =
+                \GHC.ModSummary {..} ir@HaskellIR {..} obj_path -> do
+                  dflags <- GHC.getDynFlags
+                  setDynFlagsRef dflags
+                  let mod_sym = marshalToModuleSymbol ms_mod
+                  liftIO $
+                    case runCodeGen (marshalHaskellIR ms_mod ir) dflags ms_mod of
+                      Left err -> throwIO err
+                      Right m' -> do
+                        get_ffi_mod <- readIORef get_ffi_mod_ref
+                        ffi_mod <- get_ffi_mod mod_sym
+                        let m = ffi_mod <> m'
+                        encodeFile obj_path m
+                        when is_debug $ do
+                          let p = (obj_path -<.>)
+                          writeFile (p "dump-wasm-ast") $ show m
+                          writeFile (p "dump-cmm-raw-ast") $ show cmmRaw
+                          asmPrint dflags (p "dump-cmm-raw") cmmRaw
+                          writeFile (p "dump-cmm-ast") $ show cmm
+                          asmPrint dflags (p "dump-cmm") cmm
+                          writeFile (p "dump-stg-ast") $ show stg
+                          asmPrint dflags (p "dump-stg") stg
+                          writeFile (p "dump-core-ast") $ show core
+                          asmPrint dflags (p "dump-core") $ GHC.cg_binds core
+            , withCmmIR =
+                \ir@CmmIR {..} obj_path -> do
+                  dflags <- GHC.getDynFlags
+                  setDynFlagsRef dflags
+                  let ms_mod =
+                        GHC.Module GHC.rtsUnitId $
+                        GHC.mkModuleName $ takeBaseName obj_path
+                  liftIO $
+                    case runCodeGen (marshalCmmIR ms_mod ir) dflags ms_mod of
+                      Left err -> throwIO err
+                      Right m -> do
+                        encodeFile obj_path m
+                        when is_debug $ do
+                          let p = (obj_path -<.>)
+                          writeFile (p "dump-wasm-ast") $ show m
+                          writeFile (p "dump-cmm-raw-ast") $ show cmmRaw
+                          asmPrint dflags (p "dump-cmm-raw") cmmRaw
+                          writeFile (p "dump-cmm-ast") $ show cmm
+                          asmPrint dflags (p "dump-cmm") cmm
+            }
+      writeIORef get_ffi_mod_ref get_ffi_mod
+      pure c
