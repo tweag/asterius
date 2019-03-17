c66fe565d24a449ac3c62b06d5f72fe9ceec60cb
Shao Cheng <cheng.shao@tweag.io>
2019-03-17 11:08:40 +0800

    Fix v8-node.py

--- utils/v8-node.py
+++ utils/v8-node.py
@@ -1,24 +1,32 @@
+#!/usr/bin/env python3
+
+from io import BytesIO
+from os import getcwd
 from html.parser import HTMLParser
 from urllib.request import urlopen
+from zipfile import ZipFile
 
 
-def get(url):
-    r = urlopen(url)
-    return r.read().decode(r.info().get_content_charset())
+def get_bytes(url):
+    return urlopen(url).read()
+
+
+def get_str(url):
+    return get_bytes(url).decode()
 
 
 class P0(HTMLParser):
     def __init__(self):
         super().__init__()
-        self.__gate = False
+        self.gate = False
         self.success_builds = []
 
     def handle_starttag(self, tag, attrs):
         if attrs == [("class", "status-Success")]:
-            self.__gate = True
-        elif self.__gate and tag == "a":
+            self.gate = True
+        elif self.gate and tag == "a":
             self.success_builds.append("https://ci.chromium.org" + attrs[0][1])
-            self.__gate = False
+            self.gate = False
 
 
 class P1(HTMLParser):
@@ -27,13 +35,23 @@ class P1(HTMLParser):
         self.download_link = None
 
     def handle_starttag(self, tag, attrs):
-        if len(attrs) >= 3 and attrs[2][1][-4:] == ".zip":
+        if len(attrs) >= 3 and attrs[2][1].startswith("https://storage.googleapis.com/") and attrs[2][1].endswith(
+                ".zip"):
             self.download_link = attrs[2][1]
 
 
-if __name__ == "__main__":
+def get_node_zip_url():
     p0 = P0()
-    p0.feed(get("https://ci.chromium.org/p/v8/builders/luci.v8.ci/V8%20Linux64%20-%20node.js%20integration%20ng"))
+    p0.feed(get_str("https://ci.chromium.org/p/v8/builders/luci.v8.ci/V8%20Linux64%20-%20node.js%20integration%20ng"))
     p1 = P1()
-    p1.feed(get(p0.success_builds[0]))
-    print(p1.download_link, end="")
+    p1.feed(get_str(p0.success_builds[0]))
+    return p1.download_link
+
+
+def extract_node_zip(node_zip_bytes, path):
+    with ZipFile(BytesIO(node_zip_bytes)) as z:
+        z.extractall(path)
+
+
+if __name__ == "__main__":
+    extract_node_zip(get_bytes(get_node_zip_url()), getcwd())
