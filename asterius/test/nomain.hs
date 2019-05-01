8040109ebf37d2fe1b775e6a548f9ed2a55ac312
Shao Cheng <cheng.shao@tweag.io>
2019-05-01 18:28:14 +0800

    WIP

--- asterius/test/nomain.hs
+++ asterius/test/nomain.hs
@@ -1,8 +1,19 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 import Asterius.JSRun.Main
+import Data.Binary.Get
 import qualified Data.ByteString.Lazy as LBS
+import Data.Word
 import Language.JavaScript.Inline.Core
 import System.Environment
 import System.Process
+
+evalWord :: JSSession -> JSCode -> IO Word32
+evalWord s expr = do
+  buf_val <- eval s "Buffer.allocUnsafe(4)"
+  () <- eval s $ deRefJSVal buf_val <> ".writeUInt32LE(" <> expr <> ")"
+  buf <- eval s $ takeJSVal buf_val
+  pure $ runGet getWord32host buf
 
 main :: IO ()
 main = do
@@ -10,6 +21,7 @@ main = do
   callProcess "ahc-link" $
     [ "--input-hs"
     , "test/nomain/NoMain.hs"
+    , "--full-sym-table"
     , "--ghc-option=-no-hs-main"
     , "--extra-root-symbol=NoMain_x_closure"
     ] <>
@@ -18,3 +30,9 @@ main = do
   withJSSession defJSSessionOpts $ \s -> do
     i <- newAsteriusInstance s "test/nomain/NoMain.lib.mjs" mod_buf
     hsInit s i
+    x <-
+      evalWord s $
+      deRefJSVal i <> ".wasmInstance.exports.rts_getInt(" <> deRefJSVal i <>
+      ".symbolTable.NoMain_x_closure" <>
+      ")"
+    print x
