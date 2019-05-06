4a9911c54ea241636af659158c387eeb54d80207
Shao Cheng <cheng.shao@tweag.io>
2019-05-06 15:52:26 +0800

    Redirect traces to tmp file instead of stdout to workaround cluttered output

--- asterius/src/Asterius/Iserv/Trace.hs
+++ asterius/src/Asterius/Iserv/Trace.hs
@@ -8,4 +8,4 @@ import Text.Printf
 
 trace :: Bool -> String -> IO ()
 trace verbose s =
-  when verbose $ getProgName >>= \name -> printf "[%20s] %s\n" name s
+  when verbose $ getProgName >>= \name -> appendFile "/tmp/trace.txt" $ printf "[%20s] %s\n" name s
