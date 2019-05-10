ec87615e51f400fd6a3d2a6f16d8ce6db155d828
Siddharth Bhat <siddharth.bath@tweag.io>
2019-05-10 16:38:00 +0200

    change to v2 of cheng's testcase

--- asterius/test/bytearraymini/bytearraymini.hs
+++ asterius/test/bytearraymini/bytearraymini.hs
@@ -14,22 +14,6 @@ main = do
         (# s1, mba #) ->
           case writeInt8Array# mba 0# 255# s1 of
             s2 ->
-              case writeInt8Array# mba 1# 255# s2 of
-                s3 ->
-                  case writeInt8Array# mba 2# 255# s3 of
-                    s4 ->
-                      case writeInt8Array# mba 3# 255# s4 of
-                        s5 ->
-                          case writeInt8Array# mba 4# 255# s5 of
-                            s6 ->
-                              case writeInt8Array# mba 5# 255# s6 of
-                                s7 ->
-                                  case writeInt8Array# mba 6# 255# s7 of
-                                    s8 ->
-                                      case writeInt8Array# mba 7# 255# s8 of
-                                        s9 ->
-                                          case unsafeFreezeByteArray# mba s9 of
-                                            (# s10, ba #) ->
-                                              (# s10
-                                               , I# (indexInt8Array# ba 0#)#)
+              case unsafeFreezeByteArray# mba s2 of
+                (# s10, ba #) -> (# s10, I# (indexInt8Array# ba 0#) #)
   print r
