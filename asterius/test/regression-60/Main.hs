370e41484b582c52df2aac52712613d7ebb2d9ea
Siddharth Bhat <siddharth.bath@tweag.io>
2019-05-14 17:48:18 +0200

    extend test case with test of read as well

--- asterius/test/regression-60/Main.hs
+++ asterius/test/regression-60/Main.hs
@@ -1,8 +1,17 @@
 module Main where
 import Data.Char
+import GHC.Unicode
 
 main :: IO ()
 main = do
-   let x = generalCategory '2'
+   -- We need to implement unicode primitives.
+   -- See implementation in eta:
+   -- https://github.com/typelead/eta/blob/8c1e23308b4b3951e651889f3639a38204c6d33d/libraries/base/java-utils/Utils.java#L97
+   putStrLn "testing wgencat..."
+   let x = wgencat $ ord '1'
    print x
+
+   putStrLn "testing read..."
+   let y = read "12" :: Int
+   print y
 
