9331b363918a272c94b84caf5678fc049df957f7
Hamish Mackenzie <Hamish.Mackenzie@iohk.io>
2019-05-17 15:29:12 +1200

    More nix boot-libs changes

--- nix/default.nix
+++ nix/default.nix
@@ -7,7 +7,11 @@
 let
   # our packages
   # plan-pkgs = import ./pkgs.nix;
-  plan-nix = haskell.cabalProjectToNix { src = ./..; };
+  plan-nix = haskell.cabalProjectToNix {
+    src = ./..;
+    ghc = pkgs.haskell.compiler.ghc864;
+    hackageIndexState = "2019-05-10T00:00:00Z";
+  };
   plan-pkgs = import "${plan-nix}/nix-plan/pkgs.nix";
 
   # Build the packageset with module support.
@@ -41,66 +45,78 @@ let
       })
     ];
   };
-  asterius-ghc-src-head = haskell.fetchExternal {
-    name     = "asterius-ghc-src";
-    specJSON = ./ghc-src.json;
-    override = "asterius-ghc-src";
-  };
-  ghc-prim = builtins.fetchTarball {
-    url = "http://hackage.haskell.org/package/ghc-prim-0.5.3/ghc-prim-0.5.3.tar.gz";
-    sha256 = "1inn9dr481bwddai9i2bbk50i8clzkn4452wgq4g97pcgdy1k8mn";
-  };
-  testPlan = haskell.cabalProjectToNix { src = ./..; };
-  asterius-ghc-src-this = pkgs.srcOnly pkgs.haskell.compiler.${compiler.nix-name};
-  asterius-libs-head = pkgs.copyPathToStore ../ghc-toolkit/boot-libs;
-  full-ghc-patch = pkgs.copyPathToStore ./patches/ghc/asterius.patch;
-  asterius-libs-patch = pkgs.runCommand "asterius-libs-patch" {
-    preferLocalBuild = true;
-  } ''
-    tmp=$(mktemp -d)
-    cd $tmp
-    ln -s ${asterius-ghc-src-head}/libraries old
-    ln -s ${asterius-libs-head} new
-    (diff -u -r old/ghc-prim new/ghc-prim | grep -v '^Only in' > $out) || true
+  # Patch file that can be applied to the full ghc tree
+  # full-ghc-patch = pkgs.copyPathToStore ./patches/ghc/asterius.patch;
+  ghc-head = let
+    # Only gitlab has the right submoudle refs (the ones in github mirror do not work)
+    # and only fetchgit seems to get the submoudles from gitlab
+    ghc-src = pkgs.fetchgit {
+      url = "https://gitlab.haskell.org/ghc/ghc";
+      rev = "bf6dbe3d1046573cb71fd534a326a9a0e6f1b220";
+      sha256 = "1hyq37vdicri96k05fm4w1ikdb87c430dh4aiy6ml9i3xzyxjdva";
+      fetchSubmodules = true;
+    };
+    # The patched libs are currently in the repo
+    boot-libs = pkgs.copyPathToStore ../ghc-toolkit/boot-libs;
+    # Derive the patch using diff
+    patch = pkgs.runCommand "asterius-libs-patch" {
+      preferLocalBuild = true;
+    } ''
+      tmp=$(mktemp -d)
+      cd $tmp
+      cp -r ${ghc-src}/libraries old
+      ln -s ${boot-libs} new
+      chmod +w -R old
+      mkdir -p old/rts/sm
+      cd new
+      find rts -type f -not -name rts.conf -exec cp ${ghc-src}/"{}" $tmp/old/"{}" \;
+      cd $tmp
+      for new in new/*; do
+        (diff -Nu -r old/$(basename $new) $new || true) >> $out
+      done
+    '';
+  in { inherit ghc-src boot-libs patch; };
+  ghc864 = let
+    ghc-src = pkgs.srcOnly pkgs.haskell.compiler.ghc864;
+    ghc-prim = builtins.fetchTarball {
+      url = "http://hackage.haskell.org/package/ghc-prim-0.5.3/ghc-prim-0.5.3.tar.gz";
+    };
+      sha256 = "1inn9dr481bwddai9i2bbk50i8clzkn4452wgq4g97pcgdy1k8mn";
+    patch = pkgs.copyPathToStore ./patches/ghc/ghc864-libs.patch;
+    ghc-patched-src = pkgs.runCommand "asterius-ghc864-ghc-patched-src" {
+      buildInputs = [];
+      preferLocalBuild = true;
+    } ''
+      set +x
+      cp -r ${ghc-src} $out
+      chmod +w -R $out
+      cd $out
+      cp -r rts libraries
+    '';
+    boot-libs = pkgs.runCommand "asterius-ghc864-boot-libs" {
+      buildInputs = [];
+      preferLocalBuild = true;
+    } ''
+      set +x
+      cp -r ${ghc-patched-src} $out
+      chmod +w -R $out
+      cd $out/libraries
+      patch -p1 < ${patch} || true
+      # TODO find a better way to get these
+      cp ${ghc-prim}/GHC/Prim.hs ghc-prim/GHC/Prim.hs
+      cp ${ghc-prim}/GHC/PrimopWrappers.hs ghc-prim/GHC/PrimopWrappers.hs
+      # TODO figure out a better way remove the unwanted stuff from ghc-prim.cabal
+      sed -i '96,$ d' ghc-prim/ghc-prim.cabal
   '';
-  asterius-boot-libs = pkgs.runCommand "asterius-boot-libs" {
-    buildInputs = [];
-    preferLocalBuild = true;
-  } ''
-    set +x
-    cp -r ${asterius-ghc-src-this} $out
-    chmod +w -R $out
-    cd $out
-    patch -p1 < ${full-ghc-patch} || true
-    cd $out/libraries
-    patch -p1 < ${asterius-libs-patch}
-    cp ${ghc-prim}/GHC/Prim.hs ghc-prim/GHC/Prim.hs
-    cp ${ghc-prim}/GHC/PrimopWrappers.hs ghc-prim/GHC/PrimopWrappers.hs
-    sed -i '96,$ d' ghc-prim/ghc-prim.cabal
-    mkdir ghc-prim/Asterius
-    cp ${asterius-libs-head}/ghc-prim/Asterius/Magic.hs ghc-prim/Asterius/Magic.hs
-    mv $out/rts $out/libraries
-    cat <<EOF >>$out/libraries/rts/rts.conf
-    name: rts
-    version: 1.0
-    id: rts
-    key: rts
-    license: BSD-3-Clause
-    maintainer: glasgow-haskell-users@haskell.org
-    exposed: True
-    library-dirs: \$topdir/rts
-    hs-libraries: HSrts
-    include-dirs: \$topdir/include
-    EOF
-  '';
+  in { inherit ghc-src ghc-prim ghc-patched-src boot-libs; };
 
 in
   pkgSet.config.hsPkgs // {
     _config = pkgSet.config;
-    inherit asterius-libs-patch testPlan;
+    inherit ghc-head ghc864 plan-nix;
     asterius-boot = pkgs.runCommand "asterius-boot" {
       preferLocalBuild = true;
-      nativeBuildInputs = [ pkgs.makeWrapper pkgs.haskell.compiler.${compiler.nix-name}  pkgs.autoconf  ];
+      nativeBuildInputs = [ pkgs.makeWrapper pkgs.haskell.compiler.${compiler.nix-name} pkgs.autoconf  ];
     } ''
       mkdir -p $out/bin
       mkdir -p $out/boot
@@ -108,7 +124,7 @@ in
         makeWrapper ${pkgSet.config.hsPkgs.asterius.components.exes.${exe}}/bin/${exe} $out/bin/${exe} \
           --set asterius_bindir $out/bin \
           --set asterius_bootdir $out/boot \
-          --set boot_libs_path ${asterius-boot-libs} \
+          --set boot_libs_path ${ghc864.boot-libs} \
           --set sandbox_ghc_lib_dir $(ghc --print-libdir)
       '') (pkgs.lib.attrNames pkgSet.config.hsPkgs.asterius.components.exes)}
       $out/bin/ahc-boot
