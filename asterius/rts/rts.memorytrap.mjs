026e471d6e1a87a8bce46e6221ffefe806aafac3
Shao Cheng <cheng.shao@tweag.io>
2019-05-18 23:50:02 +0800

    Implement precise read/write barriers

--- asterius/rts/rts.memorytrap.mjs
+++ asterius/rts/rts.memorytrap.mjs
@@ -2,10 +2,11 @@ import * as rtsConstants from "./rts.constants.mjs";
 import { Memory } from "./rts.memory.mjs";
 
 export class MemoryTrap {
-  constructor(logger, syms) {
+  constructor(logger, syms, memory) {
     this.logger = logger;
     this.symbolLookupTable = {};
     for (const[k, v] of Object.entries(syms)) this.symbolLookupTable[v] = k;
+    this.memory = memory;
     Object.freeze(this);
   }
 
@@ -21,101 +22,149 @@ export class MemoryTrap {
     }
   }
 
-  loadI8(p, o, v) {
+  loadI8(bp, o) {
+    /*
     this.logger.logInfo([
       "load", "i8", p, this.symbolLookupTable[p], o, v,
       this.symbolLookupTable[v]
     ]);
     this.trap(p, o);
+    */
+    const p = bp + BigInt(o);
+    return this.memory.i8Load(p);
   }
 
-  loadI16(p, o, v) {
+  loadI16(bp, o) {
+    /*
     this.logger.logInfo([
       "load", "i16", p, this.symbolLookupTable[p], o, v,
       this.symbolLookupTable[v]
     ]);
     this.trap(p, o);
+    */
+    const p = bp + BigInt(o);
+    return this.memory.i16Load(p);
   }
 
-  loadI32(p, o, v) {
+  loadI32(bp, o) {
+    /*
     this.logger.logInfo([
       "load", "i32", p, this.symbolLookupTable[p], o, v,
       this.symbolLookupTable[v]
     ]);
     this.trap(p, o);
+    */
+    const p = bp + BigInt(o);
+    return this.memory.i32Load(p);
   }
 
-  loadI64(p, o, v_lo, v_hi) {
+  loadI64(bp, o) {
+    /*
     const v = (BigInt(v_hi) << BigInt(32)) | BigInt(v_lo);
     this.logger.logInfo([
       "load", "i64", p, this.symbolLookupTable[p], o, v,
       this.symbolLookupTable[v]
     ]);
     this.trap(p, o);
+    */
+    const p = bp + BigInt(o);
+    return this.memory.i64Load(p);
   }
 
-  loadF32(p, o, v) {
+  loadF32(bp, o) {
+    /*
     this.logger.logInfo([
       "load", "f32", p, this.symbolLookupTable[p], o, v,
       this.symbolLookupTable[v]
     ]);
     this.trap(p, o);
+    */
+    const p = bp + BigInt(o);
+    return this.memory.f32Load(p);
   }
 
-  loadF64(p, o, v) {
+  loadF64(bp, o) {
+    /*
     this.logger.logInfo([
       "load", "f64", p, this.symbolLookupTable[p], o, v,
       this.symbolLookupTable[v]
     ]);
     this.trap(p, o);
+    */
+    const p = bp + BigInt(o);
+    return this.memory.f64Load(p);
   }
 
-  storeI8(p, o, v) {
+  storeI8(bp, o, v) {
+    /*
     this.logger.logInfo([
       "store", "i8", p, this.symbolLookupTable[p], o, v,
       this.symbolLookupTable[v]
     ]);
     this.trap(p, o);
+    */
+    const p = bp + BigInt(o);
+    this.memory.i8Store(p, v);
   }
 
-  storeI16(p, o, v) {
+  storeI16(bp, o, v) {
+    /*
     this.logger.logInfo([
       "store", "i16", p, this.symbolLookupTable[p], o, v,
       this.symbolLookupTable[v]
     ]);
     this.trap(p, o);
+    */
+    const p = bp + BigInt(o);
+    this.memory.i16Store(p, v);
   }
 
-  storeI32(p, o, v) {
+  storeI32(bp, o, v) {
+    /*
     this.logger.logInfo([
       "store", "i32", p, this.symbolLookupTable[p], o, v,
       this.symbolLookupTable[v]
     ]);
     this.trap(p, o);
+    */
+    const p = bp + BigInt(o);
+    this.memory.i32Store(p, v);
   }
 
-  storeI64(p, o, v_lo, v_hi) {
+  storeI64(bp, o, v) {
+    /*
     const v = (BigInt(v_hi) << BigInt(32)) | BigInt(v_lo);
     this.logger.logInfo([
       "store", "i64", p, this.symbolLookupTable[p], o, v,
       this.symbolLookupTable[v]
     ]);
     this.trap(p, o);
+    */
+    const p = bp + BigInt(o);
+    this.memory.i64Store(p, v);
   }
 
-  storeF32(p, o, v) {
+  storeF32(bp, o, v) {
+    /*
     this.logger.logInfo([
       "store", "f32", p, this.symbolLookupTable[p], o, v,
       this.symbolLookupTable[v]
     ]);
     this.trap(p, o);
+    */
+    const p = bp + BigInt(o);
+    this.memory.f32Store(p, v);
   }
 
-  storeF64(p, o, v) {
+  storeF64(bp, o, v) {
+    /*
     this.logger.logInfo([
       "store", "f64", p, this.symbolLookupTable[p], o, v,
       this.symbolLookupTable[v]
     ]);
     this.trap(p, o);
+    */
+    const p = bp + BigInt(o);
+    this.memory.f64Store(p, v);
   }
 }
--- asterius/src/Asterius/MemoryTrap.hs
+++ asterius/src/Asterius/MemoryTrap.hs
@@ -2,70 +2,56 @@
 {-# LANGUAGE OverloadedLists #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE TupleSections #-}
 
 module Asterius.MemoryTrap
   ( addMemoryTrap
-  , addMemoryTrapDeep
   ) where
 
 import Asterius.Types
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Short as SBS
-import Data.Data (Data, gmapM)
+import Data.Data (Data, gmapT)
 import qualified Data.Map.Strict as M
-import Data.Traversable
 import Type.Reflection
 
-addMemoryTrap :: Monad m => AsteriusModule -> m AsteriusModule
-addMemoryTrap m = do
-  new_function_map <-
-    fmap M.fromList $
-    for (M.toList $ functionMap m) $ \(func_sym, func) ->
-      if "__asterius" `BS.isPrefixOf` SBS.fromShort (entityName func_sym)
-        then pure (func_sym, func)
-        else (func_sym, ) <$> addMemoryTrapDeep func
-  pure m {functionMap = new_function_map}
+addMemoryTrap :: AsteriusModule -> AsteriusModule
+addMemoryTrap m =
+  let new_function_map =
+        flip M.mapWithKey (functionMap m) $ \func_sym func ->
+          if "__asterius" `BS.isPrefixOf` SBS.fromShort (entityName func_sym)
+            then func
+            else addMemoryTrapDeep func
+   in m {functionMap = new_function_map}
 
-addMemoryTrapDeep :: (Monad m, Data a) => a -> m a
+addMemoryTrapDeep :: Data a => a -> a
 addMemoryTrapDeep t =
   case eqTypeRep (typeOf t) (typeRep :: TypeRep Expression) of
     Just HRefl ->
       case t of
-        Load {ptr = Unary {unaryOp = WrapInt64, operand0 = i64_ptr}, ..} -> do
-          new_i64_ptr <- addMemoryTrapDeep i64_ptr
-          pure
-            Call
-              { target =
-                  AsteriusEntitySymbol
-                    { entityName =
-                        "__asterius_trap_load_" <> ty_name valueType bytes
-                    }
-              , operands = [new_i64_ptr, ConstI32 $ fromIntegral offset]
-              , callReturnTypes = [valueType]
-              }
-        Store {ptr = Unary {unaryOp = WrapInt64, operand0 = i64_ptr}, ..} -> do
-          new_i64_ptr <- addMemoryTrapDeep i64_ptr
-          new_value <- addMemoryTrapDeep value
-          pure
-            Call
-              { target =
-                  AsteriusEntitySymbol
-                    { entityName =
-                        "__asterius_trap_store_" <> ty_name valueType bytes
-                    }
-              , operands =
-                  [new_i64_ptr, ConstI32 $ fromIntegral offset, new_value]
-              , callReturnTypes = []
-              }
+        Load {ptr = Unary {unaryOp = WrapInt64, operand0 = i64_ptr}, ..} ->
+          let new_i64_ptr = addMemoryTrapDeep i64_ptr
+           in CallImport
+                { target' = "__asterius_load_" <> ty_name valueType bytes
+                , operands = [new_i64_ptr, ConstI32 $ fromIntegral offset]
+                , callImportReturnTypes = [valueType]
+                }
+        Store {ptr = Unary {unaryOp = WrapInt64, operand0 = i64_ptr}, ..} ->
+          let new_i64_ptr = addMemoryTrapDeep i64_ptr
+              new_value = addMemoryTrapDeep value
+           in CallImport
+                { target' = "__asterius_store_" <> ty_name valueType bytes
+                , operands =
+                    [new_i64_ptr, ConstI32 $ fromIntegral offset, new_value]
+                , callImportReturnTypes = []
+                }
         _ -> go
     _ -> go
   where
-    go = gmapM addMemoryTrapDeep t
-    ty_name I32 1 = "i8"
-    ty_name I32 2 = "i16"
-    ty_name I32 4 = "i32"
-    ty_name I64 8 = "i64"
-    ty_name F32 4 = "f32"
-    ty_name F64 8 = "f64"
+    go = gmapT addMemoryTrapDeep t
+    ty_name I32 1 = "I8"
+    ty_name I32 2 = "I16"
+    ty_name I32 4 = "I32"
+    ty_name I64 8 = "I64"
+    ty_name F32 4 = "F32"
+    ty_name F64 8 = "F64"
     ty_name _ _ = error "Asterius.MemoryTrap.addMemoryTrapDeep"
