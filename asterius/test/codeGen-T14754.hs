7929bb82b10f95f687c29efe4af2d888bcc602f4
Siddharth Bhat <siddharth.bath@tweag.io>
2019-05-21 14:32:48 +0200

    Kill spurious stuff from debugging branch

--- asterius/test/codeGen-T14754.hs
+++
@@ -1,8 +0,0 @@
-import System.Environment
-import System.Process
-
-main :: IO ()
-main = do
-  args <- getArgs
-  callProcess "ahc-link" $
-    ["--input-hs", "test/codegen-T14754/T14754.hs", "--run"] <> args
