923acef9e49fc5ca69709f6ebf390130b6caf0a5
Siddharth Bhat <siddharth.bath@tweag.io>
2019-05-27 14:54:22 +0200

    Add example output

--- asterius/test/sizeof_md5context/sizeof_md5context.hs
+++ asterius/test/sizeof_md5context/sizeof_md5context.hs
@@ -2,7 +2,11 @@
 
 #include "HsBaseConfig.h"
 import Control.Exception (assert)
+import GHC.Fingerprint
+import Control.Monad
 
 main :: IO ()
-main = assert (SIZEOF_STRUCT_MD5CONTEXT  == (88 :: Int)) (pure ())
+main = do
+  assert (SIZEOF_STRUCT_MD5CONTEXT  == (88 :: Int)) (pure ())
+  forM_ [1..10] $ \i -> print $ fingerprintString (replicate i ' ')
 
