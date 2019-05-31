95c72b90b41ac69b78d490de74abc972f3dbc383
Siddharth Bhat <siddharth.bath@tweag.io>
2019-05-31 16:34:38 +0200

    Add support for using the ws-64 testcase

--- asterius/test/ghc-testsuite.hs
+++ asterius/test/ghc-testsuite.hs
@@ -45,12 +45,14 @@ data TestCase = TestCase
   , caseStdIn, caseStdOut, caseStdErr :: LBS.ByteString
   } deriving (Show)
 
+-- | Try to read a file. if file does not exist, then return empty string.
 readFileNullable :: FilePath -> IO LBS.ByteString
 readFileNullable p = do
   exist <- doesFileExist p
   if exist
     then LBS.readFile p
     else pure LBS.empty
+
 
 getTestCases :: IO [TestCase]
 getTestCases = do
@@ -61,10 +63,16 @@ getTestCases = do
       let subroot = root </> subdir
       files <- sort <$> listDirectory subroot
       let cases = map (subroot </>) $ filter ((== ".hs") . takeExtension) files
-      for cases $ \c ->
+      for cases $ \c -> do
+        -- | GHC has some tests that differ for 32 and 64 bit architectures. So,
+        -- we first check if the 64 bit test exists. If it does, we always
+        -- use it. If it does not, we use the 32 bit test.
+        ws64exists <- doesFileExist (c -<.> "stdout-ws-64")
+        let stdoutp = c -<.> ("stdout" <>  if ws64exists then "-ws-64" else "")
+
         TestCase c <$> readFileNullable (c -<.> "stdin") <*>
-        readFileNullable (c -<.> "stdout") <*>
-        readFileNullable (c -<.> "stderr")
+          readFileNullable stdoutp <*>
+          readFileNullable (c -<.> "stderr")
 
 
 
