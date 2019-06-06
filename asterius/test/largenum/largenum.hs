d3e3bd4e7a6e4b070620963acf0e3bb9d38c42b8
Siddharth Bhat <siddharth.bath@tweag.io>
2019-06-06 15:39:44 +0200

    throw exception of the printing is incorrect

--- asterius/test/largenum/largenum.hs
+++ asterius/test/largenum/largenum.hs
@@ -7,6 +7,7 @@ import Control.Monad.State.Strict
 import qualified Data.IntMap.Strict as IM
 import GHC.Generics
 import System.Mem
+import Control.Exception (throwIO)
 import Debug.Trace (trace)
 
 main :: IO ()
@@ -14,3 +15,9 @@ main = do
     let w = (18446744073709551614 :: Word)
     print $ w
     print $ (fromIntegral w :: Integer)
+
+    if show (fromIntegral w) /=  show w
+    then throwIO . userError $ "cannot create Integer from large Word"
+    else pure ()
+
+
