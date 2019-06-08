9885fd516ba0dde43920512da2df930cd7bc3799
Shao Cheng <cheng.shao@tweag.io>
2019-06-08 09:55:56 +0800

    In a BlockChunk, always associate a regular instruction list with a safe ccall

--- asterius/src/Asterius/Passes/SafeCCall.hs
+++ asterius/src/Asterius/Passes/SafeCCall.hs
@@ -13,11 +13,14 @@ import qualified Data.ByteString as BS
 import qualified Data.ByteString.Short as SBS
 import Data.Foldable
 
-data BlockChunk
-  = Regular [Expression]
-  | SafeCCall AsteriusEntitySymbol
-              FFIImportDecl
-              Expression
+data SafeCCall =
+  SafeCCall AsteriusEntitySymbol
+            FFIImportDecl
+            Expression
+
+data BlockChunk =
+  BlockChunk [Expression]
+             (Maybe SafeCCall)
 
 splitBlockBody :: FFIMarshalState -> Expression -> [BlockChunk]
 splitBlockBody FFIMarshalState {..} instrs = foldr' w [] es
@@ -29,15 +32,18 @@ splitBlockBody FFIMarshalState {..} instrs = foldr' w [] es
           | ffiSafety imp_decl /= FFIUnsafe -> Just (k, imp_decl)
           where imp_decl = ffiImportDecls ! k
         _ -> Nothing
-    add_regular instr (Regular es':cs) = Regular (instr : es') : cs
-    add_regular instr cs = Regular [instr] : cs
+    add_regular instr (BlockChunk es' mcc:cs) =
+      BlockChunk (instr : es') mcc : cs
+    add_regular instr [] = [BlockChunk [instr] Nothing]
+    add_ccall k imp_decl instr cs =
+      BlockChunk [] (Just (SafeCCall k imp_decl instr)) : cs
     w instr@Call {..} cs =
       case get_safe target of
-        Just (k, imp_decl) -> SafeCCall k imp_decl instr : cs
+        Just (k, imp_decl) -> add_ccall k imp_decl instr cs
         _ -> add_regular instr cs
     w instr@UnresolvedSetLocal {value = Call {..}} cs =
       case get_safe target of
-        Just (k, imp_decl) -> SafeCCall k imp_decl instr : cs
+        Just (k, imp_decl) -> add_ccall k imp_decl instr cs
         _ -> add_regular instr cs
     w instr cs = add_regular instr cs
     es =
