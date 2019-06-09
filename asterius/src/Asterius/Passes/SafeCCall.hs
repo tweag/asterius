a1289bbcdd8607554a74e960913cf467e734cb22
Shao Cheng <cheng.shao@tweag.io>
2019-06-09 23:11:31 +0800

    WIP genContext

--- asterius/src/Asterius/Passes/SafeCCall.hs
+++ asterius/src/Asterius/Passes/SafeCCall.hs
@@ -3,8 +3,10 @@
 {-# LANGUAGE StrictData #-}
 
 module Asterius.Passes.SafeCCall
-  ( BlockChunk(..)
+  ( SafeCCall(..)
+  , BlockChunk(..)
   , splitBlockBody
+  , genContext
   ) where
 
 import Asterius.Internals
@@ -12,6 +14,7 @@ import Asterius.Types
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Short as SBS
 import Data.Foldable
+import Data.List
 
 data SafeCCall =
   SafeCCall AsteriusEntitySymbol
@@ -41,7 +44,7 @@ splitBlockBody FFIMarshalState {..} instrs = foldr' w [] es
       case get_safe target of
         Just (k, imp_decl) -> add_ccall k imp_decl instr cs
         _ -> add_regular instr cs
-    w instr@UnresolvedSetLocal {value = Call {..}} cs =
+    w instr@SetLocal {value = Call {..}} cs =
       case get_safe target of
         Just (k, imp_decl) -> add_ccall k imp_decl instr cs
         _ -> add_regular instr cs
@@ -51,3 +54,49 @@ splitBlockBody FFIMarshalState {..} instrs = foldr' w [] es
         Block {..} -> bodys
         Nop -> []
         _ -> [instrs]
+
+genContext ::
+     [ValueType] -> Expression -> ([Expression], Expression, [Expression])
+genContext vts e =
+  case e of
+    Call {} -> (save_instrs, e, load_instrs)
+      where I32:I32:ctx_regs = vts
+            (save_instrs, load_instrs) = gen_save_load $ zip [2 ..] ctx_regs
+    SetLocal {value = e'@Call {}, ..} -> (save_instrs, e', load_instrs)
+      where (I32:I32:ctx_regs_x, _:ctx_regs_y) = genericSplitAt index vts
+            ctx_regs = zip [2 ..] ctx_regs_x <> zip [succ index ..] ctx_regs_y
+            (save_instrs, load_instrs) = gen_save_load ctx_regs
+    _ -> error "Asterius.Passes.SafeCCall.genContext"
+  where
+    gen_save_load ctx_regs =
+      ( [ Store
+          { bytes = bs vt
+          , offset = 0
+          , ptr = pos p
+          , value = GetLocal {index = i, valueType = vt}
+          , valueType = vt
+          }
+        | (p, (i, vt)) <- p_ctx_regs
+        ]
+      , [ SetLocal
+          { index = i
+          , value =
+              Load
+                { signed = False
+                , bytes = bs vt
+                , offset = 0
+                , valueType = vt
+                , ptr = pos p
+                }
+          }
+        | (p, (i, vt)) <- p_ctx_regs
+        ])
+      where
+        p_ctx_regs = zip [0,8 ..] ctx_regs
+        pos p =
+          Symbol {unresolvedSymbol = "__asterius_context", symbolOffset = p}
+        bs vt =
+          case vt of
+            I32 -> 4
+            F32 -> 4
+            _ -> 8
