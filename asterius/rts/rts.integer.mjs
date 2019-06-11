1a14895073f3c4d3f4c668753c9d1e5ccee68bda
Siddharth Bhat <siddharth.bath@tweag.io>
2019-06-11 14:44:55 +0200

    Trying to implement quotrem, dying at type mismatch

--- asterius/rts/rts.integer.mjs
+++ asterius/rts/rts.integer.mjs
@@ -115,5 +115,40 @@ export class IntegerManager {
 
       return Number(val);
   }
+
+   quotrem2_quotient(lhs_hi_hi, lhs_hi_lo, lhs_lo_hi, lhs_lo_lo, rhs_hi, rhs_lo, ipiece) {
+      this.view.setInt32(/*offset=*/0, hi_lo, /*littleEndian=*/true);
+      this.view.setInt32(/*offset=*/4, hi_hi, /*littleEndian=*/true);
+      const hi = this.view.getBigUint64(/*offset=*/0, /*littleEndian=*/true);
+
+
+      this.view.setInt32(/*offset=*/0, lo_lo, /*littleEndian=*/true);
+      this.view.setInt32(/*offset=*/4, lo_hi, /*littleEndian=*/true);
+      const lo = this.view.getBigUint64(/*offset=*/0, /*littleEndian=*/true);
+      
+      const quot = hi / lo;
+      // find the correct value that is masked
+      const val =  Number((mul >> BigInt(32 * ipiece)) & ((BigInt(1) << BigInt(32)) - BigInt(1)));
+
+      return Number(val);
+   }
+
+    
+   quotrem2_remainder(hi_hi, hi_lo, lo_hi, lo_lo, ipiece) {
+      this.view.setInt32(/*offset=*/0, hi_lo, /*littleEndian=*/true);
+      this.view.setInt32(/*offset=*/4, hi_hi, /*littleEndian=*/true);
+      const hi = this.view.getBigUint64(/*offset=*/0, /*littleEndian=*/true);
+
+
+      this.view.setInt32(/*offset=*/0, lo_lo, /*littleEndian=*/true);
+      this.view.setInt32(/*offset=*/4, lo_hi, /*littleEndian=*/true);
+      const lo = this.view.getBigUint64(/*offset=*/0, /*littleEndian=*/true);
+      
+      const rem  = hi % lo;
+      // find the correct value that is masked
+      const val =  Number((mul >> BigInt(32 * ipiece)) & ((BigInt(1) << BigInt(32)) - BigInt(1)));
+
+      return Number(val);
+  }
   
 }
