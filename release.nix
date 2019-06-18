47eafd4925ad94aac36160c7c55c6f46bec3f473
Hamish Mackenzie <Hamish.Mackenzie@iohk.io>
2019-06-18 17:00:34 +1200

    Add a release.nix

---
+++ release.nix
@@ -0,0 +1,22 @@
+let
+  localLib = import ./nix/lib.nix;
+  disabled = [
+  ];
+in
+{ asterius ? { outPath = ./.; rev = "abcdef"; } ,... }@args:
+localLib.pkgs.lib.mapAttrsRecursiveCond
+(as: !(as ? "type" && as.type == "derivation"))
+(path: v: if (builtins.elem path disabled) then null else v)
+(localLib.nix-tools.release-nix {
+  _this = asterius;
+  package-set-path = ./.;
+
+  packages = [ "asterius" ];
+
+  required-name = "asterius-required-checks";
+  required-targets = jobs: [
+    jobs.nix-tools.libs.asterius.x86_64-darwin
+    jobs.nix-tools.libs.asterius.x86_64-linux
+  ];
+
+} (builtins.removeAttrs args ["asterius"]))
