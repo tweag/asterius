f89cce5d860c16971095c5975fd50cd65b3ceb9a
Siddharth Bhat <siddharth.bath@tweag.io>
2019-06-21 15:37:34 +0200

    checkpoint

--- asterius/rts/rts.stablename.mjs
+++ asterius/rts/rts.stablename.mjs
@@ -1,11 +1,13 @@
+// https://github.com/ghc/ghc/blob/bf73419518ca550e85188616f860961c7e2a336b/includes/rts/StableName.h
+// https://github.com/ghc/ghc/blob/43967c0c7d2d0110cfc5f9d64a7dab3a3dda8953/rts/StableName.c
 export class StableNameManager {
   constructor() {
     this.spt = new Map();
     Object.freeze(this);
   }
 
-  lookupStableName(ptr) {
-      return ptr;
+  makeStableName(obj) {
+      return obj;
   }
 
 }
