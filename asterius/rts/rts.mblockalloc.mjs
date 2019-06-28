e5bd47b91dfacd2256fc28018b44cd4cfd03f226
Siddharth Bhat <siddharth.bath@tweag.io>
2019-06-28 18:21:50 +0200

    Disable all free list stuff. Keep only the simple block allocation code.
    Right now, we die on an assert that the final spurious block descriptor we add
    does not really exist. I'm going to remove the final block desciptor to
    see if we pass. If we do, then there's something screwey going on in our
    final block descriptor handling. If not, then there's something wrong
    in our GC.

--- asterius/rts/rts.mblockalloc.mjs
+++ asterius/rts/rts.mblockalloc.mjs
@@ -7,7 +7,7 @@ export class MBlockAlloc {
     this.staticMBlocks = undefined;
     this.capacity = undefined;
     this.size = undefined;
-    this.freeList = [];
+    this.all_bds = [];
     Object.seal(this);
   }
 
@@ -38,14 +38,15 @@ export class MBlockAlloc {
 
   // allocate n megablocks contiguously.
   allocMegaGroup(n) {
-    // QUESTION: why do we subtract offset_first_block? isn't this doing a *size* calculation?
     const req_blocks = ((rtsConstants.mblock_size * n) - rtsConstants.offset_first_block) / rtsConstants.block_size;
 
+    /*
+    // QUESTION: why do we subtract offset_first_block? isn't this doing a *size* calculation?
 
     for (let i = 0; i < this.freeList.length; ++i) {
       // blocks = number of blocks in megablock??
-      const bd = this.freeList[i],
-            blocks = this.memory.i32Load(bd + rtsConstants.offset_bdescr_blocks);
+      const bd = this.freeList[i];
+      const blocks = this.memory.i32Load(bd + rtsConstants.offset_bdescr_blocks);
       
       if (req_blocks < blocks) {
         this.memory.i32Store(bd + rtsConstants.offset_bdescr_blocks, req_blocks);
@@ -63,17 +64,21 @@ export class MBlockAlloc {
         this.freeList.splice(i, 1, rest_bd);
         return bd;
       }
+      
       if (req_blocks == blocks) {
         this.freeList.splice(i, 1);
         return bd;
       }
 
-      // else??
-      throw new WebAssembly.RuntimeError("should never reach here, we should always be able to allocate enough blocks");
+
     }
-    const mblock = this.getMBlocks_(n),
-          bd = mblock + rtsConstants.offset_first_bdescr,
-          block_addr = mblock + rtsConstants.offset_first_block;
+    */
+    
+    const mblock = this.getMBlocks_(n);
+    const bd = mblock + rtsConstants.offset_first_bdescr;
+    this.all_bds.push(bd);
+    const block_addr = mblock + rtsConstants.offset_first_block;
+    
     this.memory.i64Store(bd + rtsConstants.offset_bdescr_start, block_addr);
     this.memory.i64Store(bd + rtsConstants.offset_bdescr_free, block_addr);
     this.memory.i64Store(bd + rtsConstants.offset_bdescr_link, 0);
@@ -82,30 +87,32 @@ export class MBlockAlloc {
   }
 
   // frees the segment between `l_end` and `r`, COUNTED IN BYTES
-  freeSegment(l_end, r) {
+  freeSegment(l_end, r, ix) {
     if (l_end < r) {
-      this.memory.memset(l_end, 0, r - l_end);
-      const bd = l_end + rtsConstants.offset_first_bdescr,
-            start = l_end + rtsConstants.offset_first_block,
-            blocks = (r - start) / rtsConstants.block_size;
-      this.memory.i64Store(bd + rtsConstants.offset_bdescr_start, start);
-      this.memory.i64Store(bd + rtsConstants.offset_bdescr_free, start);
-      this.memory.i64Store(bd + rtsConstants.offset_bdescr_link, 0);
-      this.memory.i32Store(bd + rtsConstants.offset_bdescr_blocks, blocks);
-      // push the block descriptor pointer into the freelist
-      this.freeList.push(bd);
+      // Change the memset to write 0x42 so we can see a clear pattern in the debug log.
+      this.memory.memset(l_end, 0x42 + ix, r - l_end);
+      // const bd = l_end + rtsConstants.offset_first_bdescr;
+      // const start = l_end + rtsConstants.offset_first_block;
+      // const blocks = (r - start) / rtsConstants.block_size;
+      // 
+      // this.memory.i64Store(bd + rtsConstants.offset_bdescr_start, start);
+      // this.memory.i64Store(bd + rtsConstants.offset_bdescr_free, start);
+      // this.memory.i64Store(bd + rtsConstants.offset_bdescr_link, 0);
+      // this.memory.i32Store(bd + rtsConstants.offset_bdescr_blocks, blocks);
+      // // push the block descriptor pointer into the freelist
+      // this.freeList.push(bd);
     }
   }
 
   // bds: List of live MegaBlocks
   preserveMegaGroups(bds) {
-    this.freeList = [];
     console.log("bds: ", bds);
     // sort in ascending order
     const sorted_bds = Array.from(bds).sort((bd0, bd1) => bd0 - bd1);
-    console.log("sorted_bds: ", sorted_bds);
 
     // add the block descriptor of the final block as well??
+    // we don't have this block descriptor on our list, when do we initialize
+    // this block descriptor??
     sorted_bds.push(Memory.tagData(rtsConstants.mblock_size * this.capacity) +
                     rtsConstants.offset_first_bdescr);
     
@@ -113,26 +120,47 @@ export class MBlockAlloc {
     //     Memory.tagData(rtsConstants.mblock_size * this.staticMBlocks),
     //   sorted_bds[0] - rtsConstants.offset_first_bdescr);
     // 
+    console.log("sorted_bds: ", sorted_bds);
+
+    console.log("all_bds: ", this.all_bds);
     for (let i = 0; i < (sorted_bds.length-1); ++i) {
-      console.log("hallo")
-      // const l_start = Number( this.memory.i64Load(sorted_bds[i] + rtsConstants.offset_bdescr_start));
-      const l_start = Number(this.memory.i64Load(sorted_bds[i] + rtsConstants.offset_bdescr_start));
+      
+      // ensure that we have actually created the block descriptor
+      const ix_in_all = this.all_bds.findIndex(function(v) { return v == sorted_bds[i]});
+      if (ix_in_all == -1) {
+        throw new WebAssembly.RuntimeError("unable to find block descriptor: " +
+                                           sorted_bds[i]);
+        
+      }
+      const ix2_in_all = this.all_bds.findIndex(function(v) { return v == sorted_bds[i+1]});
+      if (ix2_in_all == -1) {
+        throw new WebAssembly.RuntimeError("unable to find block descriptor2: " +
+                                           sorted_bds[i+1] + " i:"  + i);
+        
+      }
+
+      
+      // const l_start = Number( this.memory.i64Load(sorted_bds[i] +
+      // rtsConstants.offset_bdescr_start));
+      const l_start = Number(this.memory.i64Load(sorted_bds[i] +
+                                                 rtsConstants.offset_bdescr_start));
        
       const l_blocks =
-          this.memory.i32Load(sorted_bds[i] + rtsConstants.offset_bdescr_blocks);
+            this.memory.i32Load(sorted_bds[i] + rtsConstants.offset_bdescr_blocks);
       const l_end = l_start + (rtsConstants.block_size * l_blocks);
-      const r =  l_start + rtsConstants.block_size * rtsConstants.blocks_per_mblock;  
+      const r =  l_start + rtsConstants.block_size * rtsConstants.blocks_per_mblock;
+      
       const r_prev = sorted_bds[i + 1] - rtsConstants.offset_first_bdescr;
       
-      // if (i == 0) { continue; }
-      console.log("->>>> freeSegment: i: ", i, " l_start: " , l_start, " l_blocks: ", l_blocks, "l_end; ", 
-        l_end, " r: ", r, " r_prev: ", r_prev, "r_prev - r: ", r_prev - r);
-      this.freeSegment(l_end, r);
+      console.log("->>>> freeSegment: i: ", i,
+                  "bd: ", sorted_bds[i], " |bd[i+1]: ", sorted_bds[i+1],
+                  "\n\tix in all: ", ix_in_all,
+                  "\n\tl_start: " , l_start,
+                  " l_blocks: ", l_blocks, "l_end; ", 
+        l_end, "\n\tr: ", r, " r_prev: ", r_prev, "r_prev - r: ", r_prev - r);
+      // this.freeSegment(l_end, r_prev, i);
     }
 
-    // sort the freeList by number of blocks available.
-    this.freeList.sort(
-        (bd0, bd1) => this.memory.i32Load(bd0 + rtsConstants.offset_bdescr_blocks) -
-                  this.memory.i32Load(bd1 + rtsConstants.offset_bdescr_blocks));
+
   }
 }
