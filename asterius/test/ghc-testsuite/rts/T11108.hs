84117067009dc605bc60d48665ed586f63726039
Siddharth Bhat <siddharth.bath@tweag.io>
2019-07-03 11:42:46 +0200

    reducing

--- asterius/test/ghc-testsuite/rts/T11108.hs
+++ asterius/test/ghc-testsuite/rts/T11108.hs
@@ -1,19 +1,19 @@
 {-# LANGUAGE RecursiveDo, LambdaCase, BangPatterns #-}
-
+{-# LANGUAGE DeriveGeneric #-}
+import GHC.Generics (Generic, Generic1)
 import Control.Monad.Fix
 import Data.IORef
 import System.Mem.Weak
 import System.Mem
-
+import Control.Exception(evaluate)
+import Control.DeepSeq
 import Control.Monad
 import Data.Foldable
 
 data Pull = Pull
   { weakSelf  :: Weak Pull
   , compute :: Weak Pull -> IO Int
-  , invalidators :: IORef [Weak Pull]
-  , cached  :: IORef (Maybe Int)
-  }
+  } deriving(Generic)
 
 
 makePull :: (Weak Pull -> IO Int) -> IO Pull
@@ -21,8 +21,7 @@ makePull f = do
   rec
     -- This seems to be the culprit, changing the order makes the weakRef get gc'ed
     -- In this configuration it crashes
-
-    !foo <- Pull weak f <$> newIORef [] <*> newIORef Nothing
+    let !foo = Pull weak f
     weak <- mkWeakPtr foo Nothing
 
   return foo
@@ -30,26 +29,17 @@ makePull f = do
 
 invalidate :: Pull -> IO ()
 invalidate p = do
-  writeIORef (cached p) Nothing
-  invs <- readIORef (invalidators p)
-  writeIORef (invalidators p) []
-  traverse_ (deRefWeak >=> traverse_ invalidate) invs
+  return ()
 
 
 pull :: Weak Pull -> Pull -> IO Int
 pull weak p = do
-  modifyIORef (invalidators p) (weak :)
   pull' p
 
 pull' :: Pull -> IO Int
 pull' p = do
-  readIORef (cached p) >>= \case
-    Nothing -> do
       r <- compute p (weakSelf p)
-      writeIORef (cached p) (Just r)
       return r
-
-    Just r -> return r
 
 add :: Pull -> Int -> IO (Pull)
 add p n = makePull (\w -> (+n) <$> pull w p)
@@ -57,21 +47,12 @@ add p n = makePull (\w -> (+n) <$> pull w p)
 main = do
   h <- newIORef 0
 
+  performGC
+
   source <- makePull (const $ readIORef h)
-  p <- foldM add source (take 1000 (repeat 1))   -- 100 is not enough for crash
-
-  forM_ [1..10] $ \i -> do
-
-    writeIORef h i
-    invalidate source -- Crashes here on second iteration
-
-    --performGC
-    -- This avoids the crash
-
-    print =<< pull' p
-
-
-
+  p <- foldM add source (take 10000 (repeat 1))   -- 100 is not enough for crash
+  print =<< pull' p
+  performGC
 
 
 
