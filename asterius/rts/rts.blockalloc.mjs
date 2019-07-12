2b861a3df80e4c7a30ed17a1ecc4dbf3ee6ffc06
Siddharth Bhat <siddharth.bath@tweag.io>
2019-07-12 15:11:52 +0200

    add a small overview of how we are doing this, and links to relevant sections of the RTS

--- asterius/rts/rts.blockalloc.mjs
+++ asterius/rts/rts.blockalloc.mjs
@@ -3,6 +3,19 @@
 // https://gitlab.haskell.org/ghc/ghc/wikis/commentary/rts/storage/block-alloc
 // https://gitlab.haskell.org/ghc/ghc/blob/master/rts/sm/BlockAlloc.c
 // https://gitlab.haskell.org/ghc/ghc/blob/master/includes/rts/storage/Block.h
+//
+//
+// The algorithm described below is entirely from:
+// - BlockAlloc.c:allocGroup
+// - BlockAlloc.c:allocGroupOnNode
+// When we are asked for `n` blocks, here's the allocation strategy:
+// - IF the blocks DO NOT fit into a single megablock: 
+//     Allocate a new megagroup (a group of megablocks), setup the
+//     block descriptor and move on with life.
+// - OTHERWISE, the blocks DO fit in a single megablock:
+//     Look in the megablock freelist. If we find a megablock that
+//     can fit our blocks, allocate. If there are leftover blocks,
+//     add them back.
 import * as rtsConstants from "./rts.constants.mjs";
 import { Memory } from "./rts.memory.mjs";
 import * as assert from "assert";
@@ -110,68 +123,6 @@ export class BlockAlloc {
       console.log(`requested megablocks: ${req_mblocks}`);1
     }
     return bd;
-
-
-    /*
-    for (let i = 0; i < this.freeSegments.length; ++i) {
-      const [bd, r] = this.freeSegments[i];
-      assertBdescrAligned(bd);
-      const mblock = bd - rtsConstants.offset_first_bdescr;
-      assertMblockAligned(mblock);
-      const start = mblock + rtsConstants.offset_first_block;
-      const blocks = Math.floor((r - start) / rtsConstants.block_size);
-
-      if (req_blocks < blocks) {
-        // console.log("allocating from free segment");
-        this.memory.i32Store(bd + rtsConstants.offset_bdescr_blocks, req_blocks);
-        const rest_bd = bd + (rtsConstants.mblock_size * n);
-        assertBdescrAligned(rest_bd);
-        const rest_start = rest_bd - rtsConstants.offset_first_bdescr + rtsConstants.offset_first_block;
-        const rest_blocks = Math.floor((r - rest_start) / rtsConstants.block_size);
-        this.memory.i64Store(rest_bd + rtsConstants.offset_bdescr_start, rest_start);
-        this.memory.i64Store(rest_bd + rtsConstants.offset_bdescr_free, rest_start);
-        this.memory.i64Store(rest_bd + rtsConstants.offset_bdescr_link, 0);
-        // 0xDEADBEEF = 3735928559
-        this.memory.i32Store(rest_bd + rtsConstants.offset_bdescr_blocks, rest_blocks);
-        this.freeSegments.splice(i, 1, [rest_bd, r]);
-        // this.freeSegments.splice(i, 1);
-
-
-        // this.memory.i32Store(bd + rtsConstants.offset_bdescr_blocks, req_blocks);
-        // const rest_bd = bd + (rtsConstants.mblock_size * n),
-        //       rest_start = rest_bd - rtsConstants.offset_first_bdescr +
-        //                    rtsConstants.offset_first_block;
-        // this.memory.i64Store(rest_bd + rtsConstants.offset_bdescr_start,
-        //                      rest_start);
-        // this.memory.i64Store(rest_bd + rtsConstants.offset_bdescr_free, rest_start);
-        // this.memory.i64Store(rest_bd + rtsConstants.offset_bdescr_link, 0);
-        // this.memory.i32Store(rest_bd + rtsConstants.offset_bdescr_blocks,
-        //                      blocks - req_blocks -
-        //                          ((rtsConstants.mblock_size / rtsConstants.block_size) -
-        //                           rtsConstants.blocks_per_mblock));
-        // this.freeList.splice(i, 1, rest_bd);
-        return bd;
-      }
-
-      if (req_blocks == blocks) {
-        // console.log("allocating from free segment");
-        this.freeSegments.splice(i, 1);
-        return bd;
-      }
-
-    }
-    */
-        // console.log("allocating from new segment");
-        /*
-        */
-
-        // what we need fits inside a megablock.
-        if (req_mblocks <= 1) {
-          throw new WebAssembly.RuntimeError(`have not implemented how to allocate into a single megablock: ${req_mblocks}`);
-        } else {
-
-          throw new WebAssembly.RuntimeError(`have not implemented how to allocate into multiple megablocks: ${req_mblocks}`);
-        }
   }
 
   freeSegment(i, l_end, r) {
