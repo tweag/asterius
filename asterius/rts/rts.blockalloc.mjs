24ae10ae8a29e4c49a65c230c37f8bcc89411fe7
Siddharth Bhat <siddharth.bath@tweag.io>
2019-07-16 13:52:34 +0200

    fourth rewrite or something? decided to make this incremental as suggested

--- asterius/rts/rts.blockalloc.mjs
+++ asterius/rts/rts.blockalloc.mjs
@@ -5,18 +5,44 @@
 // https://gitlab.haskell.org/ghc/ghc/blob/master/includes/rts/storage/Block.h
 //
 //
-// The algorithm described below is entirely from:
-// - BlockAlloc.c:allocGroup
-// - BlockAlloc.c:allocGroupOnNode
-// When we are asked for `n` blocks, here's the allocation strategy:
-// - IF the blocks DO NOT fit into a single megablock: 
-//     Allocate a new megagroup (a group of megablocks), setup the
-//     block descriptor and move on with life.
-// - OTHERWISE, the blocks DO fit in a single megablock:
-//     Look in the megablock freelist. If we find a megablock that
-//     can fit our blocks, allocate. If there are leftover blocks,
-//     add them back.
+// First, some terminology:
+// megablock: large unit of allocation of *blocks*
+// block group: smallest unit of allocation. there is a "block descriptor", 
+//        and a chunk of memory which is the "block memory", which is allocated
+//        in multiple of `block_size.
+//        The block descriptor points to the block memory.
+//        Block descriptors of all blocks in a megablock are at the beginning
+//        of a megablock.
+// mega-group: collection of megablocks. Block descriptor for the mega
+//             group is in the first megablock.
 //
+//
+// Data structures:
+// ----------------
+//
+// We have a map of blocks and megablocks, mapping number of blocks/megablocks requested
+// to the free blocks/megablocks of that size.
+//
+// Allocation
+// ----------
+//
+// If allocation size is strictly less than a megablock (ie, it will fit in
+// a megablock):
+//     - Look through blockgroupfree for a block group of that size, and provide it
+//     - Split the block in the block free if there is extra space.
+//     - Otherwise, allocate a megablock, split the megablock into the
+//       allocation and the leftover free blocks.
+//
+//  If the allocation size is greater than a megablock:
+//    - Look through the megagroup free for a megablock of that size.
+//      If it does not exist, then create a new megagrup.
+//
+// Freeing
+// -------
+//
+// When we free things, we might find that during the process of freeing, we can
+// recover entire megablocks, and not just blocks. To find this, we will need
+// to coalesce adjacent blocks to check if they form a megablock.
 import * as rtsConstants from "./rts.constants.mjs";
 import { Memory } from "./rts.memory.mjs";
 import * as assert from "assert";
@@ -34,6 +60,12 @@ function ptr2bdescr(p) {
     // console.log(`BLOCK_MASK: ${BLOCK_MASK} | MBLOCK_MASK: ${MBLOCK_MASK} | BLOCK_SHIFT: ${BLOCK_SHIFT} | BDESCR_SHIFT: ${BDESCR_SHIFT}`);
     p = BigInt(p);
     return Number((((p) &  MBLOCK_MASK & ~BLOCK_MASK) >> (BLOCK_SHIFT-BDESCR_SHIFT)) | ((p) & ~MBLOCK_MASK));
+}
+
+// given a pointer, return the megablock to which it belongs to
+function ptr2mblock(p) {
+  const MBLOCK_MASK = BigInt(rtsConstants.mblock_size - 1);
+  return Number(p & MBLOCK_MASK);
 }
 
 // return the megablock a block descriptor belongs to
@@ -63,13 +95,15 @@ function assertBdescrAligned(bd) {
   assertMblockAligned(bd - rtsConstants.offset_first_bdescr);
 }
 
+
 export class BlockAlloc {
   constructor() {
     this.memory = undefined;
     this.staticMBlocks = undefined;
     this.capacity = undefined;
     this.size = undefined;
-    this.freeBlocks = [];
+    // contains (l, r) of the free block groups.
+    this.freeBlockGroups = [];
     Object.seal(this);
   }
 
@@ -92,92 +126,93 @@ export class BlockAlloc {
     return Memory.tagData(prev_size * rtsConstants.mblock_size);
   }
 
-
-  allocBlocks(req_blocks) {
-    const req_mblocks = Math.ceil(req_blocks / rtsConstants.mblock_size);
-    console.log(`req_blocks: ${req_blocks} | req_mblocks: ${req_mblocks}`);
-
-    // we want to allocate something smaller than a megablock
-    if (req_mblocks == 0) {
-      // look through our free block list
-      for(let i = 0; i < this.freeBlocks.length; ++i) {
-        const [mblock, l_end, r] = this.freeBlocks[i];
-        const blocks = (r - l_end) / rtsConstants.block_size;
-        if (req_blocks <= nblocks) {
-          console.log(`allocBlocks: req_blocks: ${req_blocks} | i:${i} | mblock: ${mblock} | bdescr: ${ptr2bdescr(block_addr)} | nblocks: ${nblocks}`);
-          // initialize the block descriptor.
-          const bd = ptr2bdescr(block_addr);
-          this.memory.i64Store(bd + rtsConstants.offset_bdescr_start, block_addr);
-          this.memory.i64Store(bd + rtsConstants.offset_bdescr_free, block_addr);
-          this.memory.i64Store(bd + rtsConstants.offset_bdescr_link, 0);
-          this.memory.i32Store(bd + rtsConstants.offset_bdescr_blocks, req_blocks);
-
-          // if we have leftover blocks, then add back the megablock to the freelist.
-          if (req_blocks < nblocks) {
-            // get the next free pointer.
-            console.log(`sizeof_block: ${rtsConstants.block_size}`);
-            const block_addr_next = block_addr + rtsConstants.block_size * (1 + req_blocks);
-            console.log(`block_addr_next: ${block_addr_next} | block_size: ${rtsConstants.block_size}`);
-            // check that the block descriptor of the new block is not the same
-            // as our block descriptor. Indeed, it should be "ahead".
-
-            if (ptr2bdescr(block_addr_next) <= bd) {
-              throw new WebAssembly.RuntimeError("we are getting the same bd, not the next bd from the new block");
-            }
-            const nblocks_next = nblocks - (req_blocks + 2);
-            // this.freeBlocks.splice(i, [mblock, block_addr_next, nblocks_next]);
-            this.freeBlocks.splice(i);
-
-          } else {
-            assert.equal(nblocks, req_blocks);
-            this.freeBlocks.splice(i);
-          }
-          console.log(`returning: ${bd}`);
-          return bd;
-
-        }
-      }
-    }
-
-    // we don't have free megablocks to pull blocks from, so allocate them.
-    const mblock = this.allocMegaBlocks(req_mblocks);
-    const bd = mblock + rtsConstants.offset_first_bdescr;
-    const block_addr = mblock + rtsConstants.offset_first_block;
+  // low level initialization function to initialize a block descriptor
+  initBdescr(bd, block_addr, nblocks) {
     this.memory.i64Store(bd + rtsConstants.offset_bdescr_start, block_addr);
     this.memory.i64Store(bd + rtsConstants.offset_bdescr_free, block_addr);
     this.memory.i64Store(bd + rtsConstants.offset_bdescr_link, 0);
-    this.memory.i32Store(bd + rtsConstants.offset_bdescr_blocks, req_blocks);
-
-    // if the whole thing is still happening inside a single megablock, then we still have space.
-    if (req_mblocks <= 1) { 
-      console.log("requested less than 1 megablock");
-      const block_addr = mblock + rtsConstants.offset_first_block;
-      // address of the next block
-      const block_addr_next = block_addr + rtsConstants.block_size * req_blocks;
-      const nblocks_next = rtsConstants.blocks_per_mblock - req_blocks; 
-      this.freeBlocks.push([mblock,block_addr_next,  nblocks_next]);
-    } else {
-      console.log(`requested megablocks: ${req_mblocks}`);1
-    }
-    console.log(`returning: ${bd}`);
-    return bd;
+    this.memory.i32Store(bd + rtsConstants.offset_bdescr_blocks, nblocks);
   }
 
-  freeSegment(i, mblock, l_end, r) {
+  // return a block with req_blocks if it exists.
+  // INVARIANT: req_blocks < blocks_per_mblock.
+  // returns a block descriptor if exists, 
+  // returns undefined otherwise
+  lookupFreeBlockGroupBdescr(req_blocks) {
+    assert(req_blocks < rtsConstants.blocks_per_mblock);
+    for(let i = 0; i < this.freeBlockGroups.length; ++i) {
+      const [l, r] = this.freeBlockGroups[i];
+      const nblocks = (r - l) / this.block_size;
+      // we don't have enough blocks, continue
+      if (nblocks < req_blocks) continue;
+
+      const bd = ptr2bdescr(l);
+      this.initBdescr(bd, k, req_blocks);
+
+      // allow splicing off a smaller block.
+      if (req_blocks < nblocks) {
+        const rest_l = l + rtsConstants.block_size * req_blocks;
+        this.freeBlockGroups.splice(i, [rest_l, r]);
+      }
+      else {
+        this.freeBlockGroups.splice(i);
+      }
+      return bd;
+
+    } // end oop
+  } // end funciion
+
+  allocBlocks(req_blocks) {
+    const req_mblocks = Math.ceil(req_blocks / rtsConstants.blocks_per_mblock);
+    // We need memory larger than a megablock. Just allocate it.
+    if (req_mblocks > 0) {
+      const mblock = this.allocMegaBlocks(req_mblocks);
+      const bd = mblock + rtsConstants.offset_first_bdescr;
+      const block_addr = mblock + rtsConstants.offset_first_block;
+      this.initBdescr(bd, block_addr, req_blocks);
+    }
+
+    assert.equal(req_mblocks, 0);
+    assert.equal(req_blocks < rtsConstants.blocks_per_mblock, true);
+
+    const bd_free = this.lookupFreeBlockGroupBdescr(req_blocks);
+
+    // if we do have a free block, return it.
+    if (bd_free) {
+      return bd_free;
+    }
+
+    // we don't have a free block group. Create a new megablock with those
+    // many block groups, and return it.
+    const mblock = this.allocMegaBlocks(req_mblocks);
+    const bd = mblock + rtsConstants.offset_first_bdescr;
+    const block_addr = mblock + rtsConstants.offset_first_block;
+    this.initBdescr(bd, block_addr, req_blocks);
+
+    // if we have some free blocks, add that to the free list.
+    if (req_blocks < rtsConstants.blocks_per_mblock) {
+      const rest_l = block_addr + rtsConstants.block_size * req_blocks;
+      const rest_r = block_addr + rtsConstants.block_size * blocks_per_mblock;
+    }
+
+  }
+
+  coalesceFreeBlockGroups() {
+  }
+
+  freeBlockGroup(i, l_end, r) {
     if (l_end < r) {
       this.memory.memset(l_end, 0x42 + i, r - l_end);
-      this.freeBlocks.push([mblock, l_end, r]);
+      this.freeBlockGroups.push([l_end, r]);
     }
+
+    coalesceFreeBlockGroups();
   }
 
   preserveGroups(bds) {
     console.log("preserveGroups:");
     console.log(`bds: ${Array.from(bds)}`);
     bds = Array.from(bds);
-    // const sorted_bds = Array.from(bds).sort((bd0, bd1) => bd0 - bd1);
-    // this.freeSegment(0, 
-    //   Memory.tagData(rtsConstants.mblock_size * this.staticMBlocks),
-    //   sorted_bds[0] - rtsConstants.offset_first_bdescr);
 
     var m = new Map();
     for(var i = 0; i < bds.length; ++i) {
@@ -190,31 +225,20 @@ export class BlockAlloc {
       }
     }
     
-    // current index passed to freeSegment
+    // current index passed to freeBlockGroup
     let ix = 0;
-
-    let sorted_megablocks = Array.from(m.keys()).sort((m0, m1) => m0 - m1);
     
-    // we are allowed to free between blocks.
+    let sorted_megablocks = Array.from(m.keys()).sort((m0, m1) => m0 - m1);
     // we are allowed to take up an entire megablock if the megablock is free.
     for(var i = 0; i < sorted_megablocks.length; ++i) {
       const mblock = sorted_megablocks[i];
       const bds = m[mblock];
-      const sorted_bds = Array.from(bds).sort((bd0, bd1) => bd0 - bd1);
-      for(var j = 0; j < sorted_bds.length; ++j) {
-        const l = this.memory.i64Load(sorted_bds[j] + rtsConstants.offset_bdescr_start);
-
-        if (j < sorted_bds.length - 1) {
-          // find the ending location of the block pointed to to by sorted_bds[j+1]
-          const r = bdescr2megablock(sorted_bds[j+1]) + rtsConstants.offset_first_block +  bdescr2index(sorted_bds[j+1]) * rtsConstants.block_size;
-        }
-        else if (i < sorted_megablocks.length - 1) {
-          // if we are at the end of the current megablock, we can wipe everything till the next
-          // megablock.
-          const r = sorted_megablocks[i+1];
-        }
-        throw new WebAssembly.RuntimeError("foo");
-        this.freeSegment(ix++, mblock, l, r);
+      for(let j = 0; j < bds; ++j) {
+        const bd = bds[j];
+        const l = this.memory.i64Load(bd + rtsConstants.offset_bdescr_start);
+        const nblocks = this.memory.i64Load(bd + rtsConstants.offset_bdescr_blocks);
+        const r = l + rtsConstants.block_size * nblocks;
+        this.freeBlockGroup(ix++, l, r);
       }
     }
   }
