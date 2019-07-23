ee43166a543408c25cd7a18e23d23d1c2752aec2
Shao Cheng <cheng.shao@tweag.io>
2019-07-23 13:27:37 +0800

    Move allocMegaGroup to heapalloc

--- asterius/rts/rts.mblockalloc.mjs
+++ asterius/rts/rts.mblockalloc.mjs
@@ -29,42 +29,6 @@ export class MBlockAlloc {
     return Memory.tagData(prev_size * rtsConstants.mblock_size);
   }
 
-  allocMegaGroup(n) {
-    const req_blocks = ((rtsConstants.mblock_size * n) - rtsConstants.offset_first_block) / rtsConstants.block_size;
-    for (let i = 0; i < this.freeList.length; ++i) {
-      const bd = this.freeList[i],
-            blocks = this.memory.i32Load(bd + rtsConstants.offset_bdescr_blocks);
-      if (req_blocks < blocks) {
-        this.memory.i32Store(bd + rtsConstants.offset_bdescr_blocks, req_blocks);
-        const rest_bd = bd + (rtsConstants.mblock_size * n),
-              rest_start = rest_bd - rtsConstants.offset_first_bdescr +
-                           rtsConstants.offset_first_block;
-        this.memory.i64Store(rest_bd + rtsConstants.offset_bdescr_start,
-                             rest_start);
-        this.memory.i64Store(rest_bd + rtsConstants.offset_bdescr_free, rest_start);
-        this.memory.i64Store(rest_bd + rtsConstants.offset_bdescr_link, 0);
-        this.memory.i32Store(rest_bd + rtsConstants.offset_bdescr_blocks,
-                             blocks - req_blocks -
-                                 ((rtsConstants.mblock_size / rtsConstants.block_size) -
-                                  rtsConstants.blocks_per_mblock));
-        this.freeList.splice(i, 1, rest_bd);
-        return bd;
-      }
-      if (req_blocks == blocks) {
-        this.freeList.splice(i, 1);
-        return bd;
-      }
-    }
-    const mblock = this.getMBlocks(n),
-          bd = mblock + rtsConstants.offset_first_bdescr,
-          block_addr = mblock + rtsConstants.offset_first_block;
-    this.memory.i64Store(bd + rtsConstants.offset_bdescr_start, block_addr);
-    this.memory.i64Store(bd + rtsConstants.offset_bdescr_free, block_addr);
-    this.memory.i64Store(bd + rtsConstants.offset_bdescr_link, 0);
-    this.memory.i32Store(bd + rtsConstants.offset_bdescr_blocks, req_blocks);
-    return bd;
-  }
-
   freeSegment(l_end, r) {
     if (l_end < r) {
       this.memory.memset(l_end, 0, r - l_end);
