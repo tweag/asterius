f35c57ed9a4636014e365b8688e28bfd27abb7b9
Hamish Mackenzie <Hamish.Mackenzie@iohk.io>
2019-07-29 12:54:21 +1200

    Always add --global-package-db in ahc-pkg

--- asterius/app/ahc-pkg.hs
+++ asterius/app/ahc-pkg.hs
@@ -2,16 +2,12 @@ import qualified Asterius.BuildInfo as A
 import System.Environment.Blank
 import System.FilePath
 import System.Process
+import Data.List (isPrefixOf)
 
 main :: IO ()
 main = do
   bootDir <- A.getBootDir
   args <- getArgs
-  callProcess A.ghcPkg $ do
-    arg <- args
-    if arg == "--global"
-      then [ "--global"
-           , "--global-package-db=" <>
-             (bootDir </> ".boot" </> "asterius_lib" </> "package.conf.d")
-           ]
-      else pure arg
+  callProcess A.ghcPkg $
+    ("--global-package-db=" <> bootDir </> ".boot" </> "asterius_lib" </> "package.conf.d")
+    : args
