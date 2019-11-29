6a9036330c55abfdcc9522ebb899bb7bd6c1c050
Shao Cheng <cheng.shao@tweag.io>
2019-11-29 18:06:14 +0100

    WIP

--- lts.sh
+++ lts.sh
@@ -5,23 +5,31 @@ rm -rf ~/.cabal
 ahc-cabal v1-update
 
 ahc-cabal v1-install -j --keep-going --minimize-conflict-set --prefix=$ASTERIUS_LIB_DIR --package-db=clear --package-db=global \
+  backprop \
   cassava-megaparsec \
+  cheapskate-lucid \
   cmark \
+  composition-extra \
+  concurrency \
   criterion \
+  Earley \
+  general-games \
   generics-sop \
   genvalidity-aeson \
   graphite \
   groundhog-th \
-  lucid \
+  JuicyPixels-extra \
   microlens-platform \
   persistent \
   relational-query \
   resourcet \
   rio \
+  safe-money \
   selda-json \
   serialise \
   shake \
   skylighting \
+  streamly \
   tasty-hspec
 
 rm -rf ~/.cabal
