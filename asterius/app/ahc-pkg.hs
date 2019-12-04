4433c7590ea1a48b76ce1479ee6ffa93beedab27
Shao Cheng <cheng.shao@tweag.io>
2019-12-04 14:32:37 +0100

    WIP

--- asterius/app/ahc-pkg.hs
+++ asterius/app/ahc-pkg.hs
@@ -1,17 +1,19 @@
 import qualified Asterius.BuildInfo as A
+import Data.Foldable
+import Data.List
 import System.Environment.Blank
 import System.FilePath
-import System.Process
+import System.Process (callProcess)
 
 main :: IO ()
 main = do
+  env <- getEnvironment
+  traverse_ unsetEnv
+    $ filter (\k -> ("GHC_" `isPrefixOf` k) || "HASKELL_" `isPrefixOf` k)
+    $ map fst env
   args <- getArgs
-  callProcess A.ghcPkg $ do
-    arg <- args
-    if arg == "--global"
-      then
-        [ "--global",
-          "--global-package-db="
-            <> (A.dataDir </> ".boot" </> "asterius_lib" </> "package.conf.d")
-        ]
-      else pure arg
+  callProcess A.ghcPkg $
+    ( "--global-package-db="
+        <> (A.dataDir </> ".boot" </> "asterius_lib" </> "package.conf.d")
+    )
+      : args
