5d5fb14e39e8c933da2c06698b03d8c88c6a2de5
Marica Odagaki <ento.entotto@gmail.com>
2020-01-05 21:55:17 -0800

    Use a little more of the Request object's API
    Notably, the .json() method returns a Promise and needs to be
    imported as 'safe' to have it treated as asynchronous.

--- asterius/test/cloudflare/cloudflare.hs
+++ asterius/test/cloudflare/cloudflare.hs
@@ -9,10 +9,23 @@ foreign export javascript "handleFetch" handleFetch :: JSObject -> IO JSObject
 handleFetch :: JSObject -> IO JSObject
 handleFetch ev = do
   req <- indexJSObject ev "request"
-  let req_str = jsonStringify req
-  pure $ js_new_response $ toJSString $ "From Haskell: " <> req_str
+  method <- indexJSObject (coerce req) "method"
+  case (fromJSString . coerce) method of
+    "POST" -> do
+      let payload = js_request_json req
+      name <- indexJSObject payload "name"
+      if js_is_undefined name then
+        pure $ js_new_response (toJSString "Name not specified") 400
+      else
+        pure $ js_new_response (toJSString $ "Hello " <> (fromJSString . coerce) name) 200
+    _ ->
+      pure $ js_new_response (toJSString "Hello from Haskell") 200
 
+foreign import javascript "${1} === undefined"
+  js_is_undefined :: JSVal -> Bool
 
-foreign import javascript "new Response(${1})"
-  js_new_response :: JSString -> JSObject
+foreign import javascript safe "${1}.json()"
+  js_request_json :: JSVal -> JSObject
 
+foreign import javascript "new Response(${1}, {\"status\": ${2}})"
+  js_new_response :: JSString -> Int -> JSObject
