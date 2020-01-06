24274ba24123c7e2e8d646ec0190c3c7992c275a
Shao Cheng <cheng.shao@tweag.io>
2020-01-06 14:46:30 +0100

    Apply documentation suggestion from @hsyl20 [ci skip]

--- asterius/src/Asterius/Builtins/CMath.hs
+++ asterius/src/Asterius/Builtins/CMath.hs
@@ -10,9 +10,34 @@ where
 
 import Asterius.Types
 
+-- | Here contains implementation of certain functions in math.h of libc. They
+-- are written in the expression IR directly without using our EDSL, and are not
+-- meant to be understood by humans. For the curious souls, here's the
+-- description of how they are written:
+--
+-- 1. Compile latest wasi-sdk to obtain the musl libc archives
+-- 2. Extract the relevant object file from the archives, e.g. scalbn.o
+-- 3. Use wasm2wat from wabt to disassemble the object file to human-readable
+--    S-expression syntax
+-- 4. Eyeball the disassembled function body, ensure it's "self-contained":
+--    doesn't call into other libc functions, and doesn't touch the imported
+--    linear memory
+-- 5. Write the expression IR by hand, copying the disassembled code line by
+--    line. Do not look back into the original musl C source, since the
+--    amazement brought by clang/llvm optimizations is a distraction and slows
+--    down this step
+-- 6. Eyeball the written IR and disassembled code listed in two columns,
+--    fingers crossed for no mistakes
+-- 7. Include the code into the builtin module, run examples to see if anything
+--    goes wrong, and run the ghc-testsuite to see if there's noticable
+--    regression
+--
+-- This is a necessary evil before we have a proper story of clang integration.
 cmathCBits :: AsteriusModule
 cmathCBits = scalbn
 
+-- | Implementation of the scalbn function in libc, used by __word_encodeDouble
+-- in StgPrimFloats
 scalbn :: AsteriusModule
 scalbn =
   mempty
