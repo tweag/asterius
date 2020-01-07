ac8089afe28bb5bed1c2b16b35e59775816541ea
Marica Odagaki <ento.entotto@gmail.com>
2020-01-06 20:15:41 -0800

    Fix Response object being reused

--- asterius/test/cloudflare/cloudflare.hs
+++ asterius/test/cloudflare/cloudflare.hs
@@ -15,11 +15,11 @@ handleFetch ev = do
       let payload = js_request_json req
       name <- indexJSObject payload "name"
       if js_is_undefined name then
-        pure $ js_new_response (toJSString "Name not specified") 400
+        js_new_response (toJSString "Name not specified") 400
       else
-        pure $ js_new_response (toJSString $ "Hello " <> (fromJSString . coerce) name) 200
+        js_new_response (toJSString $ "Hello " <> (fromJSString . coerce) name) 200
     _ ->
-      pure $ js_new_response (toJSString "Hello from Haskell") 200
+      js_new_response (toJSString "Hello from Haskell") 200
 
 foreign import javascript "${1} === undefined"
   js_is_undefined :: JSVal -> Bool
@@ -28,4 +28,4 @@ foreign import javascript safe "${1}.json()"
   js_request_json :: JSVal -> JSObject
 
 foreign import javascript "new Response(${1}, {\"status\": ${2}})"
-  js_new_response :: JSString -> Int -> JSObject
+  js_new_response :: JSString -> Int -> IO JSObject
