5d827e42a920b6579d638fc1b196ec82812e4e33
Andrea Condoluci <andrea.condoluci@tweag.io>
2020-01-08 11:59:02 +0100

    Implementing time and Performance API

--- asterius/rts/node/default.mjs
+++ asterius/rts/node/default.mjs
@@ -1,1 +1,36 @@
-export default "stub";
+import { performance } from "perf_hooks";
+
+/**
+ * This module contains Node.js-specific functionality.
+ */
+export default {
+  /**
+   * The Performance Web API.
+   */
+  Performance: performance,
+  /**
+   * A custom Time interface, used in {@link TimeCBits}.
+   */
+  Time: {
+    /**
+     * Returns the current millisecond timestamp,
+     * where 0 represents the start of the node.js process.
+     */
+    now: () => {
+      return performance.now();
+    },
+    /**
+     * Returns the current high-resolution timestamp,
+     * where 0 represents UNIX Epoch. The output is a
+     * [seconds, nanoseconds] Array.
+     */
+    time: () => {
+      return process.hrtime();
+    },
+    /**
+     * The resolution of the timestamp in nanoseconds
+     * (high-resolution, ~~1ns).
+     */
+    resolution: 1,
+  }
+};
--- asterius/rts/rts.time.mjs
+++ asterius/rts/rts.time.mjs
@@ -1,32 +1,52 @@
 import * as rtsConstants from "./rts.constants.mjs";
 
 export class TimeCBits {
-  constructor(memory) {
+  constructor(memory, targetSpecificModule) {
     this.memory = memory;
-    this.epoch = Date.now();
+    // Obtain Time API from the passed target-specific module
+    this.resolution = targetSpecificModule.Time.resolution; // ns
+    this.now = targetSpecificModule.Time.now; // output: ms
+    this.time = targetSpecificModule.Time.time; // output: [s,ns]
     Object.freeze(this);
   }
 
-  clock_getres(clk_id, res) {
-    if (res) {
-      this.memory.i64Store(res + rtsConstants.offset_timespec_tv_sec, 1);
-      this.memory.i64Store(res + rtsConstants.offset_timespec_tv_nsec, 0);
-    }
-    return 0;
-  }
-
-  clock_gettime(clk_id, tp) {
-    if (tp) {
-      const ms = Date.now(),
-        s = Math.floor(ms / 1000),
-        ns = (ms - s * 1000) * 1000000;
-      this.memory.i64Store(tp + rtsConstants.offset_timespec_tv_sec, s);
-      this.memory.i64Store(tp + rtsConstants.offset_timespec_tv_nsec, ns);
-    }
-    return 0;
-  }
-
+  /**
+   * Returns a (monotonic) nanoseconds timestamp.
+   */
   getMonotonicNSec() {
-    return (Date.now() - this.epoch) * 1000000;
+    // convert this.now() from ms to ns
+    return Math.floor(this.now() * 1000000);
+  }
+
+  /**
+   * Stores at a memory address the resolution of a given clock.
+   * @param clk_id the id of the clock
+   * @param addr the memory address 
+   */
+  clock_getres(clk_id, addr) {
+    if (addr) {
+      var sec = 0, nsec = this.resolution;
+      if (nsec > 1000000000) { // more than 1s
+        sec = Math.floor(this.resolution / 1000000000);
+        nsec = 0;
+      }
+      this.memory.i64Store(addr + rtsConstants.offset_timespec_tv_sec, sec);
+      this.memory.i64Store(addr + rtsConstants.offset_timespec_tv_nsec, nsec);
+    }
+    return 0;
+  }
+
+  /**
+   * Stores at a memory address the time of a given clock.
+   * @param clk_id the id of the clock
+   * @param addr the memory address 
+   */
+  clock_gettime(clk_id, addr) {
+    if (addr) {
+      const time = this.time();
+      this.memory.i64Store(addr + rtsConstants.offset_timespec_tv_sec, time[0]);
+      this.memory.i64Store(addr + rtsConstants.offset_timespec_tv_nsec, time[1]);
+    }
+    return 0;
   }
 }
