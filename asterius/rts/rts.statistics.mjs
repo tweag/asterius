2e5d258741d1d3e42cad16a48fc399bf1f72a41b
Andrea Condoluci <andrea.condoluci@tweag.io>
2020-01-21 11:52:43 +0100

    Comments and prettier

--- asterius/rts/rts.statistics.mjs
+++ asterius/rts/rts.statistics.mjs
@@ -1,27 +1,37 @@
+/**
+ * Class for gathering various statistics
+ * about the runtime system.
+ */
 export class Statistics {
+  /**
+   * @param time The target-specific Time module
+   * @param gcStatistics Whether to gather statistics about GC
+   */
   constructor(time, gcStatistics) {
     this.time = time;
-    this.gcStatistics = gcStatistics;
-    this.counters = {
-      init_time: 0,
-      gc_time: 0,
-      num_canceled_GCs: 0,
-      num_GCs: 0,
-      copied_bytes: 0,
-      allocated_mblocks: 0,
-      last_gc_started: 0,
-      wasm_memory_size: 0,
-    };
+    if (gcStatistics)
+      this.gcStatistics = {
+        init_time: 0,
+        gc_time: 0,
+        num_canceled_GCs: 0,
+        num_GCs: 0,
+        copied_bytes: 0,
+        allocated_mblocks: 0,
+        last_gc_started: 0,
+        wasm_memory_size: 0
+      };
     Object.freeze(this);
   }
 
   /**
-   * Force @param arg to be evaluated, i.e.
-   * in case it is a callback, @param arg is called
+   * Forces the argument to be evaluated, i.e.
+   * in case it is a callback, then it is called
    * and the result returned. Useful when tracing/logging
    * data that is expensive to compute, so that the actual
    * value is computed only if the logging level is
    * appropriate.
+   * @param arg The argument to force
+   * @private
    */
   static force(arg) {
     if (typeof arg === "function") {
@@ -31,13 +41,25 @@ export class Statistics {
     }
   }
 
+  /**
+   * Returns the CPU time in seconds.
+   * @private
+   */
   now() {
-    const [s,ns] = this.time.getCPUTime();
-    return s + (ns / 1000000000);
+    const [s, ns] = this.time.getCPUTime();
+    return s + ns / 1000000000;
   }
 
+  /**
+   * Traces the current allocate Wasm memory.
+   * @param bytes The size of the Wasm memory in bytes
+   */
   memoryInUse(bytes) {
-    this.counters.wasm_memory_size = bytes;
+    if (!this.gcStatistics) return;
+    this.gcStatistics.wasm_memory_size = Math.max(
+      this.gcStatistics.wasm_memory_size,
+      bytes
+    );
   }
 
   /**
@@ -46,78 +68,92 @@ export class Statistics {
    */
   endInit() {
     if (!this.gcStatistics) return;
-    this.counters.init_time = this.now();
+    this.gcStatistics.init_time = this.now();
   }
 
   /**
-   * Traces the request of a garbage collection
-   * i.e. {@link GC.performGC}.
+   * Traces the start of a garbage collection
+   * i.e. when {@link GC#performGC} is called.
    */
   startGC() {
     if (!this.gcStatistics) return;
-    this.counters.last_gc_started = this.now();
+    this.gcStatistics.last_gc_started = this.now();
   }
 
   /**
-   * Trace a cancelled garbage collection.
+   * Traces a canceled garbage collection.
    */
   cancelGC() {
     if (!this.gcStatistics) return;
-    this.counters.gc_time += this.now() - this.counters.last_gc_started;
-    this.counters.num_canceled_GCs += 1;
+    this.gcStatistics.gc_time += this.now() - this.gcStatistics.last_gc_started;
+    this.gcStatistics.num_canceled_GCs += 1;
   }
 
   /**
-   * Trace the end of a garbage collection.
+   * Traces the end of a garbage collection.
    */
   endGC() {
     if (!this.gcStatistics) return;
-    this.counters.gc_time += this.now() - this.counters.last_gc_started;
-    this.counters.num_GCs += 1;
+    this.gcStatistics.gc_time += this.now() - this.gcStatistics.last_gc_started;
+    this.gcStatistics.num_GCs += 1;
   }
 
   /**
-   * 
+   * Traces the number of bytes copied during
+   * GC due to evacuation. Called from {@link GC#copyClosure}.
+   * @param n The number of copied bytes
    */
   copiedBytes(n) {
     if (!this.gcStatistics) return;
-    this.counters.copied_bytes += Statistics.force(n);
+    this.gcStatistics.copied_bytes += Statistics.force(n);
   }
 
   /**
-   * Trace the allocation of new megablocks.
-   * Called from {@link Memory.getMBlocks}
-   * @param n The number of newly allocated megablocks
+   * Traces the allocation of new MBlocks.
+   * Called from {@link Memory#getMBlocks}
+   * @param n The number of newly allocated MBlocks
    */
   allocateMBlocks(n) {
     if (!this.gcStatistics) return;
-    this.counters.allocated_mblocks += Statistics.force(n);
+    this.gcStatistics.allocated_mblocks += Statistics.force(n);
   }
 
   /**
-   * Show the various statistics gathered, if any.
+   * Shows the various statistics gathered, if any.
    */
   show() {
     if (!this.gcStatistics) return;
-    const
-      counters = this.counters,
+    const counters = this.gcStatistics,
       total_time = this.now(),
       gc_time = counters.gc_time,
       mut_time = total_time - gc_time - counters.init_time;
-    const fmt = new Intl.NumberFormat("en-US", {useGrouping: true});
+    const fmt = new Intl.NumberFormat("en-US", { useGrouping: true });
 
     console.log("Garbage Collector Statistics:");
     console.log("  INIT  time", counters.init_time.toFixed(2) + "s");
     console.log("  MUT   time", mut_time.toFixed(2) + "s");
     console.log("  GC    time", gc_time.toFixed(2) + "s");
     console.log("  Total time", total_time.toFixed(2) + "s");
-    console.log("  % GC  time", (gc_time / total_time * 100).toFixed(1) + "%");
+    console.log(
+      "  % GC  time",
+      ((gc_time / total_time) * 100).toFixed(1) + "%"
+    );
 
     console.log("  " + counters.num_GCs, "garbage collections");
-    console.log("  " + counters.num_canceled_GCs, "canceled garbage collections");
+    console.log(
+      "  " + counters.num_canceled_GCs,
+      "canceled garbage collections"
+    );
 
-    console.log("  " + fmt.format(counters.copied_bytes), "bytes copied during GC");
+    console.log(
+      "  " + fmt.format(counters.copied_bytes),
+      "bytes copied during GC"
+    );
     console.log("  " + counters.allocated_mblocks, "total MBlocks allocated");
-    console.log("  " + Math.floor(counters.wasm_memory_size / 1024 / 1024) + "MB total Wasm memory in use");
+    console.log(
+      "  " +
+        Math.floor(counters.wasm_memory_size / 1024 / 1024) +
+        "MB total Wasm memory in use"
+    );
   }
 }
