bc6cebe65d18062787c6cd2f6fb8c17812014bc8
Shao Cheng <cheng.shao@tweag.io>
2020-02-28 15:32:01 +0100

    Fasten the GHC hooks (#465)

--- ghc-toolkit/src/Language/Haskell/GHC/Toolkit/Hooks.hs
+++ ghc-toolkit/src/Language/Haskell/GHC/Toolkit/Hooks.hs
@@ -7,11 +7,9 @@ module Language.Haskell.GHC.Toolkit.Hooks
 where
 
 import qualified CmmInfo as GHC
-import Control.Concurrent
 import Control.Monad.IO.Class
 import Data.Functor
-import qualified Data.Map.Strict as Map
-import qualified Data.Set as Set
+import Data.IORef
 import qualified DriverPhases as GHC
 import qualified DriverPipeline as GHC
 import qualified DynFlags as GHC
@@ -24,19 +22,20 @@ import qualified PipelineMonad as GHC
 
 hooksFromCompiler :: Compiler -> GHC.Hooks -> IO GHC.Hooks
 hooksFromCompiler Compiler {..} h = do
-  mods_set_ref <- newMVar Set.empty
-  cmm_raw_map_ref <- newMVar Map.empty
-  cmm_raw_ref <- newEmptyMVar
+  cmm_raw_map_ref <- newIORef GHC.emptyModuleEnv
+  let cmm_raw_ref_err = error "Language.Haskell.GHC.Toolkit.Hooks: unreachable"
+  cmm_raw_ref <- newIORef cmm_raw_ref_err
   pure
     h
       { GHC.cmmToRawCmmHook = Just $ \dflags maybe_ms_mod cmms -> do
           rawcmms <- GHC.cmmToRawCmm dflags maybe_ms_mod cmms
           case maybe_ms_mod of
             Just ms_mod -> do
-              let store :: MVar (Map.Map GHC.Module v) -> v -> IO ()
-                  store ref v = modifyMVar_ ref $ pure . Map.insert ms_mod v
+              let store :: IORef (GHC.ModuleEnv v) -> v -> IO ()
+                  store ref v = atomicModifyIORef' ref $
+                    \env -> (GHC.extendModuleEnv env ms_mod v, ())
               store cmm_raw_map_ref rawcmms
-            _ -> putMVar cmm_raw_ref rawcmms
+            _ -> writeIORef cmm_raw_ref rawcmms
           pure rawcmms,
         GHC.runPhaseHook = Just $ \phase input_fn dflags -> case phase of
           GHC.HscOut src_flavour _ (GHC.HscRecomp cgguts mod_summary@GHC.ModSummary {..}) ->
@@ -56,31 +55,24 @@ hooksFromCompiler Compiler {..} h = do
                 GHC.setForeignOs []
                 obj_output_fn <- GHC.phaseOutputFilename GHC.StopLn
                 pure (GHC.RealPhase GHC.StopLn, obj_output_fn)
-              f <- liftIO $ modifyMVar mods_set_ref $ \s ->
-                pure (Set.insert ms_mod s, Set.member ms_mod s)
-              if f
-                then liftIO $ do
-                  let clean :: MVar (Map.Map GHC.Module a) -> IO ()
-                      clean ref = modifyMVar_ ref $ pure . Map.delete ms_mod
-                  clean cmm_raw_map_ref
-                else
-                  ( do
-                      let fetch :: MVar (Map.Map GHC.Module v) -> IO v
-                          fetch ref =
-                            modifyMVar
-                              ref
-                              ( \m ->
-                                  let (Just v, m') =
-                                        Map.updateLookupWithKey (\_ _ -> Nothing) ms_mod m
-                                   in pure (m', v)
-                              )
-                      ir <- liftIO $ HaskellIR <$> fetch cmm_raw_map_ref
-                      withHaskellIR mod_summary ir obj_output_fn
-                  )
+              let fetch :: IORef (GHC.ModuleEnv v) -> IO v
+                  fetch ref =
+                    atomicModifyIORef'
+                      ref
+                      ( \env ->
+                          let Just v = GHC.lookupModuleEnv env ms_mod
+                           in (GHC.delModuleEnv env ms_mod, v)
+                      )
+              ir <- liftIO $ HaskellIR <$> fetch cmm_raw_map_ref
+              withHaskellIR mod_summary ir obj_output_fn
               pure r
           GHC.RealPhase GHC.Cmm -> do
             void $ GHC.runPhase phase input_fn dflags
-            ir <- liftIO $ CmmIR <$> takeMVar cmm_raw_ref
+            ir <-
+              liftIO $
+                CmmIR
+                  <$> readIORef cmm_raw_ref
+                  <* writeIORef cmm_raw_ref cmm_raw_ref_err
             obj_output_fn <- GHC.phaseOutputFilename GHC.StopLn
             withCmmIR ir obj_output_fn
             pure (GHC.RealPhase GHC.StopLn, obj_output_fn)
