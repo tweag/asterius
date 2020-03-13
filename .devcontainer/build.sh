bba107d9b96b440009f93fa43887ee35dd4d20ea
Shao Cheng <cheng.shao@tweag.io>
2020-03-13 16:15:42 +0100

    WIP

--- .devcontainer/build.sh
+++ .devcontainer/build.sh
@@ -4,17 +4,17 @@ export CPUS=$(getconf _NPROCESSORS_ONLN 2>/dev/null)
 export ASTERIUS_BUILD_OPTIONS=-j$CPUS
 export MAKEFLAGS=-j$CPUS
 
-stack build -j$CPUS \
+stack install -j$CPUS \
   brittany \
   ghcid \
   hlint \
   ormolu \
-  wabt \
   wai-app-static
 
 stack build -j$CPUS --test --no-run-tests \
   asterius \
   ghc-toolkit \
+  wabt \
   wasm-toolkit
 
 stack exec ahc-boot
