f4138ac690607717683e84a1862ab6235744facd
Georgios Karachalias <gdkaracha@gmail.com>
2020-03-16 15:53:42 +0100

    Minor refactorings in Asterius.BuildInfo

--- asterius/src/Asterius/BuildInfo.hs
+++ asterius/src/Asterius/BuildInfo.hs
@@ -1,3 +1,13 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Asterius.BuildInfo
+-- Copyright   :  (c) 2018 EURL Tweag
+-- License     :  All rights reserved (see LICENCE file in the distribution).
+--
+-- Paths for data and binary files.
+--
+-----------------------------------------------------------------------------
+
 module Asterius.BuildInfo
   ( ahc,
     ahcPkg,
@@ -21,9 +31,17 @@ binDir = unsafePerformIO Paths_asterius.getBinDir
 dataDir :: FilePath
 dataDir = unsafePerformIO Paths_asterius.getDataDir
 
-ahc, ahcPkg, ahcLd, ahcDist, unlit :: FilePath
+ahc :: FilePath
 ahc = binDir </> "ahc" <.> exeExtension
+
+ahcPkg :: FilePath
 ahcPkg = binDir </> "ahc-pkg" <.> exeExtension
+
+ahcLd :: FilePath
 ahcLd = binDir </> "ahc-ld" <.> exeExtension
+
+ahcDist :: FilePath
 ahcDist = binDir </> "ahc-dist" <.> exeExtension
+
+unlit :: FilePath
 unlit = binDir </> "unlit" <.> exeExtension
