ac5fad4ddc83ac469bddb518cb646b5365ea6d25
Georgios Karachalias <gdkaracha@gmail.com>
2020-03-16 16:19:24 +0100

    Minor refactorings in Asterius.Internals.MagicNumber

--- asterius/src/Asterius/Internals/MagicNumber.hs
+++ asterius/src/Asterius/Internals/MagicNumber.hs
@@ -7,7 +7,11 @@ where
 
 import Data.Int
 
-dataTag, functionTag, invalidAddress :: Int64
+dataTag :: Int64
 dataTag = 2097143
+
+functionTag :: Int64
 functionTag = 2097133
+
+invalidAddress :: Int64
 invalidAddress = 0x1fffffffff0000
