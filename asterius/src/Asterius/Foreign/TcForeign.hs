f63338930832fe8f548b17885b8a4e024c07f46d
Shao Cheng <cheng.shao@tweag.io>
2020-03-24 14:11:14 +0100

    WIP

--- asterius/src/Asterius/Foreign/TcForeign.hs
+++ asterius/src/Asterius/Foreign/TcForeign.hs
@@ -15,11 +15,9 @@ import Data.Maybe
 import DsMonad
 import ErrUtils
 import ForeignCall
-import qualified GHC.LanguageExtensions as LangExt
 import GhcPlugins
 import HsSyn
 import Outputable
-import Platform
 import PrelNames
 import TcEnv
 import TcExpr
@@ -48,28 +46,19 @@ asteriusTcFImport (L dloc fo@ForeignImport {fd_name = L nloc nm, fd_sig_ty = hs_
         id = mkLocalId nm sig_ty
     imp_decl' <- asteriusTcCheckFIType arg_tys res_ty imp_decl
     imp_decl'' <- processFFIImport globalFFIHookState norm_sig_ty imp_decl'
-    let fi_decl = ForeignImport
-          { fd_name = L nloc id,
-            fd_sig_ty = undefined,
-            fd_i_ext = mkSymCo norm_co,
-            fd_fi = imp_decl''
-          }
+    let fi_decl =
+          ForeignImport
+            { fd_name = L nloc id,
+              fd_sig_ty = undefined,
+              fd_i_ext = mkSymCo norm_co,
+              fd_fi = imp_decl''
+            }
     return (id, L dloc fi_decl, gres)
 asteriusTcFImport d = pprPanic "asteriusTcFImport" (ppr d)
 
 asteriusTcCheckFIType :: [Type] -> Type -> ForeignImport -> TcM ForeignImport
-asteriusTcCheckFIType arg_tys res_ty (CImport (L lc cconv) safety mh l@(CLabel _) src) =
+asteriusTcCheckFIType arg_tys res_ty (CImport (L lc JavaScriptCallConv) safety mh CWrapper src) =
   do
-    checkCg checkCOrAsmOrLlvmOrInterp
-    check
-      (isFFILabelTy (mkFunTys arg_tys res_ty))
-      (illegalForeignTyErr Outputable.empty)
-    cconv' <- asteriusCheckCConv cconv
-    return (CImport (L lc cconv') safety mh l src)
-asteriusTcCheckFIType arg_tys res_ty (CImport (L lc cconv) safety mh CWrapper src) =
-  do
-    checkCg checkCOrAsmOrLlvmOrInterp
-    cconv' <- asteriusCheckCConv cconv
     case arg_tys of
       [arg1_ty] -> do
         checkForeignArgs asteriusIsFFIExternalTy arg1_tys
@@ -80,54 +69,10 @@ asteriusTcCheckFIType arg_tys res_ty (CImport (L lc cconv) safety mh CWrapper sr
       _ ->
         addErrTc
           (illegalForeignTyErr Outputable.empty (text "One argument expected"))
-    return (CImport (L lc cconv') safety mh CWrapper src)
-asteriusTcCheckFIType arg_tys res_ty idecl@(CImport (L lc cconv) (L ls safety) mh (CFunction target) src)
-  | isDynamicTarget target =
+    return (CImport (L lc JavaScriptCallConv) safety mh CWrapper src)
+asteriusTcCheckFIType arg_tys res_ty (CImport (L lc cconv) (L ls safety) mh (CFunction target) src)
+  | not (isDynamicTarget target || cconv == PrimCallConv) =
     do
-      checkCg checkCOrAsmOrLlvmOrInterp
-      cconv' <- asteriusCheckCConv cconv
-      case arg_tys of
-        [] ->
-          addErrTc
-            ( illegalForeignTyErr
-                Outputable.empty
-                (text "At least one argument expected")
-            )
-        (arg1_ty : arg_tys) -> do
-          dflags <- getDynFlags
-          let curried_res_ty = mkFunTys arg_tys res_ty
-          check (isFFIDynTy curried_res_ty arg1_ty) (illegalForeignTyErr argument)
-          checkForeignArgs (asteriusIsFFIArgumentTy dflags safety) arg_tys
-          checkForeignRes
-            nonIOok
-            checkSafe
-            (asteriusIsFFIImportResultTy dflags)
-            res_ty
-      return $ CImport (L lc cconv') (L ls safety) mh (CFunction target) src
-  | cconv == PrimCallConv =
-    do
-      dflags <- getDynFlags
-      checkTc
-        (xopt LangExt.GHCForeignImportPrim dflags)
-        (text "Use GHCForeignImportPrim to allow `foreign import prim'.")
-      checkCg checkCOrAsmOrLlvmOrInterp
-      checkCTarget target
-      checkTc
-        (playSafe safety)
-        ( text
-            "The safe/unsafe annotation should not be used with `foreign import prim'."
-        )
-      checkForeignArgs (asteriusIsFFIArgumentTy dflags safety) arg_tys
-      checkForeignRes
-        nonIOok
-        checkSafe
-        (asteriusIsFFIImportResultTy dflags)
-        res_ty
-      return idecl
-  | otherwise =
-    do
-      checkCg checkCOrAsmOrLlvmOrInterp
-      cconv' <- asteriusCheckCConv cconv
       dflags <- getDynFlags
       checkForeignArgs (asteriusIsFFIArgumentTy dflags safety) arg_tys
       checkForeignRes
@@ -135,13 +80,15 @@ asteriusTcCheckFIType arg_tys res_ty idecl@(CImport (L lc cconv) (L ls safety) m
         checkSafe
         (asteriusIsFFIImportResultTy dflags)
         res_ty
-      checkMissingAmpersand dflags arg_tys res_ty
-      case target of
-        StaticTarget _ _ _ False
-          | not (null arg_tys) ->
-            addErrTc (text "`value' imports cannot have function types")
-        _ -> return ()
-      return $ CImport (L lc cconv') (L ls safety) mh (CFunction target) src
+      return $
+        CImport
+          (L lc JavaScriptCallConv)
+          (L ls safety)
+          mh
+          (CFunction target)
+          src
+asteriusTcCheckFIType arg_tys res_ty imp_decl =
+  tcCheckFIType arg_tys res_ty imp_decl
 
 asteriusTcForeignExports ::
   [LForeignDecl GhcRn] ->
@@ -180,17 +127,17 @@ asteriusTcFExport fo@ForeignExport {fd_name = L loc nm, fd_sig_ty = hs_ty, fd_fe
 asteriusTcFExport d = pprPanic "asteriusTcFExport" (ppr d)
 
 asteriusTcCheckFEType :: Type -> ForeignExport -> TcM ForeignExport
-asteriusTcCheckFEType sig_ty (CExport (L l (CExportStatic esrc str cconv)) src) =
+asteriusTcCheckFEType sig_ty (CExport (L l (CExportStatic esrc str JavaScriptCallConv)) src) =
   do
     checkCg checkCOrAsmOrLlvm
     checkTc (isCLabelString str) (badCName str)
-    cconv' <- asteriusCheckCConv cconv
     checkForeignArgs asteriusIsFFIExternalTy arg_tys
     checkForeignRes nonIOok noCheckSafe asteriusIsFFIExportResultTy res_ty
-    return (CExport (L l (CExportStatic esrc str cconv')) src)
+    return (CExport (L l (CExportStatic esrc str JavaScriptCallConv)) src)
   where
     (bndrs, res_ty) = tcSplitPiTys sig_ty
     arg_tys = mapMaybe binderRelevantType_maybe bndrs
+asteriusTcCheckFEType norm_sig_ty spec = tcCheckFEType norm_sig_ty spec
 
 asteriusIsFFIArgumentTy :: DynFlags -> Safety -> Type -> Validity
 asteriusIsFFIArgumentTy dflags safety ty
@@ -220,27 +167,3 @@ isAnyTy :: Type -> Bool
 isAnyTy ty = case tcSplitTyConApp_maybe ty of
   Just (tc, _) -> anyTyConKey == getUnique tc
   Nothing -> False
-
-asteriusCheckCConv :: CCallConv -> TcM CCallConv
-asteriusCheckCConv CCallConv = return CCallConv
-asteriusCheckCConv CApiConv = return CApiConv
-asteriusCheckCConv StdCallConv = do
-  dflags <- getDynFlags
-  let platform = targetPlatform dflags
-  if platformArch platform == ArchX86
-    then return StdCallConv
-    else do
-      when (wopt Opt_WarnUnsupportedCallingConventions dflags) $
-        addWarnTc
-          (Reason Opt_WarnUnsupportedCallingConventions)
-          ( text
-              "the 'stdcall' calling convention is unsupported on this platform,"
-              $$ text "treating as ccall"
-          )
-      return CCallConv
-asteriusCheckCConv PrimCallConv = do
-  addErrTc
-    ( text "The `prim' calling convention can only be used with `foreign import'"
-    )
-  return PrimCallConv
-asteriusCheckCConv JavaScriptCallConv = return JavaScriptCallConv
