cd69c7ff9ad7b72ee937a275057f508ab91f5edf
Shao Cheng <cheng.shao@tweag.io>
2020-03-25 23:40:07 +0100

    Slight improvements in `Asterius.Aeson` (#506)

--- ghc-toolkit/boot-libs/asterius-prelude/src/Asterius/UTF8.hs
+++ ghc-toolkit/boot-libs/asterius-prelude/src/Asterius/UTF8.hs
@@ -4,45 +4,47 @@ module Asterius.UTF8
   )
 where
 
-import Asterius.Magic
 import Asterius.Types
 import qualified Data.ByteString.Internal as BS
-import qualified Data.ByteString.Lazy as LBS
+import qualified Data.ByteString.Lazy.Internal as LBS
 import qualified Data.ByteString.Unsafe as BS
 import Foreign
 
-{-# INLINE utf8ToJSString #-}
-utf8ToJSString :: LBS.ByteString -> JSString
-utf8ToJSString s
-  | LBS.null s = js_str_empty
-  | otherwise = accursedUnutterablePerformIO $ do
-    dec <- js_dec
-    LBS.foldrChunks
-      (\c m -> BS.unsafeUseAsCStringLen c (uncurry (js_dec_chunk dec)) *> m)
-      (pure ())
-      s
-    js_dec_result dec
+{-# INLINEABLE utf8ToJSString #-}
+utf8ToJSString :: LBS.ByteString -> IO JSString
+utf8ToJSString LBS.Empty = pure js_str_empty
+utf8ToJSString s = do
+  dec <- js_dec
+  let w (LBS.Chunk c cs) = BS.unsafeUseAsCStringLen c (uncurry (js_dec_chunk dec)) *> w cs
+      w LBS.Empty = pure ()
+   in w s
+  r <- js_dec_result dec
+  freeJSVal dec
+  pure r
 
-{-# INLINE utf8FromJSString #-}
-utf8FromJSString :: JSString -> LBS.ByteString
-utf8FromJSString s = accursedUnutterablePerformIO $ do
-  l <- js_str_len s
-  if l == 0
-    then pure mempty
-    else fmap LBS.fromStrict $ BS.createUptoN (l * 3) $ \p ->
-      js_utf8_from_str s p (l * 3)
+{-# INLINEABLE utf8FromJSString #-}
+utf8FromJSString :: JSString -> IO BS.ByteString
+utf8FromJSString s = do
+  let l = lengthOfJSString s
+  case l of
+    0 -> pure mempty
+    _ -> BS.createUptoN (l * 3) $ \p -> js_utf8_from_str s p (l * 3)
 
-foreign import javascript "''" js_str_empty :: JSString
+foreign import javascript unsafe "''"
+  js_str_empty :: JSString
 
-foreign import javascript "(() => {const dec = new TextDecoder('utf-8', {fatal: true}); dec.result = ''; return dec;})()"
+foreign import javascript unsafe "(() => {              \
+\  const dec = new TextDecoder('utf-8', {fatal: true}); \
+\  dec.result = '';                                     \
+\  return dec;                                          \
+\  })()"
   js_dec :: IO JSVal
 
-foreign import javascript "$1.result += $1.decode(new Uint8Array(__asterius_jsffi.exports.memory.buffer, $2 & 0xffffffff, $3), {stream: true})"
+foreign import javascript unsafe "$1.result += $1.decode(__asterius_jsffi.exposeMemory($2, $3), {stream: true})"
   js_dec_chunk :: JSVal -> Ptr a -> Int -> IO ()
 
-foreign import javascript "$1.result" js_dec_result :: JSVal -> IO JSString
+foreign import javascript unsafe "$1.result"
+  js_dec_result :: JSVal -> IO JSString
 
-foreign import javascript "$1.length" js_str_len :: JSString -> IO Int
-
-foreign import javascript "(new TextEncoder()).encodeInto($1, new Uint8Array(__asterius_jsffi.exports.memory.buffer, $2 & 0xffffffff, $3)).written"
+foreign import javascript unsafe "(new TextEncoder()).encodeInto($1, __asterius_jsffi.exposeMemory($2, $3)).written"
   js_utf8_from_str :: JSString -> Ptr a -> Int -> IO Int
