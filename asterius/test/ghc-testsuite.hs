745f7b422b5186405b19f1b262baff7f6f53d900
Shao Cheng <cheng.shao@tweag.io>
2020-03-27 15:57:29 +0100

    Use tail calls for ghc-testsuite

--- asterius/test/ghc-testsuite.hs
+++ asterius/test/ghc-testsuite.hs
@@ -125,6 +125,7 @@ runTestCase l_opts tlref TestCase {..} = catch m h
                   takeFileName tmp_case_path,
                   "--console-history",
                   "--backend=binaryen",
+                  "--tail-calls",
                   "--verbose-err"
                 ]
                   <> l_opts
@@ -138,9 +139,7 @@ runTestCase l_opts tlref TestCase {..} = catch m h
           defJSSessionOpts
             { nodeExtraArgs =
                 ["--experimental-wasm-bigint" | "--debug" `elem` l_opts]
-                  <> [ "--experimental-wasm-return-call"
-                       | "--tail-calls" `elem` l_opts
-                     ]
+                  <> [ "--experimental-wasm-return-call" ]
             }
           $ \s -> do
             i <- newAsteriusInstance s (tmp_case_path -<.> "req.mjs") mod_buf
