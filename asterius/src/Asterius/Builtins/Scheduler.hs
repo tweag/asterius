7884a9524dd7e91ec4de6d48bbc9b583761a9841
Shao Cheng <cheng.shao@tweag.io>
2020-03-27 13:47:48 +0100

    WIP

---
+++ asterius/src/Asterius/Builtins/Scheduler.hs
@@ -0,0 +1,9 @@
+module Asterius.Builtins.Scheduler (schedulerImports,schedulerCBits) where
+
+import Asterius.Types
+
+schedulerImports :: [FunctionImport]
+schedulerImports = undefined
+
+schedulerCBits :: AsteriusModule
+schedulerCBits = undefined
