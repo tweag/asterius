52f851f8f1e6b7e48e906959e905d6f1a1a939ac
Shao Cheng <cheng.shao@tweag.io>
2020-04-10 02:20:45 +0200

    WIP

--- asterius/src/Asterius/Binary/Orphans.hs
+++ asterius/src/Asterius/Binary/Orphans.hs
@@ -6,6 +6,8 @@ module Asterius.Binary.Orphans
 where
 
 import qualified Binary as GHC
+import Control.Monad
+import Data.Foldable
 import qualified Data.Map.Lazy as M
 import GHC.Float
 
@@ -18,5 +20,12 @@ instance GHC.Binary Double where
   get bh = castWord64ToDouble <$> GHC.get bh
 
 instance (GHC.Binary k, GHC.Binary v) => GHC.Binary (M.Map k v) where
-  put_ bh = GHC.put_ bh . M.toAscList
-  get bh = M.fromDistinctAscList <$> GHC.get bh
+  put_ bh m =
+    GHC.put_ bh (M.size m)
+      *> for_ (M.toAscList m) (\(k, v) -> GHC.put_ bh k *> GHC.lazyPut bh v)
+  get bh =
+    fmap M.fromDistinctAscList $
+      GHC.get bh
+        >>= flip
+          replicateM
+          ((,) <$> GHC.get bh <*> GHC.lazyGet bh)
