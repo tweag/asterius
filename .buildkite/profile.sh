0191daee73e67c0ad806d096bf107322fc499672
Shao Cheng <cheng.shao@tweag.io>
2020-04-20 08:04:49 +0200

    WIP

---
+++ .buildkite/profile.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -euo pipefail
+
+cd .buildkite
+
+GHCRTS="-P -hy -l-au" ahc-link --input-hs Setup.hs
+
+mkdir reports
+mv \
+  *.eventlog \
+  *.hp \
+  *.prof \
+  reports/
+sudo chown -c -h -R $UID:$GID .
