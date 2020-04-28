ec98de40e9da3b67f8a21f92ba824a3e3c2a71b9
Georgios Karachalias <george.karachalias@gmail.com>
2020-04-28 18:41:12 +0200

    wip-nfdata-asterius-types (cheng) remove unnecessary check

--- asterius/src/Asterius/NFData/TH.hs
+++ asterius/src/Asterius/NFData/TH.hs
@@ -14,34 +14,33 @@ genNFData :: Name -> Q [Dec]
 genNFData ty = do
   TyConI dec <- reify ty
   case dec of
-    DataD [] ((== ty) -> True) [] Nothing cons _
-      | length cons <= 0xFF ->
-        pure
-          [ InstanceD
-              Nothing
-              []
-              (AppT (ConT ''NFData) (ConT ty))
-              [ FunD
-                  'rnf
-                  [ Clause
-                      [ ConP
-                          (dataConName con)
-                          (map VarP vars)
-                      ]
-                      ( NormalB $
-                          if null vars
-                            then ConE '()
-                            else
-                              foldl1'
-                                (\acc x -> AppE (AppE (VarE 'seq) acc) x)
-                                (map (AppE (VarE 'rnf) . VarE) vars)
-                      )
-                      []
-                    | con <- cons,
-                      let vars = [mkName $ "a" <> show j | j <- [1 .. dataConFields con]]
-                  ]
-              ]
-          ]
+    DataD [] ((== ty) -> True) [] Nothing cons _ ->
+      pure
+        [ InstanceD
+            Nothing
+            []
+            (AppT (ConT ''NFData) (ConT ty))
+            [ FunD
+                'rnf
+                [ Clause
+                    [ ConP
+                        (dataConName con)
+                        (map VarP vars)
+                    ]
+                    ( NormalB $
+                        if null vars
+                          then ConE '()
+                          else
+                            foldl1'
+                              (\acc x -> AppE (AppE (VarE 'seq) acc) x)
+                              (map (AppE (VarE 'rnf) . VarE) vars)
+                    )
+                    []
+                  | con <- cons,
+                    let vars = [mkName $ "a" <> show j | j <- [1 .. dataConFields con]]
+                ]
+            ]
+        ]
     _ -> fail $ "Asterius.NFData.TH.genNFData: " <> show dec
 
 dataConName :: Con -> Name
