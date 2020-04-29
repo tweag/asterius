a92e0f1e859f3b6131cc5491fede58bf9fb076b6
Georgios Karachalias <george.karachalias@gmail.com>
2020-04-29 15:07:52 +0200

    wip-parallel-experiment (cheng) refactor loadAr

--- asterius/src/Asterius/Ar.hs
+++ asterius/src/Asterius/Ar.hs
@@ -4,19 +4,35 @@
 
 module Asterius.Ar
   ( loadAr,
+    loadArchiveEntries,
+    loadArchiveEntry
   )
 where
 
 import qualified Ar as GHC
 import Asterius.Binary.ByteString
-import Asterius.Internals.Parallel
 import Asterius.Types
 import qualified IfaceEnv as GHC
 
+-- | Load an archive file from disk, deserialize all objects it contains and
+-- concatenate them into a single 'AsteriusCachedModule'.
 loadAr :: GHC.NameCacheUpdater -> FilePath -> IO AsteriusCachedModule
 loadAr ncu p = do
+  entries <- loadArchiveEntries p
+  mconcat <$> mapM (loadArchiveEntry ncu) entries
+
+-- | Load all the archive entries from an archive file @.a@, as a list of plain
+-- 'ByteString's (content only).
+{-# INLINE loadArchiveEntries #-}
+loadArchiveEntries :: FilePath -> IO [GHC.ArchiveEntry]
+loadArchiveEntries p = do -- drop ncu
   GHC.Archive entries <- GHC.loadAr p
-  parallelFor 1 entries $ \GHC.ArchiveEntry {..} -> -- TODO: Parameterize
-    tryGetBS ncu filedata >>= \case
-      Left {} -> pure mempty
-      Right m -> pure m
+  return entries
+
+-- | Deserialize an 'GHC.ArchiveEntry'. In case deserialization fails, return
+-- an empty 'AsteriusModule'.
+loadArchiveEntry :: GHC.NameCacheUpdater -> GHC.ArchiveEntry -> IO AsteriusCachedModule
+loadArchiveEntry ncu = \GHC.ArchiveEntry {..} ->
+  tryGetBS ncu filedata >>= \case
+    Left {} -> pure mempty
+    Right m -> pure m
