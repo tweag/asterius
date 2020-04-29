0841d27e51dc25d80069688aa4c4b136be567b3a
Shao Cheng <cheng.shao@tweag.io>
2020-04-29 13:58:02 +0200

    Allow linking executables with -threaded (#626)

--- asterius/app/ahc-ld.hs
+++ asterius/app/ahc-ld.hs
@@ -27,13 +27,11 @@ parseLinkTask args = do
         outputIR =
           find ("--output-ir=" `isPrefixOf`) args
             >>= stripPrefix "--output-ir=",
-        rootSymbols =
-          map fromString $
-            str_args "--extra-root-symbol=",
+        rootSymbols = map fromString $ str_args "--extra-root-symbol=",
         exportFunctions =
-          ("main" :)
-            $ map fromString
-            $ str_args "--export-function="
+          ("main" :) $ map fromString $
+            str_args
+              "--export-function="
       }
   where
     prog_name
@@ -47,8 +45,11 @@ parseLinkTask args = do
     link_output = args !! succ output_i
     Just output_i = elemIndex "-o" args
     link_objs = filter ((== "o.") . take 2 . reverse) args
+    fix_libhsrts "libHSrts_thr.a" = "libHSrts.a"
+    fix_libhsrts lib = lib
     link_libnames =
-      map ((<> ".a") . ("lib" <>) . drop 2) $ filter ((== "-l") . take 2) args
+      map (fix_libhsrts . (<> ".a") . ("lib" <>) . drop 2) $
+        filter ((== "-l") . take 2) args
     link_libdirs = map (drop 2) $ filter ((== "-L") . take 2) args
     str_args arg_prefix = mapMaybe (stripPrefix arg_prefix) args
 
