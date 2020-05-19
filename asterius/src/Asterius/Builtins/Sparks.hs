b87cb38070d747926125193953041a70b79b9c35
Shao Cheng <cheng.shao@tweag.io>
2020-05-19 14:00:29 +0200

    [ci skip] Apply suggestions from code review
    Co-authored-by: George Karachalias <gdkaracha@gmail.com>

--- asterius/src/Asterius/Builtins/Sparks.hs
+++ asterius/src/Asterius/Builtins/Sparks.hs
@@ -11,6 +11,12 @@ import Asterius.Types
 sparksCBits :: AsteriusModule
 sparksCBits = newSpark
 
+-- | Create a new spark, as a result of calling @par@. This function is
+-- effectively a no-op (as opposed to it's GHC counterpart, see
+-- https://gitlab.haskell.org/ghc/ghc/-/blob/master/rts/Sparks.c#L54-80), since
+-- non-threaded rts sparks are evaluated sequentially. Nevertheless, it is
+-- required for @par@ to operate (see issue
+-- https://github.com/tweag/asterius/issues/653).
 newSpark :: AsteriusModule
 newSpark = runEDSL "newSpark" $ do
   setReturnTypes [I64]
