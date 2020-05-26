f208d12122ccd6dd39166e144f0ce00631666f70
Georgios Karachalias <george.karachalias@gmail.com>
2020-05-26 15:23:35 +0200

    WIP (refactoring)

--- asterius/src/Asterius/Boot.hs
+++ asterius/src/Asterius/Boot.hs
@@ -55,17 +55,18 @@ data BootArgs
 defaultBootArgs :: BootArgs
 defaultBootArgs = BootArgs
   { bootDir = dataDir </> ".boot",
-    configureOptions =
-      "--disable-shared\
-      \ --disable-profiling\
-      \ --disable-debug-info\
-      \ --disable-library-for-ghci\
-      \ --disable-split-objs\
-      \ --disable-split-sections\
-      \ --disable-library-stripping\
-      \ -O2\
-      \ --ghc-option=-v1\
-      \ --ghc-option=-dsuppress-ticks",
+    configureOptions = unwords
+      ["--disable-shared",
+       "--disable-profiling",
+       "--disable-debug-info",
+       "--disable-library-for-ghci",
+       "--disable-split-objs",
+       "--disable-split-sections",
+       "--disable-library-stripping",
+       "-O2",
+       "--ghc-option=-v1",
+       "--ghc-option=-dsuppress-ticks"
+      ],
     builtinsOptions = defaultBuiltinsOptions
   }
 
