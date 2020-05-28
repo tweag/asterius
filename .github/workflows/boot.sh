27691feed61b6cc259e50c90c250d01c1ae62b18
Shao Cheng <cheng.shao@tweag.io>
2020-05-28 23:56:55 +0200

    WIP

--- .github/workflows/boot.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-stack update
-stack -j2 build --test --no-run-tests asterius
-. .envrc
-ahc-boot
---
+++ .github/workflows/setup-deps.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -eu
+
+sudo apt install -y \
+  libnuma-dev
+curl -L https://github.com/WebAssembly/binaryen/archive/version_93.tar.gz | tar xz -C /tmp
+pushd /tmp/binaryen-version_93
+mkdir build
+cd build
+cmake \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_INSTALL_PREFIX=/usr \
+  -G "Unix Makefiles" \
+  ..
+sudo make -j2 install
+popd
