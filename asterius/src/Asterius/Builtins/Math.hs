543077d1e559ca3e98b1eca01c3c5b77183b7db0
Georgios Karachalias <george.karachalias@gmail.com>
2020-06-11 15:17:41 +0200

    Use a list comprehension instead

--- asterius/src/Asterius/Builtins/Math.hs
+++ asterius/src/Asterius/Builtins/Math.hs
@@ -14,80 +14,22 @@ import Asterius.Types
 
 mathCBits :: AsteriusModule
 mathCBits =
-  mathSin
-    <> mathCos
-    <> mathTan
-    <> mathSinh
-    <> mathCosh
-    <> mathTanh
-    <> mathAsin
-    <> mathAcos
-    <> mathAtan
-    <> mathLog
-    <> mathExp
-
-mathSin :: AsteriusModule
-mathSin = runEDSL "sin" $ do
-  setReturnTypes [F64]
-  x <- param F64
-  callImport' "__asterius_sin_F64" [x] F64 >>= emit
-
-mathCos :: AsteriusModule
-mathCos = runEDSL "cos" $ do
-  setReturnTypes [F64]
-  x <- param F64
-  callImport' "__asterius_cos_F64" [x] F64 >>= emit
-
-mathTan :: AsteriusModule
-mathTan = runEDSL "tan" $ do
-  setReturnTypes [F64]
-  x <- param F64
-  callImport' "__asterius_tan_F64" [x] F64 >>= emit
-
-mathSinh :: AsteriusModule
-mathSinh = runEDSL "sinh" $ do
-  setReturnTypes [F64]
-  x <- param F64
-  callImport' "__asterius_sinh_F64" [x] F64 >>= emit
-
-mathCosh :: AsteriusModule
-mathCosh = runEDSL "cosh" $ do
-  setReturnTypes [F64]
-  x <- param F64
-  callImport' "__asterius_cosh_F64" [x] F64 >>= emit
-
-mathTanh :: AsteriusModule
-mathTanh = runEDSL "tanh" $ do
-  setReturnTypes [F64]
-  x <- param F64
-  callImport' "__asterius_tanh_F64" [x] F64 >>= emit
-
-mathAsin :: AsteriusModule
-mathAsin = runEDSL "asin" $ do
-  setReturnTypes [F64]
-  x <- param F64
-  callImport' "__asterius_asin_F64" [x] F64 >>= emit
-
-mathAcos :: AsteriusModule
-mathAcos = runEDSL "acos" $ do
-  setReturnTypes [F64]
-  x <- param F64
-  callImport' "__asterius_acos_F64" [x] F64 >>= emit
-
-mathAtan :: AsteriusModule
-mathAtan = runEDSL "atan" $ do
-  setReturnTypes [F64]
-  x <- param F64
-  callImport' "__asterius_atan_F64" [x] F64 >>= emit
-
-mathLog :: AsteriusModule
-mathLog = runEDSL "log" $ do
-  setReturnTypes [F64]
-  x <- param F64
-  callImport' "__asterius_log_F64" [x] F64 >>= emit
-
-mathExp :: AsteriusModule
-mathExp = runEDSL "exp" $ do
-  setReturnTypes [F64]
-  x <- param F64
-  callImport' "__asterius_exp_F64" [x] F64 >>= emit
+  mconcat
+    [ runEDSL (mkEntitySymbol op) $ do
+        setReturnTypes [F64]
+        x <- param F64
+        callImport' ("__asterius_" <> op <> "_F64") [x] F64 >>= emit
+      | op <-
+          [ "sin",
+            "cos",
+            "tan",
+            "sinh",
+            "cosh",
+            "tanh",
+            "asin",
+            "acos",
+            "atan",
+            "log",
+            "exp"
+          ]
+    ]
