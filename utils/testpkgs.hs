2fcea6666c09261c645c18df34152b57230a4f79
Shao Cheng <cheng.shao@tweag.io>
2020-06-15 11:58:31 +0200

    WIP

---
+++ utils/testpkgs.hs
@@ -0,0 +1,69 @@
+{-# LANGUAGE LambdaCase #-}
+{-# OPTIONS_GHC -Wall -O2 -threaded -rtsopts "-with-rtsopts=-H512m -I0 -qg" #-}
+
+import Control.Concurrent
+import Control.Exception
+import Control.Monad
+import Data.IORef
+import Data.List
+import System.Environment.Blank
+import System.Exit
+import System.IO
+import System.IO.Unsafe
+import System.Process
+
+pooledFoldMap :: Monoid m => Int -> (a -> IO m) -> [a] -> IO m
+pooledFoldMap nprocs f jobs = do
+  queue <- newIORef jobs
+  rets <- forM [1 .. nprocs] $ \_ -> do
+    cache <- newIORef mempty
+    ret <- newEmptyMVar
+    let w = join $ atomicModifyIORef' queue $ \case
+          (x : xs) ->
+            ( xs,
+              do
+                r <- f x
+                modifyIORef' cache (r <>)
+                w
+            )
+          [] -> ([], pure ())
+    void $
+      forkFinally
+        w
+        ( \case
+            Left (SomeException err) -> putMVar ret (throw err)
+            _ -> readIORef cache >>= putMVar ret
+        )
+    pure ret
+  mconcat <$> forM rets takeMVar
+
+testPkg :: String -> IO [String]
+testPkg pkg = do
+  (c, _stdout, _stderr) <-
+    readProcessWithExitCode
+      "ahc-cabal"
+      [ "v1-install",
+        "--dry-run",
+        "--package-db=clear",
+        "--package-db=global",
+        pkg
+      ]
+      ""
+  withMVar stdoutLock $ \_ -> putStr _stdout
+  withMVar stderrLock $ \_ -> hPutStr stderr _stderr
+  pure $ case c of
+    ExitSuccess -> [pkg]
+    _ -> []
+
+main :: IO ()
+main = do
+  [n, pkgs_path] <- getArgs
+  pkgs <- words <$> readFile pkgs_path
+  pkgs_out <- sort <$> pooledFoldMap (read n) testPkg pkgs
+  writeFile pkgs_path $ unlines pkgs_out
+
+stdoutLock, stderrLock :: MVar ()
+{-# NOINLINE stdoutLock #-}
+stdoutLock = unsafePerformIO $ newMVar ()
+{-# NOINLINE stderrLock #-}
+stderrLock = unsafePerformIO $ newMVar ()
