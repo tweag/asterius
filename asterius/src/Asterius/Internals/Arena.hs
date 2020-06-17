11a4df503d9924ece561613374facd268c3050f9
Shao Cheng <cheng.shao@tweag.io>
2020-06-17 18:51:47 +0200

    Use arena allocation for the binaryen backend

---
+++ asterius/src/Asterius/Internals/Arena.hs
@@ -0,0 +1,26 @@
+module Asterius.Internals.Arena
+  ( Arena,
+    alloc,
+    with,
+  )
+where
+
+import Control.Exception
+import Foreign.C
+import Foreign.Ptr
+
+newtype Arena = Arena (Ptr Arena)
+
+alloc :: Arena -> Int -> IO (Ptr a)
+alloc a len
+  | len > 0 = c_arenaAlloc a (fromIntegral len)
+  | otherwise = fail $ "arenaAlloc: invalid length " <> show len
+
+with :: (Arena -> IO r) -> IO r
+with = bracket new free
+
+foreign import ccall unsafe "newArena" new :: IO Arena
+
+foreign import ccall unsafe "arenaAlloc" c_arenaAlloc :: Arena -> CSize -> IO (Ptr a)
+
+foreign import ccall unsafe "arenaFree" free :: Arena -> IO ()
