2ef412add3c6b97da27485dce040d89ff485fa74
Cheng Shao <cheng.shao@tweag.io>
2020-06-18 16:39:37 +0200

    Check in AutoApply.cmm (#687)

--- ghc-toolkit/Setup.hs
+++ ghc-toolkit/Setup.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE CPP #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# OPTIONS_GHC -Wall #-}
 
@@ -8,21 +7,19 @@ import Distribution.Simple.BuildPaths hiding
   ( exeExtension,
   )
 import Distribution.Simple.LocalBuildInfo
-import Distribution.Simple.Program
 import Distribution.Types.BuildInfo
 import Distribution.Types.Library
 import Distribution.Types.LocalBuildInfo
 import Distribution.Types.PackageDescription
 import System.Directory
 import System.FilePath
-import System.Process
 
 main :: IO ()
 main =
   defaultMainWithHooks
     simpleUserHooks
       { confHook = \t f -> do
-          lbi@LocalBuildInfo {localPkgDescr = pkg_descr@PackageDescription {library = Just lib@Library {libBuildInfo = bi}}, compiler = Compiler {compilerProperties = m}, withPrograms = prog_db} <-
+          lbi@LocalBuildInfo {localPkgDescr = pkg_descr@PackageDescription {library = Just lib@Library {libBuildInfo = bi}}, compiler = Compiler {compilerProperties = m}} <-
             confHook simpleUserHooks t f
           let [clbi@LibComponentLocalBuildInfo {componentUnitId = uid}] =
                 componentNameCLBIs lbi mainLibName
@@ -31,8 +28,6 @@ main =
                 absoluteComponentInstallDirs pkg_descr lbi uid NoCopyDest
               self_bindir = bindir self_installdirs
               self_datadir = datadir self_installdirs
-              rts_datadir = self_datadir </> "boot-libs" </> "rts"
-              Just ghc_prog = lookupProgram ghcProgram prog_db
           createDirectoryIfMissing True amp
           writeFile (amp </> "BuildInfo_ghc_toolkit.hs") $
             "module BuildInfo_ghc_toolkit where\nghcLibDir :: FilePath\nghcLibDir = "
@@ -41,16 +36,6 @@ main =
               ++ show self_datadir
               ++ "\nbinDir :: FilePath\nbinDir = "
               ++ show self_bindir
-          autoapply <-
-            readProcess
-              (replaceBaseName (locationPath (programLocation ghc_prog)) "runghc")
-              [ "--ghc-arg=-Ighc-libdir/include",
-                "--ghc-arg=-Iinclude-private",
-                "genapply" </> "Main.hs"
-              ]
-              ""
-          createDirectoryIfMissing True rts_datadir
-          writeFile (rts_datadir </> "AutoApply.cmm") autoapply
           pure
             lbi
               { localPkgDescr =
@@ -73,8 +58,4 @@ main =
       }
 
 mainLibName :: ComponentName
-#if MIN_VERSION_Cabal(3,0,0)
 mainLibName = CLibName defaultLibName
-#else
-mainLibName = defaultLibName
-#endif
