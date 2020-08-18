d7e9e02bf208c25fd92f80d916c8fdf3bdb85d55
Cheng Shao <cheng.shao@tweag.io>
2020-08-18 12:04:52 +0200

    Fix clock support (#744)

--- asterius/src/Asterius/CodeGen/Droppable.hs
+++ asterius/src/Asterius/CodeGen/Droppable.hs
@@ -8,5 +8,6 @@ where
 import Asterius.Types
 
 ccallResultDroppable :: EntitySymbol -> [ValueType]
-ccallResultDroppable sym | sym == "memset" || sym == "memcpy" = [I64]
+ccallResultDroppable sym
+  | sym `elem` ["memset", "memcpy", "clock_gettime", "clock_getres"] = [I64]
 ccallResultDroppable _ = []
