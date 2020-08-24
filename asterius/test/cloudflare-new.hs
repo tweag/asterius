f07ba9f8c97654cea28af11e285a3bc609a23a52
Cheng Shao <cheng.shao@tweag.io>
2020-08-24 19:36:58 +0000

    WIP

--- asterius/test/cloudflare-new.hs
+++ asterius/test/cloudflare-new.hs
@@ -1,19 +1,24 @@
 import System.Directory
 import System.Environment
+import System.FilePath
 import System.Process
 
 main :: IO ()
 main = do
   args <- getArgs
-  withCurrentDirectory "test/cloudflare-new" $ callCommand "npm install"
-  callProcess "ahc-link" $
-    [ "--bundle",
-      "--browser",
-      "--input-hs",
-      "test/cloudflare-new/Worker.hs",
-      "--input-mjs",
-      "test/cloudflare-new/Worker.mjs",
-      "--export-function=handleFetch",
-      "--no-main"
-    ]
-      <> args
+  withCurrentDirectory "test/cloudflare-new" $ do
+    callCommand "npm install"
+    callProcess "ahc-link" $
+      [ "--bundle",
+        "--browser",
+        "--input-hs",
+        "Worker.hs",
+        "--input-mjs",
+        "Worker.mjs",
+        "--export-function=handleFetch",
+        "--no-main"
+      ]
+        <> args
+    createDirectoryIfMissing True "worker"
+    copyFile "Worker.wasm" ("worker" </> "module.wasm")
+    callCommand "npx wrangler preview --headless"
