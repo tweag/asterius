db52dac9fc8ff956b991f54897f6a5bf83b8287b
Georgios Karachalias <george.karachalias@gmail.com>
2020-09-28 12:50:56 +0200

    WIP (just comments)

--- asterius/src/Asterius/Resolve.hs
+++ asterius/src/Asterius/Resolve.hs
@@ -54,14 +54,15 @@ resolveAsteriusModule ::
 resolveAsteriusModule pic_is_on debug m_globals_resolved =
   (new_mod, final_m, ss_off_map, fn_off_map, table_slots, initial_mblocks)
   where
-    -- Create the offset tables first. A dummy relocation function already
-    -- exists in the input module so the maps will be created correctly.
+    -- Create the function offset table first. A dummy relocation function
+    -- already so the map will be created correctly.
     (fn_off_map, last_func_offset) = makeFunctionOffsetTable m_globals_resolved
-    -- Create the data segments and the new relocation function second.
-    -- We need to update the module with the real relocation function
-    -- before we proceed.
+    -- Create the data segments, the statics offset table, and the final module
+    -- next. If we are generating position independent code (i.e. @--pic@ is
+    -- on), the relocation function must be replaced, and new data segments
+    -- (and corresponding statics) must be added, to hold the offsets needed by
+    -- the relocation function. All this is handled by @makeMemory@.
     (segs, ss_off_map, last_data_offset, final_m) = makeMemory pic_is_on m_globals_resolved fn_off_map
-    -- Continue with the rest using the "real" module.
     func_table = makeFunctionTable fn_off_map
     table_slots = fromIntegral last_func_offset
     func_imports =
