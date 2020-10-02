f5dc5e99c68830e7a4df21a944b8dac16962f53b
Cheng Shao <cheng.shao@tweag.io>
2020-10-02 17:25:02 +0000

    WIP no binaryen passes for now

--- asterius/src/Asterius/Main/Task.hs
+++ asterius/src/Asterius/Main/Task.hs
@@ -54,8 +54,8 @@ data Task
 defTask :: Task
 defTask = Task
   { target = Node,
-    optimizeLevel = 4,
-    shrinkLevel = 2,
+    optimizeLevel = 0,
+    shrinkLevel = 0,
     inputHS = error "Asterius.Main.parseTask: missing inputHS",
     outputDirectory = error "Asterius.Main.parseTask: missing outputDirectory",
     outputBaseName = error "Asterius.Main.parseTask: missing outputBaseName",
