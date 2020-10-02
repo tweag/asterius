c3f899d7867a02af1446a810f5b3194dde65600d
Georgios Karachalias <george.karachalias@gmail.com>
2020-10-02 16:32:57 +0200

    WIP

--- asterius/nofib/distclean.py
+++ asterius/nofib/distclean.py
@@ -5,11 +5,21 @@ import shutil
 import subprocess
 import sys
 
-working_directory = os.getcwd()
+# CONSTANTS
+# #############################################################################
 
 valid_compilers = ["ghc", "ahc"]
 valid_modes = ["fast", "norm", "slow"]
 categories = ["imaginary", "real", "shootout", "spectral"]
+
+# SETUP
+# #############################################################################
+
+working_directory = os.getcwd()
+
+# if working_directory
+if not (os.path.basename(working_directory) == "nofib"):
+  sys.exit("Script can only be executed in the nofib directory. Currently in {0}.".format(working_directory))
 
 def findProgram(progName):
   if shutil.which(progName):
@@ -17,61 +27,14 @@ def findProgram(progName):
   else:
     sys.exit("Could not find program {0}!".format(progName))
 
-# Find the needed executables (or die trying..)
+# Find the (absolute addresses of the) needed executables (or die trying..)
 ahc  = findProgram("ahc-link")
 ghc  = findProgram("ghc")
 time = findProgram("time")
 node = findProgram("node")
 
-print (working_directory)
-print (valid_modes)
-
-print ("ahc: {0}".format(ahc))
-print ("ghc: {0}".format(ghc))
-print ("time: {0}".format(time))
-print ("node: {0}".format(node))
-
-# # Testfile name
-# function setTestFile {
-#   if [ -f "Main.hs" ]; then
-#     TESTFILE="Main.hs"
-#   elif [ -f "Main.lhs" ]; then
-#     TESTFILE="Main.lhs"
-#   else
-#     exit 2
-#   fi
-# }
-
-def findMain(testdir):
-  if os.path.exists(os.path.join(testdir, "Main.hs")):
-    return "Main.hs" # os.path.join(testdir, "Main.hs")
-  elif os.path.exists(os.path.join(testdir, "Main.lhs")):
-    return "Main.lhs" # os.path.join(testdir, "Main.lhs")
-  else:
-    sys.exit("No Main.hs or Main.lhs found in {0}".format(testdir))
-
-def findExecutable(testdir, compiler):
-  assert (compiler in valid_compilers)
-  if compiler == "ghc":
-    filename = "Main"
-  else:
-    filename = "Main.mjs"
-  filepath = os.path.join(testdir, filename)
-  if os.path.exists(filepath):
-    return filepath
-  else:
-    sys.exit("No {0} found in {1}".format(filename, testdir))
-
-
-#   local executable=""
-#   if [ ${COMP} == "ghc" ]; then
-#     executable="${PWD}/Main"
-#   elif [ ${COMP} == "ahc" ]; then
-#     executable="${NODEJS} ${PWD}/Main.mjs"
-#   else
-#     exit 3
-#   fi
-
+# UTILITIES
+# #############################################################################
 
 def readEntireFile(filepath):
   handle = open(filepath,mode='r')
@@ -81,151 +44,84 @@ def readEntireFile(filepath):
 
 def readOptionsFromFile(filepath):
   if os.path.exists(filepath) and os.path.isfile(filepath):
-    return readEntireFile(filepath)
+    return readEntireFile(filepath).split()
   else:
-    return ""
+    return []
 
-# # Compile/link-time options
-# function setCompileOptions {
-#   local copts=""
-#   if [ -f "Main.COMPILE_OPTS" ]; then
-#     copts=$(<Main.COMPILE_OPTS)
-#   fi
-#
-#   if [ ${COMP} == "ghc" ]; then
-#     COMPILE_OPTS=${copts}
-#   elif [ ${COMP} == "ahc" ]; then
-#     COMPILE_OPTS=""
-#     for word in $copts; do
-#       COMPILE_OPTS="${COMPILE_OPTS} --ghc-option=${word}"
-#     done # CONCATENATE THEM
-#     COMPILE_OPTS="${COMPILE_OPTS} --input-hs" # last
-#   else
-#     exit 3
-#   fi
-# }
-
-def readTestCompileOptionsGHC(testdir):
-  opt_filepath = os.path.join(testdir,"Main.COMPILE_OPTS")
-  return readOptionsFromFile(opt_filepath)
-
-def readTestCompileOptionsAHC(testdir):
-  opt_filepath = os.path.join(testdir,"Main.COMPILE_OPTS")
-  ghc_opts = readOptionsFromFile(opt_filepath)
-  ahc_opts = ""
-  for option in ghc_opts.split():
-    ahc_opts += " --ghc-option={0}".format(option)
-  return ahc_opts
-  # TODO: CAREFUL: --input-hs has not been used
-
-def readTestCompileOptions(testdir, compiler):
+def getCompilerExe(compiler):
   assert (compiler in valid_compilers)
   if compiler == "ghc":
-    return readTestCompileOptionsGHC(testdir)
+    return ghc
   else:
-    return readTestCompileOptionsAHC(testdir)
+    return ahc
 
+# #############################################################################
 
-# # Runtime options
-# function setRuntimeOptions {
-#   RUNTIME_OPTS=""
-#
-#   if [ ${COMP} == "ghc" ]; then
-#     RUNTIME_OPTS="${RUNTIME_OPTS} +RTS -V0 -RTS"
-#     if [ -f "RTS_EXTRA_OPTS" ]; then
-#       local extra_opts=$(<RTS_EXTRA_OPTS)
-#       RUNTIME_OPTS="${RUNTIME_OPTS} ${extra_opts}"
-#     fi
-#   fi
-#
-#   if [ -f "${MODE}_OPTS" ]; then
-#     local ropts=$(<${MODE}_OPTS)
-#     RUNTIME_OPTS="${RUNTIME_OPTS} ${ropts}"
-#   fi
-# }
+def findMain(testdir):
+  if os.path.exists(os.path.join(testdir, "Main.hs")):
+    return os.path.join(testdir, "Main.hs")
+  elif os.path.exists(os.path.join(testdir, "Main.lhs")):
+    return os.path.join(testdir, "Main.lhs")
+  else:
+    sys.exit("No Main.hs or Main.lhs found in {0}".format(testdir))
+
+# READ COMPILE-TIME OPTIONS
+# #############################################################################
+
+# Read the compile-time options from Main.COMPILE_OPTS. If we are compiling
+# using Asterius, wrap them in --ghc-option=.
+def readTestCompileOptions(testdir, compiler):
+  assert (compiler in valid_compilers)
+  # Read the compile-time options from Main.COMPILE_OPTS
+  opt_filepath = os.path.join(testdir,"Main.COMPILE_OPTS")
+  ghc_opts = readOptionsFromFile(opt_filepath)
+  # For Asterius, wrap them in --ghc-option=
+  if compiler == "ghc":
+    return ghc_opts
+  else:
+    ahc_opts = []
+    for option in ghc_opts:
+      ahc_opts.append("--ghc-option={0}".format(option))
+    return ahc_opts
+
+# READ RUNTIME OPTIONS
+# #############################################################################
 
 def readModeSpecificOptions(testdir, mode):
   assert (mode in valid_modes)
   opt_filepath = os.path.join(testdir, mode.upper() + "_OPTS")
   return readOptionsFromFile(opt_filepath)
 
-def readTestRuntimeOptionsGHC(testdir, mode):
-  assert (mode in valid_modes)
-  extra_opts_filepath = os.path.join(testdir, "RTS_EXTRA_OPTS")
-  rts_opts = " ".join([
-    "+RTS -V0 -RTS",
-    readOptionsFromFile(extra_opts_filepath),
-    readModeSpecificOptions(testdir, mode)
-    ])
-  return rts_opts
-
-def readTestRuntimeOptionsAHC(testdir, mode):
-  assert (mode in valid_modes)
-  rts_opts = readModeSpecificOptions(testdir, mode)
-  return rts_opts
-
 def readTestRuntimeOptions(testdir, mode, compiler):
   assert (compiler in valid_compilers)
   if compiler == "ghc":
-    return readTestRuntimeOptionsGHC(testdir, mode)
+    return ["+RTS", "-V0", "-RTS"] \
+         + readOptionsFromFile(os.path.join(testdir, "RTS_EXTRA_OPTS")) \
+         + readModeSpecificOptions(testdir, mode)
   else:
-    return readTestRuntimeOptionsAHC(testdir, mode)
+    return readModeSpecificOptions(testdir, mode)
 
-def getCompilerExe(compiler):
-  assert (compiler in valid_compilers)
-  if compiler == "ghc":
-    return shutil.which("ghc")
-  else:
-    return shutil.which("ahc-link")
 
 def compileTestFile(testdir, compiler):
   assert (compiler in valid_compilers)
 
-  compiler_exe = getCompilerExe(compiler)
-  compile_opts = readTestCompileOptions(testdir, compiler)
-  testfile = findMain(testdir)
+  command = [getCompilerExe(compiler)] \
+          + readTestCompileOptions(testdir, compiler) \
+          + (["--input-hs"] if compiler == "ahc" else []) \
+          + [findMain(testdir)]
 
-  if compiler == "ahc":
-    all_opts = " ".join([compile_opts, "--input-hs", testfile])
-  else:
-    all_opts = " ".join([compile_opts, testfile])
-
-  full_command = compiler_exe + " " + all_opts
-  print ("Compile test: {0}".format(full_command))
-
-  # Switch to the test directory
+  # Execute the command in the right directory
   current_directory = os.getcwd()
   os.chdir(testdir)
-  # Compile the file
-  subprocess.call(full_command.split())
-  # Return to the previous directory
+  print ("Compile test: {0}".format(command))
+  subprocess.call(command)
   os.chdir(current_directory)
 
-# function runTest {
-#   local executable=""
-#   if [ ${COMP} == "ghc" ]; then
-#     executable="${PWD}/Main"
-#   elif [ ${COMP} == "ahc" ]; then
-#     executable="${NODEJS} ${PWD}/Main.mjs"
-#   else
-#     exit 3
-#   fi
-#
-#   local input_file_name=$1.${mode}stdin
-#   if [ -f "${input_file_name}" ]; then
-#     echo "EXECUTING: ${executable} <${input_file_name} 1>${testname}.${COMP}.stdout 2>${testname}.${COMP}.stderr"
-#     ${executable} <${input_file_name} 1>${testname}.${COMP}.stdout 2>${testname}.${COMP}.stderr
-#   else
-#     echo "EXECUTING: ${executable} 1>${testname}.${COMP}.stdout 2>${testname}.${COMP}.stderr"
-#     ${executable} 1>${testname}.${COMP}.stdout 2>${testname}.${COMP}.stderr
-#   fi
-# }
+# #############################################################################
 
 def runTestFile(testdir, testname, compiler, mode):
   assert (compiler in valid_compilers)
   assert (mode in valid_modes)
-
-  main_exe = findExecutable(testdir, compiler)
 
   # Input file
   stdinfilepath = os.path.join(testdir,"{0}.{1}stdin".format(testname, mode.lower()))
@@ -240,17 +136,18 @@ def runTestFile(testdir, testname, compiler, mode):
   stderrfilepath = os.path.join(testdir,"{0}.{1}.stderr".format(testname, compiler))
   stderrfilehandle = open(stderrfilepath, mode='w')
 
-  full_command = main_exe + " " + readTestRuntimeOptions(testdir, mode, compiler)
-  print ("Run test: {0}".format(full_command))
-  print ("Run test: {0}".format(full_command.split()))
+  command = ([node] if compiler == "ahc" else []) \
+          + [os.path.join(testdir, "Main.mjs") if compiler == "ahc" else os.path.join(testdir, "Main")] \
+          + readTestRuntimeOptions(testdir, mode, compiler)
+
+  print ("Run test: {0}".format(command))
 
   # Switch to the test directory
   current_directory = os.getcwd()
   os.chdir(testdir)
   # Run the test
   proc = subprocess.Popen(
-    full_command.split(),
-    # [main_exe], # TODO: Compute and use the runtime options!!
+    command,
     stdin=stdinfilehandle,
     stdout=stdoutfilehandle,
     stderr=stderrfilehandle,
@@ -266,7 +163,21 @@ def runTestFile(testdir, testname, compiler, mode):
   if not (proc.returncode == 0):
     sys.exit("Non-zero exit code for {0}!".format(testdir))
 
-  # print(readEntireFile(stderrfilepath))
+# #############################################################################
+
+def runAndTimeTest(category, testname, compiler, mode):
+  time_parameters = [
+    time,
+    "--format",
+    "{0},{1},{2},{3},%E,%U,%S".format(category, testname, compiler, mode),
+    "--output=../../aggregate.txt",
+    "--append"
+    ]
+
+# Would be nice..
+#   $(which time) --format "%E,%U,%S" --output=asdftest --append du -h ~
+
+# #############################################################################
 
 def main():
   for category in categories:
@@ -276,22 +187,15 @@ def main():
       if os.path.isdir(testdir):
         print ("testname: {0}".format(testname))
         print ("mainfile: {0}".format(findMain(testdir))) # mainfile
-        print ("ghc-options: {0}".format(readTestCompileOptionsGHC(testdir))) # ghc_compile_opts
-        print ("ahc-options: {0}".format(readTestCompileOptionsAHC(testdir))) # ghc_compile_opts
-        print ("ghc-rts-options: {0}".format(readTestRuntimeOptionsGHC(testdir, "fast"))) # ghc_rts_opts
-        print ("ahc-rts-options: {0}".format(readTestRuntimeOptionsAHC(testdir, "fast"))) # ghc_rts_opts
 
-        compileTestFile(testdir, "ghc")
-        runTestFile(testdir, testname, "ghc", "fast")
+        # compileTestFile(testdir, "ahc")
+        # runTestFile(testdir, testname, "ahc", "fast")
 
-        # print (lvl2)
-        # for item3 in os.listdir(lvl2):
-        #   lvl3 =  os.path.join(lvl2, item3)
-        #   if if shoulddelete(item3):
-        #     os.remove(lvl3)
-
+        compileTestFile(testdir, "ahc")
+        runTestFile(testdir, testname, "ahc", "fast")
 
 # CLEANUP
+# #############################################################################
 
 def shoulddelete(filename):
   to_delete_filenames = set([
@@ -313,7 +217,7 @@ def shoulddelete(filename):
       or filename in to_delete_filenames
     )
 
-def distclean():
+def cleanup():
   for category in categories:
     category_path = os.path.join(working_directory, category)
     for testname in os.listdir(category_path):
@@ -323,8 +227,23 @@ def distclean():
           if shoulddelete(item):
             os.remove(os.path.join(testdir,item))
 
+  # Relative paths
+  explicit_removals = [
+    "./real/fulsom/Bah",
+    "./real/compress/Lzw",
+    "./real/maillist/runtime_files/fast.tex",
+    "./real/maillist/runtime_files/norm.tex",
+    "./real/maillist/runtime_files/slow.tex"
+    ]
+
+  for filename in explicit_removals:
+    if os.path.exists(filename):
+      os.remove(filename)
+
+# #############################################################################
+
 if __name__ == "__main__":
-  # distclean()
+  # cleanup()
   main()
 
 
