ad42769975a2e676a6973ae7d6421ed99ad3caa5
Cheng Shao <cheng.shao@tweag.io>
2020-10-17 13:51:22 +0000

    Remove text shims

--- asterius/src/Asterius/Builtins.hs
+++ asterius/src/Asterius/Builtins.hs
@@ -136,9 +136,7 @@ rtsAsteriusModule opts =
         SM.fromList $
           map
             (\(func_sym, (_, func)) -> (func_sym, func))
-            ( floatCBits
-                <> textCBits
-            )
+            floatCBits
     }
     <> hsInitFunction opts
     <> wasmApplyRelocsFunction opts
@@ -615,8 +613,7 @@ rtsFunctionImports debug =
        )
     <> map
       (fst . snd)
-      ( floatCBits <> textCBits
-      )
+      floatCBits
     <> schedulerImports
     <> exportsImports
     <> envImports
@@ -710,20 +707,6 @@ rtsGlobalExports = mempty
 
 emitErrorMessage :: [ValueType] -> BS.ByteString -> Expression
 emitErrorMessage vts ev = Barf {barfMessage = ev, barfReturnTypes = vts}
-
-textCBits :: [(EntitySymbol, (FunctionImport, Function))]
-textCBits =
-  map
-    ( \(func_sym, param_vts, ret_vts) ->
-        ( mkEntitySymbol func_sym,
-          generateRTSWrapper "text" func_sym param_vts ret_vts
-        )
-    )
-    [ ("_hs_text_memcpy", [I64, I64, I64, I64, I64], []),
-      ("_hs_text_memcmp", [I64, I64, I64, I64, I64], [I64]),
-      ("_hs_text_decode_utf8", [I64, I64, I64, I64], [I64]),
-      ("_hs_text_encode_utf8", [I64, I64, I64, I64], [])
-    ]
 
 floatCBits :: [(EntitySymbol, (FunctionImport, Function))]
 floatCBits =
