a22826876e0d81de13a8f1c401be8ed814f5b217
Cheng Shao <cheng.shao@tweag.io>
2020-10-25 16:45:53 +0000

    Update wasi-sdk & use correct lto flags

--- .github/workflows/setup-deps.sh
+++ .github/workflows/setup-deps.sh
@@ -20,4 +20,4 @@ sudo dpkg -i /tmp/binaryen.deb
 rm /tmp/binaryen.deb
 
 sudo mkdir -p /opt/wasi-sdk
-curl -L https://github.com/TerrorJack/wasi-sdk/releases/download/201014/wasi-sdk-11.5g3cbd9d212e9a-linux.tar.gz | sudo tar xz -C /opt/wasi-sdk --strip-components=1
+curl -L https://github.com/TerrorJack/wasi-sdk/releases/download/201025/wasi-sdk-11.6g353da434653e-linux.tar.gz | sudo tar xz -C /opt/wasi-sdk --strip-components=1
