053b116b2f85e883c0621e4816e9b77c90d5ce21
Cheng Shao <cheng.shao@tweag.io>
2020-11-03 09:31:29 +0000

    WIP

--- utils/make-packages.py
+++ utils/make-packages.py
@@ -10,8 +10,32 @@ ghc_repo_url = "https://github.com/TerrorJack/ghc.git"
 ghc_repo_branch = "asterius-8.8"
 workdir = os.getcwd()
 ghc_repo_path = os.path.join(workdir, "ghc")
+hadrian_path = os.path.join(ghc_repo_path, "hadrian", "build.stack.sh")
 ghci_asterius_path = os.path.join(workdir, "ghci-asterius")
 ghc_asterius_path = os.path.join(workdir, "ghc-asterius")
+
+autogen_files = [
+    "_build/generated/GHCConstantsHaskellExports.hs",
+    "_build/generated/GHCConstantsHaskellType.hs",
+    "_build/generated/GHCConstantsHaskellWrappers.hs",
+    "_build/stage0/compiler/build/Config.hs",
+    "_build/stage0/compiler/build/ghc_boot_platform.h",
+    "_build/stage0/compiler/build/primop-can-fail.hs-incl",
+    "_build/stage0/compiler/build/primop-code-size.hs-incl",
+    "_build/stage0/compiler/build/primop-commutable.hs-incl",
+    "_build/stage0/compiler/build/primop-data-decl.hs-incl",
+    "_build/stage0/compiler/build/primop-fixity.hs-incl",
+    "_build/stage0/compiler/build/primop-has-side-effects.hs-incl",
+    "_build/stage0/compiler/build/primop-list.hs-incl",
+    "_build/stage0/compiler/build/primop-out-of-line.hs-incl",
+    "_build/stage0/compiler/build/primop-primop-info.hs-incl",
+    "_build/stage0/compiler/build/primop-strictness.hs-incl",
+    "_build/stage0/compiler/build/primop-tag.hs-incl",
+    "_build/stage0/compiler/build/primop-vector-tycons.hs-incl",
+    "_build/stage0/compiler/build/primop-vector-tys-exports.hs-incl",
+    "_build/stage0/compiler/build/primop-vector-tys.hs-incl",
+    "_build/stage0/compiler/build/primop-vector-uniques.hs-incl"
+]
 
 
 def ghc_checkout():
@@ -49,35 +73,31 @@ def ghc_configure():
     subprocess.run([os.path.join(ghc_repo_path, "boot"), "--hadrian"],
                    cwd=ghc_repo_path,
                    check=True)
-    subprocess.run([os.path.join(ghc_repo_path, "configure")],
+    subprocess.run([
+        "stack",
+        "--stack-yaml=" + os.path.join(ghc_repo_path, "hadrian", "stack.yaml"),
+        "exec", "--no-ghc-package-path", "--no-stack-exe", "--",
+        os.path.join(ghc_repo_path, "configure")
+    ],
                    cwd=ghc_repo_path,
                    check=True)
 
 
+def patch_hadrian():
+    with open(os.path.join(ghc_repo_path, "hadrian", "stack.yaml"),
+              mode="w") as f:
+        f.write("resolver: lts-16.20\n")
+    shutil.copy(os.path.join(workdir, "UserSettings.hs"),
+                os.path.join(ghc_repo_path, "hadrian", "UserSettings.hs"))
+
+
+def make_hadrian():
+    patch_hadrian()
+    subprocess.run([hadrian_path, "--version"], cwd=ghc_repo_path, check=True)
+
+
 def make_autogen():
-    subprocess.run([
-        os.path.join(ghc_repo_path, "hadrian", "build.sh"),
-        "_build/generated/GHCConstantsHaskellExports.hs",
-        "_build/generated/GHCConstantsHaskellType.hs",
-        "_build/generated/GHCConstantsHaskellWrappers.hs",
-        "_build/stage0/compiler/build/Config.hs",
-        "_build/stage0/compiler/build/ghc_boot_platform.h",
-        "_build/stage0/compiler/build/primop-can-fail.hs-incl",
-        "_build/stage0/compiler/build/primop-code-size.hs-incl",
-        "_build/stage0/compiler/build/primop-commutable.hs-incl",
-        "_build/stage0/compiler/build/primop-data-decl.hs-incl",
-        "_build/stage0/compiler/build/primop-fixity.hs-incl",
-        "_build/stage0/compiler/build/primop-has-side-effects.hs-incl",
-        "_build/stage0/compiler/build/primop-list.hs-incl",
-        "_build/stage0/compiler/build/primop-out-of-line.hs-incl",
-        "_build/stage0/compiler/build/primop-primop-info.hs-incl",
-        "_build/stage0/compiler/build/primop-strictness.hs-incl",
-        "_build/stage0/compiler/build/primop-tag.hs-incl",
-        "_build/stage0/compiler/build/primop-vector-tycons.hs-incl",
-        "_build/stage0/compiler/build/primop-vector-tys-exports.hs-incl",
-        "_build/stage0/compiler/build/primop-vector-tys.hs-incl",
-        "_build/stage0/compiler/build/primop-vector-uniques.hs-incl"
-    ],
+    subprocess.run([hadrian_path, "-j"] + autogen_files,
                    cwd=ghc_repo_path,
                    check=True)
 
@@ -152,11 +172,8 @@ def make_ghc_asterius():
     os.mkdir(autogen_path)
     shutil.copy(os.path.join(ghc_repo_path, "includes", "CodeGen.Platform.hs"),
                 autogen_path)
-    for f in glob.glob(os.path.join(
-            ghc_repo_path, "_build", "generated", "*")) + glob.glob(
-                os.path.join(ghc_repo_path, "_build", "stage0", "compiler",
-                             "build", "*")):
-        shutil.copy(f, autogen_path)
+    for f in autogen_files:
+        shutil.copy(os.path.join(ghc_repo_path, f), autogen_path)
     patch_ghc_cabal()
     patch_ghc_include()
 
@@ -164,6 +181,7 @@ def make_ghc_asterius():
 if __name__ == "__main__":
     ghc_checkout()
     ghc_clean()
+    make_hadrian()
     ghc_configure()
     make_autogen()
     make_ghci_asterius()
