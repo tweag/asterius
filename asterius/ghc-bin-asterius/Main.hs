d4b05b0b245538933abf2ee5f46208a8324ded42
Cheng Shao <cheng.shao@tweag.io>
2020-11-30 10:57:53 +0000

    Don't fixEnv in ahc/ahc-pkg

--- asterius/ghc-bin-asterius/Main.hs
+++ asterius/ghc-bin-asterius/Main.hs
@@ -79,7 +79,6 @@ import Data.Maybe
 import Prelude
 
 import qualified Asterius.BuildInfo as A
-import qualified Asterius.FixEnv as A
 import qualified Asterius.FrontendPlugin as A
 import Control.Exception
 import System.Process
@@ -98,7 +97,6 @@ import System.Process
 
 main :: IO ()
 main = do
-   A.fixEnv
    initGCStatistics -- See Note [-Bsymbolic and hooks]
    hSetBuffering stdout LineBuffering
    hSetBuffering stderr LineBuffering
