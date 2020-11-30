e248d9c7b8be823309224c915371400942932ebb
Cheng Shao <cheng.shao@tweag.io>
2020-11-30 12:17:11 +0000

    Do fixEnv in ahc-link/ahc-dist

--- asterius/app/ahc-dist.hs
+++ asterius/app/ahc-dist.hs
@@ -1,9 +1,11 @@
 import Asterius.Binary.File
 import Asterius.Binary.NameCache
+import Asterius.FixEnv
 import Asterius.Main
 
 main :: IO ()
 main = do
+  fixEnv
   ncu <- newNameCacheUpdater
   task <- getTask
   ld_result <- getFile ncu $ inputHS task
