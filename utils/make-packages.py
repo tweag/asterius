139749a166e0fe876f380432706e96dcf70155e1
Cheng Shao <cheng.shao@tweag.io>
2020-12-01 10:46:52 +0000

    Fix a missed patch line in ghc-pkg-asterius

--- utils/make-packages.py
+++ utils/make-packages.py
@@ -333,6 +333,8 @@ def patch_ghc_pkg_cabal():
                 ls.append("Name: ghc-pkg-asterius\n")
             elif l.lower().split() == ["executable", "ghc-pkg"]:
                 ls.append("Executable ghc-pkg-asterius\n")
+            elif l.strip().lower().startswith("ghc-boot,"):
+                ls.append("                   ghc-boot-asterius,\n")
             else:
                 ls.append(l)
     with open(os.path.join(ghc_pkg_asterius_path, "ghc-pkg-asterius.cabal"),
