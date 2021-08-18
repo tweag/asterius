434bda31104c033e6d27c7834a6580158773f004
Cheng Shao <cheng.shao@tweag.io>
2021-08-18 11:45:22 +0000

    WIP

--- shell.nix
+++ shell.nix
@@ -1,16 +1,16 @@
-{ sources ? import ./sources.nix { }
+{ sources ? import ./nix/sources.nix { }
 , haskellNix ? import sources.haskell-nix { }
 , pkgs ? import sources.nixpkgs
     (haskellNix.nixpkgsArgs // {
       overlays = haskellNix.nixpkgsArgs.overlays ++ [
         (import "${sources.wasi-sdk}/nix/binaryen.nix")
         (import "${sources.wasi-sdk}/nix/wasmtime.nix")
-        (import ./libghcconstants.nix)
-        (import ./wizer.nix)
+        (import ./nix/libghcconstants.nix)
+        (import ./nix/wizer.nix)
       ];
     })
 , ghc ? "ghc8105"
-, hsPkgs ? pkgs.callPackage ./pkg-set.nix { inherit pkgs ghc; }
+, hsPkgs ? pkgs.callPackage ./nix/pkg-set.nix { inherit pkgs ghc; }
 }:
 (hsPkgs.shellFor {
   packages = ps: with ps; [ asterius ghc-toolkit wasm-toolkit ];
